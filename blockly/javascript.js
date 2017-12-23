13d63ed5aee848345a351cbca5de2803965d0138
fustyles <34397289+fustyles@users.noreply.github.com>
2017-12-24 00:09:07 +0800

    Delete javascript.js

--- blockly/javascript.js
+++
@@ -1,9 +0,0 @@
-Blockly.JavaScript['showimage_france'] = function(block) {
-  var value_imageurl = Blockly.JavaScript.valueToCode(block, 'ImageUrl', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_imagewidth = Blockly.JavaScript.valueToCode(block, 'ImageWidth', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_imageheight = Blockly.JavaScript.valueToCode(block, 'ImageHeight', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '<image src="' + value_imageurl + '" width="' + value_imagewidth + '" height="' + value_imageheight + '">';
-  // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.JavaScript.ORDER_NONE];
-};
