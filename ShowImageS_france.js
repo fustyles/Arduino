7c73cc96b8598b71277c203e5738836f28267a3d
fustyles <34397289+fustyles@users.noreply.github.com>
2017-12-23 23:51:34 +0800

    Delete ShowImageS_france.js

--- ShowImageS_france.js
+++
@@ -1,9 +0,0 @@
-Blockly.JavaScript['showimage_france'] = function(block) {
-  var value_imageurl = Blockly.JavaScript.valueToCode(block, 'ImageUrl', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_imagewidth = Blockly.JavaScript.valueToCode(block, 'ImageWidth', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_imageheight = Blockly.JavaScript.valueToCode(block, 'ImageHeight', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...';
-  // TODO: Change ORDER_NONE to the correct strength.
-  return [code, Blockly.JavaScript.ORDER_NONE];
-};
