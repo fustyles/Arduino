25f918b589dfb2c4574e33cbdf597b2535237c39
fustyles <34397289+fustyles@users.noreply.github.com>
2017-12-24 00:05:08 +0800

    Add files via upload

--- blockly/javascript.js
+++ blockly/javascript.js
@@ -3,7 +3,7 @@ Blockly.JavaScript['showimage_france'] = function(block) {
   var value_imagewidth = Blockly.JavaScript.valueToCode(block, 'ImageWidth', Blockly.JavaScript.ORDER_ATOMIC);
   var value_imageheight = Blockly.JavaScript.valueToCode(block, 'ImageHeight', Blockly.JavaScript.ORDER_ATOMIC);
   // TODO: Assemble JavaScript into code variable.
-  var code = '...';
+  var code = '<image src="' + value_imageurl + '" width="' + value_imagewidth + '" height="' + value_imageheight + '">';
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
