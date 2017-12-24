e75c9c6914ff0d7583bec38ff43edfd28ec69467
fustyles <34397289+fustyles@users.noreply.github.com>
2017-12-25 01:44:58 +0800

    Update zh-hans.js

--- blockly2/msg/blocks/zh-hans.js
+++ blockly2/msg/blocks/zh-hans.js
@@ -1,4 +1,4 @@
 Blockly.Msg.TEXT_MAIN = "显示网页";
-Blockly.Msg.TEXT_URL = "Url";
-Blockly.Msg.TEXT_WIDTH = "Width";
-Blockly.Msg.TEXT_HEIGHT = "Height";
+Blockly.Msg.TEXT_URL = "网址";
+Blockly.Msg.TEXT_WIDTH = "视窗宽度";
+Blockly.Msg.TEXT_HEIGHT = "视窗高度";
