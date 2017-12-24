5fd6afec977969d6c98bda36fc66f970db6c5d6f
fustyles <34397289+fustyles@users.noreply.github.com>
2017-12-25 01:33:39 +0800

    Update blocks.js

--- blockly2/blocks.js
+++ blockly2/blocks.js
@@ -5,3 +5,29 @@ Blockly.Blocks['iframe2_fustyles'] = {
       .appendField(Blockly.Msg.TEXT_SHOW);
   }
 };
+
+Blockly.Blocks['PageUrl'] = {
+  init: function() {
+  this.appendValueInput('string_')
+      .setCheck('String')
+      .appendField(Blockly.Msg.TEXT_SHOW);
+  }
+};
+
+Blockly.Blocks['PageWidth'] = {
+  init: function() {
+  this.appendValueInput('string_')
+      .setCheck('Number')
+      .appendField(Blockly.Msg.TEXT_SHOW);
+  }
+};
+
+Blockly.Blocks['PageHeight'] = {
+  init: function() {
+  this.appendValueInput('string_')
+      .setCheck('Number')
+      .appendField(Blockly.Msg.TEXT_SHOW);
+  }
+};
+
+
