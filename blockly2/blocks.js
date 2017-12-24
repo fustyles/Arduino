c34f50679924205453fccdbf580388b1070f60cc
fustyles <34397289+fustyles@users.noreply.github.com>
2017-12-24 22:05:13 +0800

    Update blocks.js

--- blockly2/blocks.js
+++ blockly2/blocks.js
@@ -3,9 +3,6 @@ Blockly.Blocks['iframe2_fustyles'] = {
   this.appendValueInput('string_')
       .setCheck('String')
       .appendField(Blockly.Msg.TEXT_SHOW);
-  this.setOutput(false, null);
-  this.setTooltip('');
   this.setColour(Blockly.Blocks.colour.HUE);
-  this.setHelpUrl('');
   }
 };
