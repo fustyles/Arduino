479a6e5892c089db150e70ec33aa765c65113545
fustyles <34397289+fustyles@users.noreply.github.com>
2017-12-25 10:34:18 +0800

    Delete javascript.js

--- blockly2/javascript.js
+++
@@ -1,8 +0,0 @@
-Blockly.JavaScript['iframe2_fustyles'] = function (block) {
-  var value_pageurl = Blockly.JavaScript.valueToCode(block, 'Url', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_pagewidth = Blockly.JavaScript.valueToCode(block, 'Width', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_pageheight = Blockly.JavaScript.valueToCode(block, 'Height', Blockly.JavaScript.ORDER_ATOMIC);
-
-  var code = ' document.getElementById("demo-area-01-show").innerHTML = "<iframe src=' + value_pageurl  + ' width=' + value_pagewidth + ' height=' + value_pageheight + '>";\n';
-  return code;
-};
