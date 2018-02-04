ca436a105a5b255b85b99edf34b214d8b9800ed5
fustyles <34397289+fustyles@users.noreply.github.com>
2018-02-04 10:33:15 +0800

    Update ESP8266_LED.ino

--- ESP8266_LED.ino
+++ ESP8266_LED.ino
@@ -1,4 +1,4 @@
-﻿//Author : ChungYi Fu
+//Author : ChungYi Fu
 
 #include <SoftwareSerial.h>
 SoftwareSerial mySerial(4, 5); // Arduino RX:4, TX:5 
@@ -23,11 +23,9 @@ void setup()
   mySerial.println("AT+CIPSERVER=1,80");
   waitreply(1000);
   mySerial.println("AT+CWJAP=\"id\",\"pwd\"");
-  waitreply(2000);
-  mySerial.println("AT+CIFSR");
-  waitreply(2000);
+  waitreply(10000);
 }
-void loop() // run over and over
+void loop() 
 {
   String str="";
   if (mySerial.available())
@@ -38,16 +36,44 @@ void loop() // run over and over
       str += char(toupper(c));
       Serial.print(c);
       delay(10);
+      if (str.indexOf("HTTP")!= -1) break;
     }  
   }
- 
-  if (str.indexOf("?TURNON")!= -1)
-    digitalWrite(pinLED,1);
-  else if (str.indexOf("?TURNOFF")!= -1)
-    digitalWrite(pinLED,0);  
 
-  if (str.indexOf("IPD,")!= -1)   
+  if (str.indexOf("?TURNON")!= -1)
   {
+    digitalWrite(pinLED,1);
+    feedback(str,"<font color=red>TURN ON</font>",32);
+  }
+  else if (str.indexOf("?TURNOFF")!= -1)
+  {
+    digitalWrite(pinLED,0);  
+    feedback(str,"<font color=blue>TURN OFF</font>",34);
+  }
+  else if (str.indexOf("IPD,")!= -1)
+  {
+    feedback(str,"SORRY",7);
+  }  
+}
+
+void waitreply(int timelimit)
+{
+    int st = 0;
+    long int starttime = millis();
+    while( (starttime + timelimit) > millis())
+    {
+        while(mySerial.available())
+        {
+            char c = mySerial.read(); 
+            delay(10);
+            st=1;
+        }
+        if (st==1) return;
+    } 
+}
+
+void feedback(String str,String response,int len)
+{
     String CID = String(str.charAt(str.indexOf("IPD,")+4));
     
     mySerial.println("AT+CIPSEND="+CID+",8");
@@ -70,37 +96,19 @@ void loop() // run over and over
     mySerial.println("</head>");
     waitreply(2000);
     
-    mySerial.println("AT+CIPSEND="+CID+",24");
+    mySerial.println("AT+CIPSEND="+CID+",8");
     delay(50);
-    mySerial.println("<body><font color=red>");
+    mySerial.println("<body>");
     waitreply(2000);    
     
-    String feedback = "HAPPY NEW YEAR";
-    mySerial.println("AT+CIPSEND="+CID+","+String(feedback.length()+2));
+    mySerial.println("AT+CIPSEND="+CID+","+String(len));
     delay(50);
-    mySerial.println(feedback);
+    mySerial.println(response);
     waitreply(2000);
     
-    mySerial.println("AT+CIPSEND="+CID+",23");
+    mySerial.println("AT+CIPSEND="+CID+",16");
     delay(50);
-    mySerial.println("</font></body></html>");
+    mySerial.println("</body></html>");
     waitreply(2000);
     mySerial.println("AT+CIPCLOSE="+CID);
-  }  
-}
-
-void waitreply(int timelimit)
-{
-    int st = 0;
-    long int starttime = millis();
-    while( (starttime + timelimit) > millis())
-    {
-        while(mySerial.available())
-        {
-            char c = mySerial.read(); 
-            delay(10);
-            st=1;
-        }
-        if (st==1) return;
-    } 
 }
