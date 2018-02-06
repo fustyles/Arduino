2ba594612bbb454b6203b4ff70df4a574a759c4d
fustyles <34397289+fustyles@users.noreply.github.com>
2018-02-06 18:10:57 +0800

    Update ESP8266_LED.ino

--- ESP8266_LED.ino
+++ ESP8266_LED.ino
@@ -109,6 +109,7 @@ void SendData(String data,int TimeLimit)
 
 void Feedback(String CID,String Response,byte datatype)
 {
+  SendData("HTTP/1.1 200 OK",2000);
   SendData("Access-Control-Allow-Origin:*",2000);
   SendData("Access-Control-Allow-Methods: GET, POST, PUT",2000);
   
