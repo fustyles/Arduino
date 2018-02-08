605591cf9ee7d6bfdb3ca0d57167053094e89bcb
fustyles <34397289+fustyles@users.noreply.github.com>
2018-02-08 22:07:14 +0800

    Update Bluetooth_HC06_MyFirmata.ino

--- Bluetooth_HC06_MyFirmata.ino
+++ Bluetooth_HC06_MyFirmata.ino
@@ -25,7 +25,7 @@ SoftwareSerial mySerial(10, 11); // Arduino RX:10, TX:11
 void setup()
 {
   Serial.begin(9600);
-  mySerial.begin(57600);
+  mySerial.begin(9600);
   Serial.println("Hello World");
 }
 
