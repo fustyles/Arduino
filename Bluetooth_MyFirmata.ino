e69820a898044a33a254519924a5ea5f48e23bec
fustyles <34397289+fustyles@users.noreply.github.com>
2018-02-11 01:42:52 +0800

    Update Bluetooth_MyFirmata.ino

--- Bluetooth_MyFirmata.ino
+++ Bluetooth_MyFirmata.ino
@@ -1,56 +1,39 @@
-// ESP8266 ESP-01
+// Bluetooth HC05&HC06
 
-// Author : ChungYi Fu (Kaohsiung, Taiwan)  2018-2-11 00:50
+// Author : ChungYi Fu (Kaohsiung, Taiwan)  2018-2-10 00:30 
 
-// Command format :
+// Command format :  
 // ?cmd  
-// Number： ?cmd=num1  ?cmd=num1,num2
+// Number：  ?cmd=num1  ?cmd=num1,num2
 // String ： ?&cmd=str1  ?&cmd=str1,str2 
 // String+Number ： ?+cmd=str1,num2   
 
-// AP IP： 192.168.4.1
-// http://192.168.4.1/?&resetwifi=id,pwd
-// http://192.168.4.1/?ip
-// http://192.168.4.1/?&at=AT+Command
-// http://192.168.4.1/?inputpullup=3
-// http://192.168.4.1/?pinmode=3,1
-// http://192.168.4.1/?digitalwrite=3,1
-// http://192.168.4.1/?analogwrite=3,200
-// http://192.168.4.1/?digitalread=3
-// http://192.168.4.1/?analogread=3
-// http://192.168.4.1/?&message=Hello
-// http://192.168.4.1/?&message=Hello,World
-// http://192.168.4.1/?+message=Hello,100
-
-// STA IP：
-// Query： http://192.168.4.1/?ip
+// ?inputpullup=3
+// ?pinmode=3,1
+// ?digitalwrite=3,1
+// ?analogwrite=3,200
+// ?digitalread=3
+// ?analogread=3
+// ?&message=Hello
+// ?&message=Hello,World
+// ?+message=Hello,100
 
 
 #include <SoftwareSerial.h>
 SoftwareSerial mySerial(10, 11); // Arduino RX:10, TX:11 
 
-String SSID="wifi_id";
-String PWD="wifi_pwd";
-String ClientIP="";
-
 void setup()
 {
   Serial.begin(9600);
-  mySerial.begin(9600);
-
-  SendData("AT+RST",5000);
-  SendData("AT+CWMODE=3",2000);
-  SendData("AT+CIPMUX=1",2000);
-  SendData("AT+CIPSERVER=1,80",2000);
-  //SendData("AT+CIPSTA=\"192.168.0.3\",\"192.168.0.1\",\"255.255.255.0\"",2000);
-  SendData("AT+CWJAP=\""+SSID+"\",\""+PWD+"\"",5000);
+  mySerial.begin(57600);    //Check your bluetooth baud rate
+  Serial.println("Hello World");
 }
 
 void loop() 
 {
   String ReceiveData="", command="";
-  String cmd="",str1="",str2="";
   long int num1=-1,num2=-1;
+  String cmd="",str1="",str2="";
   byte ReceiveState=0,cmdState=1,num1State=0,num2State=0;
   
   if (mySerial.available())
@@ -102,58 +85,16 @@ void loop()
       }
     }  
     Serial.println(ReceiveData);
-    
-    if (ReceiveData.indexOf("WIFI GOT IP")!=-1)
-    { 
-        pinMode(13,1);
-        for (int i=0;i<20;i++)
-        {
-          digitalWrite(13,1);
-          delay(50);
-          digitalWrite(13,0);
-          delay(50);
-        }
-        long int StartTime=millis();
-        while( (StartTime+3000) > millis())
-        {
-            while(mySerial.available())
-            {
-                mySerial.read();
-            }
-        } 
-  
-        ClientIP="";
-        int readstate=0,j=0;
-        mySerial.println("AT+CIFSR");
-        mySerial.flush();
-        delay(5);
-        while(mySerial.available())
-        {
-              char c=mySerial.read();
-              String t=String(c);
-              //Serial.print(t);
-              
-              if (t.indexOf("\"")!=-1) j++;
-              if (j==5) 
-                readstate=1;
-              else if (j==6)
-                readstate=0;
-              if ((readstate==1)&&(t.indexOf("\"")==-1)) ClientIP=ClientIP+t;
-        } 
-        Serial.println(ClientIP);
-    }
   }
-  
-  if ((ReceiveData.indexOf(" HTTP")!=-1)&&(ReceiveData.indexOf("?")!=-1))
+
+  if (ReceiveData.indexOf("?")==0)
   {
     Serial.println("");
     Serial.println("command: "+command);
-    Serial.println("cmd= "+cmd);
+    Serial.println("cmd: "+cmd);
     Serial.println("num1= "+String(num1)+" ,num2= "+String(num2));
     Serial.println("str1= "+String(str1)+" ,str2= "+String(str2));
-    
-    String CID=String(ReceiveData.charAt(ReceiveData.indexOf("IPD,")+4));
-    
+
     while (mySerial.available())
     {
       mySerial.read();
@@ -162,116 +103,52 @@ void loop()
     if (cmd=="your command")
       {
         // you can do anything
-        
-        //Feedback(CID,"<font color=\"red\">Hello World</font>",0);  --> HTML
-        //Feedback(CID,"Hello World",1);  --> XML
-        //Feedback(CID,"Hello World",2);  --> JSON
-        //Feedback(CID,"<html>Hello World</html>",3);  --> Custom definition
-      }
-    else if (cmd=="ip")
-      {
-        Feedback(CID,"<html>STAIP:"+ClientIP+"</html>",3);
-      }
-    else if (cmd=="&at")
-      {
-        mySerial.println(str1);
-        mySerial.flush();
-        delay(5);
-        Feedback(CID,"<html>"+command+"</html>",3);
+        //SendData("Hello World");
       }
     else if (cmd=="inputpullup")
       {
         pinMode(num1, INPUT_PULLUP);
-        Feedback(CID,"<html>"+command+"</html>",3);
+        SendData(command);
       }  
     else if (cmd=="pinmode")
       {
         pinMode(num1, num2);
-        Feedback(CID,"<html>"+command+"</html>",3);
+        SendData(command);
       }        
-   else if (cmd=="digitalwrite")
+    else if (cmd=="digitalwrite")
       {
         digitalWrite(num1,num2);
-        Feedback(CID,"<html>"+command+"</html>",3);
+        SendData(command);
       }   
     else if (cmd=="digitalread")
       {
-        Feedback(CID,"<html>"+String(digitalRead(num1))+"</html>",3);
+        SendData(String(digitalRead(num1)));
       }    
     else if (cmd=="analogwrite")
       {
         analogWrite(num1,num2);
-        Feedback(CID,"<html>"+command+"</html>",3);
+        SendData(command);
       }       
     else if (cmd=="analogread")
       {
-        Feedback(CID,"<html>"+String(analogRead(num1))+"</html>",3);
+        SendData(String(analogRead(num1)));
       }    
-    else if (cmd=="&resetwifi")
-      {
-        Feedback(CID,"<html>"+str1+","+str2+"</html>",3);
-        SendData("AT+CWJAP=\""+str1+"\",\""+str2+"\"",5000);
-        mySerial.flush();
-      }       
     else if (cmd=="&message")
       {
-        Feedback(CID,"<html>"+str1+","+str2+"</html>",3);
+        SendData(str1+","+str2);
       }     
     else if (cmd=="+message")
       {
-        Feedback(CID,"<html>"+str1+","+String(num2)+"</html>",3);
+        SendData(str1+","+String(num2));
       }                
     else 
       {
-        Feedback(CID,"<html>Command is not defined</html>",3);
+        SendData("Command is not defined");
       }  
   }
 }
 
-void SendData(String data,int TimeLimit)
+void SendData(String data)
 {
-  mySerial.println(data);
-  mySerial.flush();
-  delay(20);
-  WaitReply(TimeLimit);
-}
-
-void Feedback(String CID,String Response,byte datatype)
-{
-  if (datatype==0)
-  {
-    Response="<!DOCTYPE HTML><html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"></head><body>"+Response+"</body></html>";
-  }
-  else if (datatype==1) 
-  {
-    Response="<?xml version=\"1.0\" encoding=\"UTF-8\"?><ESP8266><Data><Text>"+Response+"</Text></Data></ESP8266>";
-  }  
-  else if (datatype==2) 
-  {
-    Response="[{\"ESP8266\":\""+Response+"\"}]";
-  }
-  else
-    Response=Response;
-
-  SendData("AT+CIPSEND="+CID+","+(Response.length()+2),2000);
-  SendData(Response,2000);
-  SendData("AT+CIPCLOSE="+CID,2000);
-}
-
-String WaitReply(long int TimeLimit)
-{
-  String ReceiveData="";
-  byte ReceiveState=0;
-  long int StartTime=millis();
-  while( (StartTime+TimeLimit) > millis())
-  {
-      while(mySerial.available())
-      {
-          ReceiveData=ReceiveData+char(mySerial.read());
-          delay(10);
-          ReceiveState=1;
-      }
-      if (ReceiveState==1) return ReceiveData;
-  } 
-  return "";
+  mySerial.print(data);
 }
