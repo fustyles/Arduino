78c502fb60b4ccea5d082391c48b6cfcbb04ebbf
fustyles <34397289+fustyles@users.noreply.github.com>
2018-02-12 12:09:17 +0800

    Update ESP8266_MyFirmata.ino

--- ESP8266_MyFirmata.ino
+++ ESP8266_MyFirmata.ino
@@ -48,6 +48,7 @@ void setup()
   SendData("AT+CWMODE=3",2000);
   SendData("AT+CIPMUX=1",2000);
   SendData("AT+CIPSERVER=1,80",2000);
+  //SendData("AT+CWDHCP=2,1",2000);
   //SendData("AT+CIPSTA=\"192.168.0.100\",\"192.168.0.1\",\"255.255.255.0\"",2000);
   SendData("AT+CWJAP=\""+SSID+"\",\""+PWD+"\"",5000); 
 }
