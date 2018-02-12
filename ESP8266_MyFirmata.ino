61dd3bdbe910658017ede4bc65cd6a2b98b225ef
fustyles <34397289+fustyles@users.noreply.github.com>
2018-02-12 23:24:48 +0800

    Update ESP8266_MyFirmata.ino

--- ESP8266_MyFirmata.ino
+++ ESP8266_MyFirmata.ino
@@ -47,8 +47,8 @@ void setup()
   Serial.begin(9600);
   
   //You must change ESP8266 uart baud rate to 9600 by "AT+UART_DEF=9600,8,1,0,0"
-  mySerial.begin(115200);   //Default uart baud rate
-  SendData("AT+UART_DEF=9600,8,1,0,0",2000);   //Change uart baud rate
+  mySerial.begin(115200);   //Default baud rate -> 19200,38400,57600,74880,115200
+  SendData("AT+UART_DEF=9600,8,1,0,0",2000);   //Change uart baud rate to 9600
   
   mySerial.begin(9600);
   
