c866c1be82ec063fcff94019d738538c32fa7579
fustyles <34397289+fustyles@users.noreply.github.com>
2018-02-16 13:56:47 +0800

    Update ESP8266_MyFirmata.ino

--- ESP8266_MyFirmata.ino
+++ ESP8266_MyFirmata.ino
@@ -326,7 +326,7 @@ void getVariable()
     
       
         pinMode(13,1);
-        for (int i=0;i<10;i++)
+        for (int i=0;i<20;i++)
         {
           digitalWrite(13,1);
           delay(50);
