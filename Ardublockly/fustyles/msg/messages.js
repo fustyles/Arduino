63d8d8cd51341f42beb0f48b06f74df03b9e63d5
fustyles <34397289+fustyles@users.noreply.github.com>
2018-02-17 20:43:41 +0800

    Add files via upload

---
+++ Ardublockly/fustyles/msg/messages.js
@@ -0,0 +1,29 @@
+﻿/**
+ * @license Licensed under the Apache License, Version 2.0 (the "License"):
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+/**
+ * @fileoverview English strings for test blocks. All names have the postfix
+ *     BLOCKS_{category} from the blocks_config.json data.
+ */
+'use strict';
+
+goog.require('Blockly.Msg.en');
+
+/**
+ * Due to the frequency of long strings, the 80-column wrap rule need not apply
+ * to message files.
+ */
+
+/// Toolbox category name
+//Blockly.Msg.BLOCKS_FUSTYLES_TOOLBOX = 'FUSTYLES';
+
+/// Ardublockly name
+Blockly.Msg.BLOCKS_FUSTYLES_TITLE = 'RGB LED ';
+Blockly.Msg.BLOCKS_FUSTYLES_RED_TITLE = '紅';
+Blockly.Msg.BLOCKS_FUSTYLES_GREEN_TITLE = '綠';
+Blockly.Msg.BLOCKS_FUSTYLES_BLUE_TITLE = '藍';
+Blockly.Msg.BLOCKS_FUSTYLES_RED_VALUE = '  紅';
+Blockly.Msg.BLOCKS_FUSTYLES_GREEN_VALUE = '  綠';
+Blockly.Msg.BLOCKS_FUSTYLES_BLUE_VALUE = '  藍';
