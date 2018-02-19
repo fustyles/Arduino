e79cb8a66be93fc455cab15ff7341c3772121c33
fustyles <34397289+fustyles@users.noreply.github.com>
2018-02-19 16:01:31 +0800

    Update Bluetooth_MyFirmata.ino

--- Bluetooth_MyFirmata.ino
+++ Bluetooth_MyFirmata.ino
@@ -1,12 +1,12 @@
 /*
 Bluetooth
-Author : ChungYi Fu (Kaohsiung, Taiwan)  2018-2-19 01:30 
-Command format :  ?cmd = str1 ,str2   
+Author : ChungYi Fu (Kaohsiung, Taiwan)  2018-2-19 16:00 
+Command format :  ?cmd = str1;str2   
 
 ?inputpullup=3
-?pinmode=3,1
-?digitalwrite=3,1
-?analogwrite=3,200
+?pinmode=3;1
+?digitalwrite=3;1
+?analogwrite=3;200
 ?digitalread=3
 ?analogread=3
 */
@@ -37,7 +37,7 @@ void loop()
     if (cmd=="yourcmd")
       {
         //you can do anything
-        //SendData(cmd+"="+str1+","+str2);
+        //SendData(cmd+"="+str1+";"+str2);
       } 
     else if (cmd=="inputpullup")
       {
@@ -82,7 +82,7 @@ void SendData(String data)
 void getVariable()
 {
   ReceiveData="";command="";cmd="";str1="";str2="";
-  byte ReceiveState=0,cmdState=1,num1State=0,num2State=0,commastate=0,equalstate=0;
+  byte ReceiveState=0,cmdState=1,str1State=0,str2State=0,commastate=0,equalstate=0;
   
   if (mySerial.available())
   {
@@ -91,31 +91,31 @@ void getVariable()
       char c=mySerial.read();
       ReceiveData=ReceiveData+String(c);
       
-      if (String(c).indexOf("?")!=-1) ReceiveState=1;
-      if (String(c).indexOf(" ")!=-1) ReceiveState=0;
+      if (c=='?') ReceiveState=1;
+      if ((c==' ')||(c=='\n')) ReceiveState=0;
       if (ReceiveState==1)
       {
         command=command+String(c);
 
-        if ((String(c).indexOf("=")!=-1)&&(ReceiveState==1)) cmdState=0;
-        if ((cmdState==1)&&(String(c).indexOf("?")==-1)) cmd=cmd+String(c);
+        if ((c=='=')&&(ReceiveState==1)) cmdState=0;
+        if ((cmdState==1)&&(c!='?')) cmd=cmd+String(c);
 
-        if ((String(c).indexOf("=")!=-1)&&(ReceiveState==1)&&(num2State==0)) num1State=1;
-        if (((String(c).indexOf(",")!=-1)||(String(c).indexOf(" ")!=-1))&&(ReceiveState==1)) num1State=0;
-        if ((num1State==1)&&(String(c).indexOf("=")==-1))
+        if ((c=='=')&&(ReceiveState==1)&&(str2State==0)) str1State=1;
+        if (((c==';')||(c==' '))&&(ReceiveState==1)) str1State=0;
+        if ((str1State==1)&&(c!='='))
             str1=str1+String(c);
         
-        if ((String(c).indexOf(",")!=-1)&&(ReceiveState==1)) num2State=1;
-        if ((String(c).indexOf(" ")!=-1)&&(ReceiveState==1)) num2State=0;
-        if ((num2State==1)&&(String(c).indexOf(",")==-1))
+        if ((c==';')&&(ReceiveState==1)) str2State=1;
+        if ((c==' ')&&(ReceiveState==1)) str2State=0;
+        if ((str2State==1)&&(c!=';'))
             str2=str2+String(c);
-        else if ((num1State==1)&&(c=='=')&&(equalstate==1))
+        else if ((str1State==1)&&(c=='=')&&(equalstate==1))
           str1=str1+String(c); 
-        else if ((num2State==1)&&(c==',')&&(commastate==1))
+        else if ((str2State==1)&&(c==';')&&(commastate==1))
           str2=str2+String(c); 
           
-        if (num1State==1) equalstate=1;
-        if (num2State==1) commastate=1;
+        if (str1State==1) equalstate=1;
+        if (str2State==1) commastate=1;
       }
     }  
     Serial.println(ReceiveData);
