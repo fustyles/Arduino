4a7416cd86b1e49cf608b253a0fe166386616fc2
fustyles <34397289+fustyles@users.noreply.github.com>
2018-02-21 17:27:22 +0800

    Update Bluetooth_MyFirmata.ino

--- Bluetooth_MyFirmata.ino
+++ Bluetooth_MyFirmata.ino
@@ -1,7 +1,7 @@
 /*
 Bluetooth
-Author : ChungYi Fu (Kaohsiung, Taiwan)  2018-2-20 00:00 
-Command format :  ?cmd = str1;str2;str3
+Author : ChungYi Fu (Kaohsiung, Taiwan)  2018-2-21 17:30 
+Command format :  ?cmd = str1;str2;str3;str4;str5;str6;str7;str8;str9
 ?inputpullup=3
 ?pinmode=3;1
 ?digitalwrite=3;1
@@ -14,7 +14,7 @@ Command format :  ?cmd = str1;str2;str3
 #include <SoftwareSerial.h>
 SoftwareSerial mySerial(10, 11); // Arduino RX:10, TX:11 
 
-String ReceiveData="", command="",cmd="",str1="",str2="",str3="";
+String ReceiveData="", command="",cmd="",str1="",str2="",str3="",str4="",str5="",str6="",str7="",str8="",str9="";
 
 void setup()
 {
@@ -31,12 +31,12 @@ void loop()
     Serial.println("");
     Serial.println("command: "+command);
     Serial.println("cmd: "+cmd);
-    Serial.println("str1= "+str1+" ,str2= "+str2+" ,str3= "+str3);
+    Serial.println("cmd= "+cmd+" ,str1= "+str1+" ,str2= "+str2+" ,str3= "+str3+" ,str4= "+str4+" ,str5= "+str5+" ,str6= "+str6+" ,str7= "+str7+" ,str8= "+str8+" ,str9= "+str9);
     
     if (cmd=="yourcmd")
       {
         //you can do anything
-        //SendData(cmd+"="+str1+";"+str2+";"+str3);
+        //SendData(cmd+"="+str1+";"+str2);
       } 
     else if (cmd=="inputpullup")
       {
@@ -80,8 +80,8 @@ void SendData(String data)
 
 void getVariable()
 {
-  ReceiveData="";command="";cmd="";str1="";str2="";str3="";
-  byte ReceiveState=0,cmdState=1,str1State=0,str2State=0,str3State=0,equalstate=0,semicolonstate=0;
+  ReceiveData="";command="";cmd="";str1="";str2="";str3="";str4="";str5="";str6="";str7="";str8="";str9="";
+  byte ReceiveState=0,cmdState=1,strState=1,questionstate=0,equalstate=0,semicolonstate=0;
   
   if (mySerial.available())
   {
@@ -97,30 +97,23 @@ void getVariable()
       {
         command=command+String(c);
         
-        if ((c=='=')&&(str1State==0)&&(str2State==0)&&(str3State==0))
-        {
-          cmdState=0;str1State=1;str2State=0;str3State=0;
-        }
-        else if ((c==';')&&(str1State==1)&&(str2State==0)&&(str3State==0)) 
-        {
-          cmdState=0;str1State=0;str2State=1;str3State=0;
-        }
-        else if ((c==';')&&(str1State==0)&&(str2State==1)&&(str3State==0)) 
-        {
-          cmdState=0;str1State=0;str2State=0;str3State=1;
-        }       
-        else if (c==' ')
-        {
-          cmdState=0;str1State=0;str2State=0;str3State=0;
-        }
+        if (c=='=') cmdState=0;
+        if (c==';') strState++;
 
-        if ((cmdState==1)&&(c!='?')) cmd=cmd+String(c);
-        if ((str1State==1)&&((c!='=')||(equalstate==1))) str1=str1+String(c);
-        if ((str2State==1)&&(c!=';')) str2=str2+String(c);
-        if ((str3State==1)&&((c!=';')||(semicolonstate==1))) str3=str3+String(c);
+        if ((cmdState==1)&&((c!='?')||(questionstate==1))) cmd=cmd+String(c);
+        if ((cmdState==0)&&(strState==1)&&((c!='=')||(equalstate==1))) str1=str1+String(c);
+        if ((cmdState==0)&&(strState==2)&&(c!=';')) str2=str2+String(c);
+        if ((cmdState==0)&&(strState==3)&&(c!=';')) str3=str3+String(c);
+        if ((cmdState==0)&&(strState==4)&&(c!=';')) str4=str4+String(c);
+        if ((cmdState==0)&&(strState==5)&&(c!=';')) str5=str5+String(c);
+        if ((cmdState==0)&&(strState==6)&&(c!=';')) str6=str6+String(c);
+        if ((cmdState==0)&&(strState==7)&&(c!=';')) str7=str7+String(c);
+        if ((cmdState==0)&&(strState==8)&&(c!=';')) str8=str8+String(c);
+        if ((cmdState==0)&&(strState>=9)&&((c!=';')||(semicolonstate==1))) str9=str9+String(c);
         
-        if (str1State==1) equalstate=1;
-        if (str3State==1) semicolonstate=1;
+        if (c=='?') questionstate=1;
+        if (c=='=') equalstate=1;
+        if ((strState>=9)&&(c==';')) semicolonstate=1;
       }
     }  
     Serial.println(ReceiveData);
