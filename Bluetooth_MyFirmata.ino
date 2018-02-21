b78ac702ced1fa7de20ffc7ccb9132ba184dcbca
fustyles <34397289+fustyles@users.noreply.github.com>
2018-02-21 17:27:40 +0800

    Update Bluetooth_MyFirmata.ino

--- Bluetooth_MyFirmata.ino
+++ Bluetooth_MyFirmata.ino
@@ -1,7 +1,7 @@
 /*
 Bluetooth
 Author : ChungYi Fu (Kaohsiung, Taiwan)  2018-2-21 17:30 
-Command format :  ?cmd = str1;str2;str3;str4;str5;str6;str7;str8;str9
+Command format :  ?cmd=str1;str2;str3;str4;str5;str6;str7;str8;str9
 ?inputpullup=3
 ?pinmode=3;1
 ?digitalwrite=3;1
