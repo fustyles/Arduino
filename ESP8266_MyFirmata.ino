2d6a820c5502835861b81e6ebcde4ff1b16d3a1c
fustyles <34397289+fustyles@users.noreply.github.com>
2018-02-25 11:51:34 +0800

    Update ESP8266_MyFirmata.ino

--- ESP8266_MyFirmata.ino
+++ ESP8266_MyFirmata.ino
@@ -117,7 +117,7 @@ void setup()
 {
   Serial.begin(9600);
   
-  //You must change uart baud rate of ESP-01(S) to 9600.
+  //You must change uart baud rate of ESP-01 to 9600.
   mySerial.begin(115200);   //Default uart baud rate -> 19200,38400,57600,74880,115200
   SendData("AT+UART_CUR=9600,8,1,0,0",2000);   //Change uart baud rate of ESP-01 to 9600
   mySerial.begin(9600);  // 9600 ,you will get more stable data.
