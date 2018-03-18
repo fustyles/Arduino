f7f96ec8ffc0da2d41c419e2c8b7e8359c6ecfd4
fustyles <34397289+fustyles@users.noreply.github.com>
2018-03-18 21:28:15 +0800

    Update ESP32_MyFirmata.ino

--- ESP32_MyFirmata.ino
+++ ESP32_MyFirmata.ino
@@ -48,10 +48,10 @@ void ExecuteCommand()
   Serial.println("cmd= "+cmd+" ,str1= "+str1+" ,str2= "+str2+" ,str3= "+str3+" ,str4= "+str4+" ,str5= "+str5+" ,str6= "+str6+" ,str7= "+str7+" ,str8= "+str8+" ,str9= "+str9);
   Serial.println("");
   
-  if (cmd=="your command")
+  if (cmd=="your cmd")
   {
     // You can do anything
-    // Feedback="<font color=red>Hello World</font>";
+    // Feedback="<font color=\"red\">Hello World</font>";
   }
   else if (cmd=="ip")
   {
