ff26016a7562bca04c48d801b5eeb9f6d47f1af6
fustyles <34397289+fustyles@users.noreply.github.com>
2018-03-19 21:44:13 +0800

    Update ESP01_MyFirmata.ino

--- ESP01_MyFirmata.ino
+++ ESP01_MyFirmata.ino
@@ -26,6 +26,9 @@ http://192.168.4.1/?car=pinL1;pinL2;pinR1;pinR2;L_speed;R_speed;Delay;state
 STAIP：
 Query： http://192.168.4.1/?ip
 Link：http://192.168.4.1/?resetwifi=id;pwd
+
+Control Page
+https://github.com/fustyles/webduino/blob/master/ESP8266_MyFirmata.html
 */
 
 // Check your Wi-Fi Router's Settings
