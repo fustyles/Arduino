73d2f9af7978e6de6e3abb54720fa37bd9871c36
fustyles <34397289+fustyles@users.noreply.github.com>
2018-03-30 01:20:09 +0800

    Update ESP32_MyFirmata.ino

--- ESP32_MyFirmata.ino
+++ ESP32_MyFirmata.ino
@@ -1,7 +1,7 @@
 /* 
 Arduino IDE + NodeMCU (ESP32)
 
-Author : ChungYi Fu (Taiwan)  2018-3-30 00:30
+Author : ChungYi Fu (Taiwan)  2018-3-30 01:30
 
 Command Format :  ?cmd=str1;str2;str3;str4;str5;str6;str7;str8;str9
 
