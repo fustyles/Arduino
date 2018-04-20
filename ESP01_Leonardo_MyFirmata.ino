436485e3a98f89c8eb1ffbc86fa629f732ecef5d
fustyles <34397289+fustyles@users.noreply.github.com>
2018-04-20 21:56:07 +0800

    Update ESP01_Leonardo_MyFirmata.ino

--- ESP01_Leonardo_MyFirmata.ino
+++ ESP01_Leonardo_MyFirmata.ino
@@ -324,10 +324,11 @@ String WaitReply(long int TimeLimit)
       delay(4);
       while(mySerial.available())
       {
-        ReceiveData=ReceiveData+String(char(mySerial.read()));
-        //ReceiveData=ReceiveData+mySerial.readStringUntil('\r'); 
+        //ReceiveData=ReceiveData+String(char(mySerial.read()));
+        ReceiveData=ReceiveData+mySerial.readStringUntil('\r'); 
       }
-      return ReceiveData;
+      //Serial.println(ReceiveData);
+      if (ReceiveData.indexOf("OK")!=-1) return ReceiveData;
     }
   } 
   return ReceiveData;
