c51057d904021d122b6c7ba777d6acdb9ff46ca2
fustyles <34397289+fustyles@users.noreply.github.com>
2018-04-20 08:55:51 +0800

    Update Bluetooth_MyFirmata.ino

--- Bluetooth_MyFirmata.ino
+++ Bluetooth_MyFirmata.ino
@@ -3,7 +3,8 @@ Arduino Uno(Uart) + Bluetooth
 
 Author : ChungYi Fu (Kaohsiung, Taiwan)  2018-3-19 09:30 
 
-Command Format : ?cmd=str1;str2;str3;str4;str5;str6;str7;str8;str9
+Command Format : 
+?cmd=str1;str2;str3;str4;str5;str6;str7;str8;str9
 
 ?inputpullup=pin
 ?pinmode=pin;value
