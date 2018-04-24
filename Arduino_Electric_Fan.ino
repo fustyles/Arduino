a27428006468f6af6bff5bbb1f43d2d7d1b6f3ff
fustyles <34397289+fustyles@users.noreply.github.com>
2018-04-24 23:54:21 +0800

    Add files via upload

---
+++ Arduino_Electric_Fan.ino
@@ -0,0 +1,75 @@
+#include <Servo.h>
+Servo myservo;
+
+int angle=90;          //風向初始角度
+int degree=5;          //單位時間風向旋轉角度
+
+int motorPin1=5;       //馬達驅動IC腳位
+int motorPin2=6;       //馬達驅動IC腳位
+int speedButton = 7;   //風速四段變速切換按鈕腳位(吊扇)
+int rotateButton = 8;  //風向旋轉切換按鈕腳位
+
+int pressCount=0;      //風速切換按鈕點選次數
+int rotateState=-1;    //風向旋轉狀態 1=旋轉, -1=暫停
+
+void setup() {
+  pinMode(speedButton, INPUT);
+  pinMode(rotateButton, INPUT);
+  pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin2, OUTPUT);    
+  
+  myservo.attach(9);
+  myservo.write(angle);
+  
+  Serial.begin(9600);
+}
+
+void loop() {
+  //讀取風速切換按鈕狀態
+  if (digitalRead(speedButton)==1)
+  {
+    pressCount++;
+    if (pressCount%4==0)         
+    {
+      analogWrite(motorPin1,0);
+      analogWrite(motorPin2,0);
+      pressCount=0;
+    }
+    else if (pressCount%4==1) 
+    {
+      analogWrite(motorPin1,100);
+      analogWrite(motorPin2,0);
+    }
+    else if (pressCount%4==2) 
+    {
+      analogWrite(motorPin1,120);
+      analogWrite(motorPin2,0);
+    }
+    else if (pressCount%4==3) 
+    {
+      analogWrite(motorPin1,140);
+      analogWrite(motorPin2,0);
+    }
+    delay(200);
+  }
+  
+  //讀取風向旋轉切換按鈕狀態
+  if (digitalRead(rotateButton)==1)
+  {
+    rotateState*=(-1);     //改變風向旋轉狀態設定值
+    delay(200);
+  }
+  
+  if (rotateState==1)      //風向來回轉動
+  {
+    angle+=degree;
+    if ((angle<5)||(angle>175))
+    {
+      degree*=(-1);
+      angle+=degree*2;
+    }
+    myservo.write(angle);
+    delay(200);
+  }
+}
+
