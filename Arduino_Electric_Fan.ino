11ba8e8353f58dfcb56da23660a5a531e4c8dbc2
fustyles <34397289+fustyles@users.noreply.github.com>
2018-04-27 21:50:34 +0800

    Update Arduino_Electric_Fan.ino

--- Arduino_Electric_Fan.ino
+++ Arduino_Electric_Fan.ino
@@ -8,6 +8,7 @@ int motorPin1=5;       //馬達驅動IC腳位
 int motorPin2=6;       //馬達驅動IC腳位
 int speedButton = 7;   //風速四段變速切換按鈕腳位(吊扇)
 int rotateButton = 8;  //風向旋轉切換按鈕腳位
+int ServoPin=9;        //伺服馬達腳位
 
 int pressCount=0;      //風速切換按鈕點選次數
 int rotateState=-1;    //風向旋轉狀態 1=旋轉, -1=暫停
@@ -18,7 +19,7 @@ void setup() {
   pinMode(motorPin1, OUTPUT);
   pinMode(motorPin2, OUTPUT);    
   
-  myservo.attach(9);
+  myservo.attach(ServoPin);
   myservo.write(angle);
   
   Serial.begin(9600);
