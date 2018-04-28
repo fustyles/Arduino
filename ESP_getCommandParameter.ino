56bf1d9442d2555245810ac1c16b0f45ed062603
fustyles <34397289+fustyles@users.noreply.github.com>
2018-04-28 18:18:26 +0800

    Add files via upload

---
+++ ESP_getCommandParameter.ino
@@ -0,0 +1,184 @@
+/* 
+ESP 
+
+Author : ChungYi Fu (Taiwan)  2018-04-28 18:30
+
+Command Format :  
+http://APIP/?cmd=str1;str2;str3;str4;str5;str6;str7;str8;str9
+http://STAIP/?cmd=str1;str2;str3;str4;str5;str6;str7;str8;str9
+*/
+
+#include <ESP8266WiFi.h>
+
+const char* ssid     = "";   //your network SSID
+const char* password = "";   //your network password
+
+const char* apssid = "MyFirmata ESP01";
+const char* appassword = "12345678";         //AP password require at least 8 characters.
+
+WiFiServer server(80);
+
+String Feedback="", Command="",cmd="",str1="",str2="",str3="",str4="",str5="",str6="",str7="",str8="",str9="";
+byte ReceiveState=0,cmdState=1,strState=1,questionstate=0,equalstate=0,semicolonstate=0;
+
+void ExecuteCommand()
+{
+  Serial.println("");
+  Serial.println("Command: "+Command);
+  Serial.println("cmd= "+cmd+" ,str1= "+str1+" ,str2= "+str2+" ,str3= "+str3+" ,str4= "+str4+" ,str5= "+str5+" ,str6= "+str6+" ,str7= "+str7+" ,str8= "+str8+" ,str9= "+str9);
+  Serial.println("");
+  
+  if (cmd=="yourcmd")
+  {
+    // You can do anything
+    Feedback="<font color=\"red\">Hello World</font>";
+  }
+  else 
+  {
+    Feedback="Command is not defined";
+  }
+}
+
+void setup()
+{
+    Serial.begin(115200);
+    delay(10);
+    
+    WiFi.mode(WIFI_AP_STA);
+    
+    WiFi.softAP(apssid, appassword);
+  
+    //WiFi.softAPConfig(IPAddress(192, 168, 4, 1), IPAddress(192, 168, 4, 1), IPAddress(255, 255, 255, 0));
+  
+    delay(1000);
+    Serial.println("");
+    Serial.println("APIP address: ");
+    Serial.println(WiFi.softAPIP());  
+  
+    //WiFi.config(IPAddress(192, 168, 201, 100), IPAddress(192, 168, 201, 2), IPAddress(255, 255, 255, 0));
+
+    WiFi.begin(ssid, password);
+  
+    delay(1000);
+    Serial.println("");
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+    
+    long int StartTime=millis();
+    while (WiFi.status() != WL_CONNECTED) 
+    {
+        delay(500);
+        if ((StartTime+5000) < millis()) break;
+    } 
+  
+    if (WiFi.localIP().toString()!="0.0.0.0")
+    {
+      pinMode(2, OUTPUT);
+      for (int i=0;i<5;i++)
+      {
+        digitalWrite(2,LOW);
+        delay(100);
+        digitalWrite(2,HIGH);
+        delay(100);
+      }
+    }  
+
+    Serial.println("");
+    Serial.println("STAIP address: ");
+    Serial.println(WiFi.localIP());
+    
+    server.begin();
+}
+
+void loop()
+{
+  Command="";cmd="";str1="";str2="";str3="";str4="";str5="";str6="";str7="";str8="";str9="";
+  ReceiveState=0,cmdState=1,strState=1,questionstate=0,equalstate=0,semicolonstate=0;
+
+  WiFiClient client = server.available();
+
+  if (client) 
+  { 
+    String currentLine = "";
+
+    while (client.connected()) 
+    {
+      if (client.available()) 
+      {
+        char c = client.read();             
+        
+        getCommand(c);
+                
+        if (c == '\n') 
+        {
+          if (currentLine.length() == 0) 
+          {
+            client.println("HTTP/1.1 200 OK");
+            client.println("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+            client.println("Access-Control-Allow-Methods: GET,POST,PUT,DELETE,OPTIONS");
+            client.println("Content-Type: text/html; charset=utf-8");
+            client.println("Access-Control-Allow-Origin: *");
+            //client.println("Connection: close");
+            client.println();
+            client.println("<!DOCTYPE HTML>");
+            client.println("<html><head>");
+            client.println("<meta charset=\"UTF-8\">");
+            client.println("<meta http-equiv=\"Access-Control-Allow-Origin\" content=\"*\">");
+            client.println("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
+            client.println("</head><body>");
+            client.println(Feedback);
+            client.println("</body></html>");
+            client.println();
+                        
+            Feedback="";
+            break;
+          } else {
+            currentLine = "";
+          }
+        } 
+        else if (c != '\r') 
+        {
+          currentLine += c;
+        }
+
+        if ((currentLine.indexOf("/?")!=-1)&&(currentLine.indexOf(" HTTP")!=-1))
+        {
+          currentLine="";
+          Feedback="";
+          ExecuteCommand();
+        }
+      }
+    }
+    delay(1);
+    client.stop();
+  }
+}
+
+void getCommand(char c)
+{
+  if (c=='?') ReceiveState=1;
+  if ((c==' ')||(c=='\r')||(c=='\n')) ReceiveState=0;
+  
+  if (ReceiveState==1)
+  {
+    Command=Command+String(c);
+    
+    if (c=='=') cmdState=0;
+    if (c==';') strState++;
+  
+    if ((cmdState==1)&&((c!='?')||(questionstate==1))) cmd=cmd+String(c);
+    if ((cmdState==0)&&(strState==1)&&((c!='=')||(equalstate==1))) str1=str1+String(c);
+    if ((cmdState==0)&&(strState==2)&&(c!=';')) str2=str2+String(c);
+    if ((cmdState==0)&&(strState==3)&&(c!=';')) str3=str3+String(c);
+    if ((cmdState==0)&&(strState==4)&&(c!=';')) str4=str4+String(c);
+    if ((cmdState==0)&&(strState==5)&&(c!=';')) str5=str5+String(c);
+    if ((cmdState==0)&&(strState==6)&&(c!=';')) str6=str6+String(c);
+    if ((cmdState==0)&&(strState==7)&&(c!=';')) str7=str7+String(c);
+    if ((cmdState==0)&&(strState==8)&&(c!=';')) str8=str8+String(c);
+    if ((cmdState==0)&&(strState>=9)&&((c!=';')||(semicolonstate==1))) str9=str9+String(c);
+    
+    if (c=='?') questionstate=1;
+    if (c=='=') equalstate=1;
+    if ((strState>=9)&&(c==';')) semicolonstate=1;
+  }
+}
