a7931bae11a1d245e0e5757e10ca4585311369c8
fustyles <34397289+fustyles@users.noreply.github.com>
2018-05-03 01:12:43 +0800

    Update ESP12E_UartWifi_MyFirmata.ino.ino

--- ESP12E_UartWifi_MyFirmata.ino.ino
+++ ESP12E_UartWifi_MyFirmata.ino.ino
@@ -392,7 +392,7 @@ void tcp(String domain,String request,int port)
       Feedback="";
       boolean state = false;
       long StartTime = millis();
-      while ((StartTime+3000) > millis())
+      while ((StartTime+4000) > millis())
       {
         while (client_tcp.available()) 
         {
@@ -421,7 +421,7 @@ Arduino Uno
 Uart Command Format:
 ?cmd=str1;str2;str3;str4;str5;str6;str7;str8;str9
 
-?tcp=domain;port;request;waitstate
+?tcp=domain;port;request
 ?ifttt=event;key;value1;value2;value3
 ?thingspeakupdate=key;field1;field2;field3;field4;field5;field6;field7;field8
 
