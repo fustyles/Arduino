84ac9a17d41fa168ba72a390aa14c697382c7712
fustyles <34397289+fustyles@users.noreply.github.com>
2018-05-06 17:14:51 +0800

    Update ESP32_BleWifi_MyFirmata.ino

--- ESP32_BleWifi_MyFirmata.ino
+++ ESP32_BleWifi_MyFirmata.ino
@@ -1,7 +1,7 @@
 /* 
 NodeMCU (ESP32)
  
-Author : ChungYi Fu (Taiwan)  2018-05-06 13:00
+Author : ChungYi Fu (Taiwan)  2018-05-06 17:00
 
 Wifi Command Format :  
 http://APIP/?cmd=str1;str2;str3;str4;str5;str6;str7;str8;str9
@@ -214,13 +214,16 @@ void setup()
     Serial.println("STAIP address: ");
     Serial.println(WiFi.localIP());
 
-    //WiFi.softAP(apssid, appassword);
-    WiFi.softAP((WiFi.localIP().toString()+"_"+(String)apssid).c_str(), appassword);
+    if (WiFi.localIP().toString()!="0.0.0.0")
+      WiFi.softAP((WiFi.localIP().toString()+"_"+(String)apssid).c_str(), appassword);
+    else
+      WiFi.softAP(apssid, appassword);
+      
     //WiFi.softAPConfig(IPAddress(192, 168, 4, 1), IPAddress(192, 168, 4, 1), IPAddress(255, 255, 255, 0)); 
     Serial.println("");
     Serial.println("APIP address: ");
     Serial.println(WiFi.softAPIP());    
-    server.begin();  
+    server.begin();   
 }
 
 void loop()
