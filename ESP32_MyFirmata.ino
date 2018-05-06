a317f123edda2096d2c8483d23374543e1b0c074
fustyles <34397289+fustyles@users.noreply.github.com>
2018-05-06 12:58:21 +0800

    Update ESP32_MyFirmata.ino

--- ESP32_MyFirmata.ino
+++ ESP32_MyFirmata.ino
@@ -84,8 +84,11 @@ void ExecuteCommand()
     } 
     Serial.println("");
     Serial.println("STAIP: "+WiFi.localIP().toString());
-    WiFi.softAP((WiFi.localIP().toString()+"_"+(String)apssid).c_str(), appassword);
-    server.begin();
+    if (WiFi.localIP().toString()!="0.0.0.0") 
+    {
+      WiFi.softAP((WiFi.localIP().toString()+"_"+(String)apssid).c_str(), appassword);
+      server.begin();
+    }
     Feedback="STAIP: "+WiFi.localIP().toString();
   }    
   else if (cmd=="inputpullup")
