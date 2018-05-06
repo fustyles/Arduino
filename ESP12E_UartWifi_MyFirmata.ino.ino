20b8559456437b50bbe7a455f78804916dbb69b2
fustyles <34397289+fustyles@users.noreply.github.com>
2018-05-06 14:56:59 +0800

    Update ESP12E_UartWifi_MyFirmata.ino.ino

--- ESP12E_UartWifi_MyFirmata.ino.ino
+++ ESP12E_UartWifi_MyFirmata.ino.ino
@@ -1,11 +1,15 @@
 /* 
 Arduino Uno + NodeMCU (ESP12E) (without using AT Command)
+
 Author : ChungYi Fu (Taiwan)  2018-05-06 14:00
+
 Wifi Command Format :  
 http://APIP/?cmd=str1;str2;str3;str4;str5;str6;str7;str8;str9
 http://STAIP/?cmd=str1;str2;str3;str4;str5;str6;str7;str8;str9
+
 Uart Command Format : 
 ?cmd=str1;str2;str3;str4;str5;str6;str7;str8;str9
+
 Default APIP： 192.168.4.1
 http://192.168.4.1/?ip
 http://192.168.4.1/?mac
@@ -20,9 +24,11 @@ http://192.168.4.1/?analogread=pin
 http://192.168.4.1/?tcp=domain;port;request
 http://192.168.4.1/?ifttt=event;key;value1;value2;value3
 http://192.168.4.1/?thingspeakupdate=key;field1;field2;field3;field4;field5;field6;field7;field8
+
 STAIP：
 Query： http://192.168.4.1/?ip
 Link：http://192.168.4.1/?resetwifi=ssid;password
+
 Control Page (http)
 https://github.com/fustyles/webduino/blob/master/ESP8266_MyFirmata.html
 */
