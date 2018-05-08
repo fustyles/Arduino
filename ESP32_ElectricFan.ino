a003463002d4a33337414a1429b0b4c091816dd9
fustyles <34397289+fustyles@users.noreply.github.com>
2018-05-08 19:58:15 +0800

    Add files via upload

---
+++ ESP32_ElectricFan.ino
@@ -0,0 +1,237 @@
+/* 
+ESP32 Electric Fan
+
+Author : ChungYi Fu (Taiwan)  2018-05-08 20:00
+
+Command Format :  
+http://APIP/?cmd=str1;str2;str3;str4;str5;str6;str7;str8;str9
+http://STAIP/?cmd=str1;str2;str3;str4;str5;str6;str7;str8;str9
+
+STAIP：
+http://192.168.4.1/?resetwifi=ssid;password
+*/
+
+#include <WiFi.h>
+
+const char* ssid     = "";   //your network SSID
+const char* password = "";   //your network password
+
+const char* apssid = "ESP32 ElectricFan";
+const char* appassword = "12345678";         //AP password require at least 8 characters.
+
+WiFiServer server(80);
+
+#include "esp32-hal-ledc.h"
+
+int angle=90;           //風向初始角度
+int degree=5;           //單位時間風向旋轉角度
+int servoPin=12;        //伺服馬達腳位
+int motorPin1=14;       //馬達驅動IC腳位
+int motorPin2=27;       //馬達驅動IC腳位
+int rotateState=0;      //風向旋轉狀態 1=旋轉, 0=暫停
+int rotateInterval=500;
+int speedValue=150;
+
+String Feedback="", Command="",cmd="",str1="",str2="",str3="",str4="",str5="",str6="",str7="",str8="",str9="";
+byte ReceiveState=0,cmdState=1,strState=1,questionstate=0,equalstate=0,semicolonstate=0;
+
+void ExecuteCommand()
+{
+  Serial.println("");
+  Serial.println("Command: "+Command);
+  Serial.println("cmd= "+cmd+" ,str1= "+str1+" ,str2= "+str2+" ,str3= "+str3+" ,str4= "+str4+" ,str5= "+str5+" ,str6= "+str6+" ,str7= "+str7+" ,str8= "+str8+" ,str9= "+str9);
+  Serial.println("");
+  
+  if (cmd=="rotate")
+  {
+    rotateState=str1.toInt();
+  }  
+  else if (cmd=="speed")
+  {
+    speedValue=str1.toInt();
+    ledcWrite(2,0);    
+    ledcWrite(3,str1.toInt());
+  }  
+  else if (cmd=="resetwifi")
+  {
+    WiFi.begin(str1.c_str(), str2.c_str());
+    Serial.print("Connecting to ");
+    Serial.println(str1);
+    long int StartTime=millis();
+    while (WiFi.status() != WL_CONNECTED) 
+    {
+        delay(500);
+        if ((StartTime+10000) < millis()) break;
+    } 
+    Serial.println("");
+    Serial.println("STAIP: "+WiFi.localIP().toString());
+    Feedback="STAIP: "+WiFi.localIP().toString();
+  }    
+  else 
+  {
+    Feedback="Command is not defined";
+  }
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(10);
+  
+  WiFi.mode(WIFI_AP_STA);
+  
+  //WiFi.config(IPAddress(192, 168, 201, 100), IPAddress(192, 168, 201, 2), IPAddress(255, 255, 255, 0));
+  WiFi.begin(ssid, password);
+  delay(1000);
+  Serial.println("");
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  long int StartTime=millis();
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+      delay(500);
+      if ((StartTime+5000) < millis()) break;
+  } 
+  Serial.println("");
+  Serial.println("STAIP address: ");
+  Serial.println(WiFi.localIP());
+  
+  if (WiFi.localIP().toString()!="0.0.0.0")
+    WiFi.softAP((WiFi.localIP().toString()+"_"+(String)apssid).c_str(), appassword);
+  else
+    WiFi.softAP(apssid, appassword);
+    
+  //WiFi.softAPConfig(IPAddress(192, 168, 4, 1), IPAddress(192, 168, 4, 1), IPAddress(255, 255, 255, 0)); 
+  Serial.println("");
+  Serial.println("APIP address: ");
+  Serial.println(WiFi.softAPIP());    
+  server.begin(); 
+
+  pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin2, OUTPUT);    
+  
+  ledcSetup(1, 50, 16); // channel 1, 50 Hz, 16-bit width
+  ledcAttachPin(servoPin, 1);   // GPIO 22 assigned to channel 1
+  ledcWrite(1, map(angle, 0, 180, 1638, 7864));
+
+  ledcAttachPin(motorPin1, 2);
+  ledcSetup(2, 5000, 8);
+  ledcAttachPin(motorPin2, 3);
+  ledcSetup(3, 5000, 8);
+}
+
+void loop()
+{
+  Command="";cmd="";str1="";str2="";str3="";str4="";str5="";str6="";str7="";str8="";str9="";
+  ReceiveState=0,cmdState=1,strState=1,questionstate=0,equalstate=0,semicolonstate=0;
+
+  WiFiClient client = server.available();
+
+  if (client) 
+  { 
+    String currentLine = "";
+
+    while (client.connected()) 
+    {
+      if (client.available()) 
+      {
+        char c = client.read();             
+        
+        getCommand(c);
+                
+        if (c == '\n') 
+        {
+          if (currentLine.length() == 0) 
+          {
+            client.println("HTTP/1.1 200 OK");
+            client.println("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+            client.println("Access-Control-Allow-Methods: GET,POST,PUT,DELETE,OPTIONS");
+            client.println("Content-Type: text/html; charset=utf-8");
+            client.println("Access-Control-Allow-Origin: *");
+            //client.println("Connection: close");
+            client.println();
+            client.println("<!DOCTYPE HTML>");
+            client.println("<html><head>");
+            client.println("<meta charset=\"UTF-8\">");
+            client.println("<meta http-equiv=\"Access-Control-Allow-Origin\" content=\"*\">");
+            client.println("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
+            client.println("</head><body><form>");
+            client.println(Feedback);
+            client.println("<br/><br/>");
+            client.println("Rotate: ");
+            client.println("<input type=\"button\" onclick=\"location.href='?rotate=1'\" value=\"Start\">");
+            client.println("<input type=\"button\" onclick=\"location.href='?rotate=0'\" value=\"Stop\">");
+            client.println("<br/><br/>");
+            client.println("Speed: ");
+            client.println("<input type=\"range\" name=\"pwm\" min=\"0\" max=\"255\" step=\"1\" value=\""+String(speedValue)+"\" onchange=\"set.value='Set '+pwm.value;\">");
+            client.println("<input type=\"button\" onclick=\"location.href='?speed=0'\" value=\"Stop\">");
+            client.println("<input type=\"button\" name=\"set\" onclick=\"location.href='?speed='+pwm.value;\" value=\"Set "+String(speedValue)+"\">");
+            client.println("</form></body></html>");
+            client.println();
+                        
+            Feedback="";
+            break;
+          } else {
+            currentLine = "";
+          }
+        } 
+        else if (c != '\r') 
+        {
+          currentLine += c;
+        }
+
+        if ((currentLine.indexOf("/?")!=-1)&&(currentLine.indexOf(" HTTP")!=-1))
+        {
+          currentLine="";
+          Feedback="";
+          ExecuteCommand();
+        }
+      }
+    }
+    delay(1);
+    client.stop();
+  }
+
+  if (rotateState==1)      //風向來回轉動
+  {
+    angle+=degree;
+    if ((angle<5)||(angle>175))
+    {
+      degree*=(-1);
+      angle+=degree*2;
+    }
+    Serial.println(angle);
+    ledcWrite(1, map(angle, 0, 180, 1638, 7864));
+    delay(rotateInterval);
+  }  
+}
+
+void getCommand(char c)
+{
+  if (c=='?') ReceiveState=1;
+  if ((c==' ')||(c=='\r')||(c=='\n')) ReceiveState=0;
+  
+  if (ReceiveState==1)
+  {
+    Command=Command+String(c);
+    
+    if (c=='=') cmdState=0;
+    if (c==';') strState++;
+  
+    if ((cmdState==1)&&((c!='?')||(questionstate==1))) cmd=cmd+String(c);
+    if ((cmdState==0)&&(strState==1)&&((c!='=')||(equalstate==1))) str1=str1+String(c);
+    if ((cmdState==0)&&(strState==2)&&(c!=';')) str2=str2+String(c);
+    if ((cmdState==0)&&(strState==3)&&(c!=';')) str3=str3+String(c);
+    if ((cmdState==0)&&(strState==4)&&(c!=';')) str4=str4+String(c);
+    if ((cmdState==0)&&(strState==5)&&(c!=';')) str5=str5+String(c);
+    if ((cmdState==0)&&(strState==6)&&(c!=';')) str6=str6+String(c);
+    if ((cmdState==0)&&(strState==7)&&(c!=';')) str7=str7+String(c);
+    if ((cmdState==0)&&(strState==8)&&(c!=';')) str8=str8+String(c);
+    if ((cmdState==0)&&(strState>=9)&&((c!=';')||(semicolonstate==1))) str9=str9+String(c);
+    
+    if (c=='?') questionstate=1;
+    if (c=='=') equalstate=1;
+    if ((strState>=9)&&(c==';')) semicolonstate=1;
+  }
+}
