c284037e2a35f57dd31ce6ddb897f1dfc3fda97e
fustyles <34397289+fustyles@users.noreply.github.com>
2018-05-08 20:03:32 +0800

    Update ESP32_ElectricFan.ino

--- ESP32_ElectricFan.ino
+++ ESP32_ElectricFan.ino
@@ -29,7 +29,7 @@ int servoPin=12;         //伺服馬達腳位
 int motorPin1=14;        //馬達驅動IC腳位
 int motorPin2=27;        //馬達驅動IC腳位
 int rotateState=0;       //風向旋轉狀態 1=旋轉, 0=暫停
-int rotateInterval=500;  //風向旋轉間隔時間
+int rotateInterval=500;  //風向旋轉間隔時間 (ms)
 int speedValue=150;      //初始風速
 
 String Feedback="", Command="",cmd="",str1="",str2="",str3="",str4="",str5="",str6="",str7="",str8="",str9="";
