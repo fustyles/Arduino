bda57d470207870b37f110739095b661dd4bed37
fustyles <34397289+fustyles@users.noreply.github.com>
2018-05-10 14:14:27 +0800

    Update ESP32_ElectricFan.ino

--- ESP32_ElectricFan.ino
+++ ESP32_ElectricFan.ino
@@ -1,6 +1,6 @@
 /* 
 Electric Fan (NodeMCU ESP32)
-You must provide stable and large enough current for ESP.
+You must provide stable and large enough current for ESP32.
 
 Author : ChungYi Fu (Taiwan)  2018-05-09 23:00
 
