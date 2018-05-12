7a5e1600e8fa746f4e606be5d8e50e4305f31cb4
fustyles <34397289+fustyles@users.noreply.github.com>
2018-05-13 00:35:18 +0800

    Update ESP32_Chart.ino

--- ESP32_Chart.ino
+++ ESP32_Chart.ino
@@ -1,6 +1,8 @@
 /* 
-Humidity&Temperature (NodeMCU ESP32)
+Humidity&Temperature Chart (NodeMCU ESP32)
+
 Author : ChungYi Fu (Taiwan)  2018-05-13 00:00
+
 Command Format :  
 http://STAIP/?chartType=LineChart
 http://STAIP/?chartType=AreaChart
