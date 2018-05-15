c0d580cf7ca16f69c87bc8fdf05a187b0d3a4e6c
fustyles <34397289+fustyles@users.noreply.github.com>
2018-05-15 19:58:56 +0800

    Update ESP32_Chart_Google.ino

--- ESP32_Chart_Google.ino
+++ ESP32_Chart_Google.ino
@@ -1,6 +1,6 @@
 /* 
 NodeMCU ESP32 Chart (Google)
-Author : ChungYi Fu (Taiwan)  2018-05-14 19:00
+Author : ChungYi Fu (Taiwan)  2018-05-15 20:00
 Command Format :  
 http://STAIP   (default:LineChart)
 http://STAIP/?chartType=LineChart
@@ -22,8 +22,8 @@ int timeInterval=5000;                 //Sensor time interval (ms)
 int chartWidth=600;                    //Chart width (px)
 int chartHeight=600;                   //Chart height (px)
 String xTitle="Time";                  //Title of the X axis 
-String yTitle1="Temperature(°F )";  //Title of the Y axis 
-String yTitle2="Humidity( % )";  //Title of the Y axis 
+String yTitle1="Temperature(°F )";     //Title of the Y axis 
+String yTitle2="Humidity( % )";        //Title of the Y axis 
 unsigned long time1,time2;
 int count=0;           
 
