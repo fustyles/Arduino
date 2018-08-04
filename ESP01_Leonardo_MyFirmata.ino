8b06187482f03ef73a8b0963813fd4c4c686858a
fustyles <34397289+fustyles@users.noreply.github.com>
2018-08-04 18:10:44 +0800

    Update ESP01_Leonardo_MyFirmata.ino

--- ESP01_Leonardo_MyFirmata.ino
+++ ESP01_Leonardo_MyFirmata.ino
@@ -15,7 +15,7 @@ Expanding Arduino Serial Port Buffer Size
 https://internetofhomethings.com/homethings/?p=927
 
 Control Page
-https://github.com/fustyles/webduino/blob/master/ESP8266_MyFirmata.html
+https://github.com/fustyles/Arduino/blob/master/ESP8266_MyFirmata.html
 
 Keyboard Modifiers (keyboardpress)
 https://www.arduino.cc/en/Reference/KeyboardModifiers
