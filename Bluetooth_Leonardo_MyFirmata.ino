91b821badfbef7d2692d6f3f8e3215f333ec8eff
fustyles <34397289+fustyles@users.noreply.github.com>
2018-08-07 00:16:01 +0800

    Update Bluetooth_Leonardo_MyFirmata.ino

--- Bluetooth_Leonardo_MyFirmata.ino
+++ Bluetooth_Leonardo_MyFirmata.ino
@@ -44,6 +44,8 @@ void executecommand()
   //Serial.println("command: "+command);
   Serial.println("cmd= "+cmd+" ,str1= "+str1+" ,str2= "+str2+" ,str3= "+str3+" ,str4= "+str4+" ,str5= "+str5+" ,str6= "+str6+" ,str7= "+str7+" ,str8= "+str8+" ,str9= "+str9);
   
+  //If you want to execute command quickly, please don't execute "Feedback"!
+  
   if (cmd=="yourcmd")
     {
       //you can do anything
@@ -52,18 +54,18 @@ void executecommand()
   else if (cmd=="inputpullup")
     {
       pinMode(str1.toInt(), INPUT_PULLUP);
-      SendData(command);  //If you want to execute command quickly, don't feedback!
+      SendData(command);
     }  
   else if (cmd=="pinmode")
     {
       pinMode(str1.toInt(), str2.toInt());
-      SendData(command);  //If you want to execute command quickly, don't feedback!
+      SendData(command);
     }        
   else if (cmd=="digitalwrite")
     {
       pinMode(str1.toInt(), OUTPUT);
       digitalWrite(str1.toInt(),str2.toInt());
-      SendData(command);  //If you want to execute command quickly, don't feedback!
+      SendData(command);
     }   
   else if (cmd=="digitalread")
     {
@@ -73,7 +75,7 @@ void executecommand()
     {
       pinMode(str1.toInt(), OUTPUT);
       analogWrite(str1.toInt(),str2.toInt());
-      SendData(command);  //If you want to execute command quickly, don't feedback!
+      SendData(command);
     }       
   else if (cmd=="analogread")
     {
@@ -82,42 +84,42 @@ void executecommand()
   else if (cmd=="mousemove")
     {
       Mouse.move(str1.toInt(), str2.toInt(), str3.toInt());
-      //SendData(command);  //If you want to execute command quickly, don't feedback!
+      //SendData(command);
     }  
   else if (cmd=="mouseclickleft")
     {
       Mouse.click(MOUSE_LEFT);
-      //SendData(command);  //If you want to execute command quickly, don't feedback!
+      //SendData(command);
     } 
   else if (cmd=="mouseclickright")
     {
       Mouse.click(MOUSE_RIGHT);
-      //SendData(command);  //If you want to execute command quickly, don't feedback!
+      //SendData(command);
     } 
   else if (cmd=="mouseclickmiddle")
     {
       Mouse.click(MOUSE_MIDDLE);
-      //SendData(command);  //If you want to execute command quickly, don't feedback!
+      //SendData(command);
     }     
   else if (cmd=="mousepressleft")
     {
       Mouse.press(MOUSE_LEFT);
-      //SendData(command);  //If you want to execute command quickly, don't feedback!
+      //SendData(command);
     } 
   else if (cmd=="mousepressright")
     {
       Mouse.press(MOUSE_RIGHT);
-      //SendData(command);  //If you want to execute command quickly, don't feedback!
+      //SendData(command);
     } 
   else if (cmd=="mousepressmiddle")
     {
       Mouse.press(MOUSE_MIDDLE);
-      //SendData(command);  //If you want to execute command quickly, don't feedback!
+      //SendData(command);
     } 
   else if (cmd=="mouserelease")
     {
       Mouse.release();
-      //SendData(command);  //If you want to execute command quickly, don't feedback!
+      //SendData(command);
     }     
   else if (cmd=="keyboardpress")
     {
@@ -126,22 +128,22 @@ void executecommand()
       if (str3!="") Keyboard.press(char(str3.toInt()));
       delay(str4.toInt());
       Keyboard.releaseAll();
-      //SendData(command);  //If you want to execute command quickly, don't feedback!
+      //SendData(command);
     }  
   else if (cmd=="keyboardprint")
     {
       Keyboard.print(str1);
-      //SendData(command);  //If you want to execute command quickly, don't feedback!
+      //SendData(command);
     }  
   else if (cmd=="keyboardprintln")
     {
       Keyboard.println(str1);
-      //SendData(command);  //If you want to execute command quickly, don't feedback!
+      //SendData(command);
     }  
   else if (cmd=="keyboardwrite")
     {
       Keyboard.write(char(str1.toInt()));
-      //SendData(command);  //If you want to execute command quickly, don't feedback!
+      //SendData(command);
     }      
   else 
     {
