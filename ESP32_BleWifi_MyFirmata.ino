9c2e1855717aa9a1cf26a397429d0579b210b31d
fustyles <34397289+fustyles@users.noreply.github.com>
2018-08-06 17:38:52 +0800

    Update ESP32_BleWifi_MyFirmata.ino

--- ESP32_BleWifi_MyFirmata.ino
+++ ESP32_BleWifi_MyFirmata.ino
@@ -1,7 +1,7 @@
 /* 
 NodeMCU (ESP32)
  
-Author : ChungYi Fu (Taiwan)  2018-05-06 17:00
+Author : ChungYi Fu (Kaohsiung, Taiwan)  2018-08-05 22:00
 
 Wifi Command Format :  
 http://APIP/?cmd=str1;str2;str3;str4;str5;str6;str7;str8;str9
