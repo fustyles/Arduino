79d8d1aaa200cf266a539a80a00badfd1e27e6df
fustyles <34397289+fustyles@users.noreply.github.com>
2018-08-06 17:38:13 +0800

    Update ESP12E_ElectricFan.ino

--- ESP12E_ElectricFan.ino
+++ ESP12E_ElectricFan.ino
@@ -2,7 +2,7 @@
 Electric Fan (NodeMCU ESP12E)
 You must provide stable and large enough current for ESP or the fan will not work properly.
 
-Author : ChungYi Fu (Taiwan)  2018-05-09 23:30
+Author : ChungYi Fu (Kaohsiung, Taiwan)  2018-08-05 22:00
 
 Command Format :  
 http://APIP/?cmd=str1;str2;str3;str4;str5;str6;str7;str8;str9
