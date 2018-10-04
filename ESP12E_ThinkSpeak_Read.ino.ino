93cdc2aaa82a00e8b5ebbb39d324a2aa93be5004
fustyles <34397289+fustyles@users.noreply.github.com>
2018-10-04 23:04:54 +0800

    Add files via upload

---
+++ ESP12E_ThinkSpeak_Read.ino.ino
@@ -0,0 +1,91 @@
+/* 
+NodeMCU (ESP12E)
+Author : ChungYi Fu (Kaohsiung, Taiwan)  2018-10-04 23:00
+*/
+
+#include <ESP8266WiFi.h>     //ESP12E
+
+const char* ssid     = "xxxxx";   //your network SSID
+const char* password = "xxxxx";   //your network password
+
+void setup()
+{
+    Serial.begin(115200);
+    delay(10);
+    
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    delay(1000);
+    Serial.println("");
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+    
+    long int StartTime=millis();
+    while (WiFi.status() != WL_CONNECTED) 
+    {
+        delay(500);
+        if ((StartTime+10000) < millis()) break;
+    } 
+  
+    if (WiFi.localIP().toString()!="0.0.0.0")
+    {
+      pinMode(2, OUTPUT);
+      for (int i=0;i<5;i++)
+      {
+        digitalWrite(2,LOW);
+        delay(100);
+        digitalWrite(2,HIGH);
+        delay(100);
+      }
+    }  
+
+    Serial.println("");
+    Serial.println("STAIP address: ");
+    Serial.println(WiFi.localIP());
+    Serial.println("");
+}
+
+void loop()
+{
+  String data = tcp("api.thingspeak.com","/channels/463224/fields/1.json?results=1",80);
+  Serial.println(data);
+  delay(5000);
+}
+
+String tcp(String domain,String request,int port)
+{
+    WiFiClient client_tcp;
+    
+    if (client_tcp.connect(domain, port)) 
+    {
+      Serial.println("GET " + request);
+      client_tcp.println("GET " + request + " HTTP/1.1");
+      client_tcp.println("Host: " + domain);
+      client_tcp.println("Connection: close");
+      client_tcp.println();
+
+      String getResponse="",Feedback="";
+      boolean state = false;
+      long StartTime = millis();
+      while ((StartTime+5000) > millis())
+      {
+        while (client_tcp.available()) 
+        {
+            char c = client_tcp.read();
+            if (c == '\n') 
+            {
+              if (getResponse.length()==0) state=true; 
+              getResponse = "";
+            } 
+            else if (c != '\r')
+              getResponse += String(c);
+            if (state==true) Feedback += String(c);
+         }
+         if ((state==true)&&(Feedback.length()!= 0)) break;
+      }
+      return Feedback;
+      client_tcp.stop();
+    }
+    else
+      return "Connection failed";  
+}
