39659bbfabc2d40b998e85545270ae8f0a720efd
fustyles <34397289+fustyles@users.noreply.github.com>
2018-10-05 22:04:50 +0800

    Update ESP12E_ThinkSpeak.ino

--- ESP12E_ThinkSpeak.ino
+++ ESP12E_ThinkSpeak.ino
@@ -67,6 +67,18 @@ void loop()
   Serial.println("field1="+field1);
   String field2 = data.substring(data.indexOf("field2,")+7,data.indexOf(",field3"));
   Serial.println("field2="+field2);
+  String field3 = data.substring(data.indexOf("field3,")+7,data.indexOf(",field4"));
+  Serial.println("field3="+field3);
+  String field4 = data.substring(data.indexOf("field4,")+7,data.indexOf(",field5"));
+  Serial.println("field4="+field4);
+  String field5 = data.substring(data.indexOf("field5,")+7,data.indexOf(",field6"));
+  Serial.println("field5="+field5);
+  String field6 = data.substring(data.indexOf("field6,")+7,data.indexOf(",field7"));
+  Serial.println("field6="+field6);
+  String field7 = data.substring(data.indexOf("field7,")+7,data.indexOf(",field8"));
+  Serial.println("field7="+field7);
+  String field8 = data.substring(data.indexOf("field8,")+7,data.length());
+  Serial.println("field8="+field8);  
     
   delay(15000);
 }
