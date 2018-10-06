7cd5b1970b16c612c18e4278e2b3a34053550a99
fustyles <34397289+fustyles@users.noreply.github.com>
2018-10-06 18:55:38 +0800

    Update ESP12E_ThinkSpeak.ino

--- ESP12E_ThinkSpeak.ino
+++ ESP12E_ThinkSpeak.ino
@@ -1,6 +1,6 @@
 /* 
 NodeMCU (ESP12E)
-Author : ChungYi Fu (Kaohsiung, Taiwan)  2018-10-06 17:00
+Author : ChungYi Fu (Kaohsiung, Taiwan)  2018-10-06 19:00
 */
 
 #include <ESP8266WiFi.h>     //ESP12E
@@ -123,7 +123,7 @@ String tcp(String domain,String request,int port,byte wait)
 
       String getResponse="",Feedback="";
       boolean state = false;
-      int waitTime = 2000;   // timeout 2 seconds
+      int waitTime = 3000;   // timeout 3 seconds
       long startTime = millis();
       while ((startTime + waitTime) > millis())
       {
