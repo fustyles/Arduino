47b078341575a5e23fcb21b27a3e4f0d8383c14a
fustyles <34397289+fustyles@users.noreply.github.com>
2018-10-07 10:47:41 +0800

    Update ESP32_UartWifi_MyFirmata.ino

--- ESP32_UartWifi_MyFirmata.ino
+++ ESP32_UartWifi_MyFirmata.ino
@@ -447,11 +447,13 @@ void tcp(String domain,String request,int port)
 
 /*
 Arduino Uno
+
 Uart Command Format:
 ?cmd=str1;str2;str3;str4;str5;str6;str7;str8;str9
-?tcp=domain;port;request
+?tcp=domain;port;request;wait
 ?ifttt=event;key;value1;value2;value3
 ?thingspeakupdate=key;field1;field2;field3;field4;field5;field6;field7;field8
+
 #include <SoftwareSerial.h>
 SoftwareSerial mySerial(10, 11); // NodeMCU(ESP32) RX(gpio16), TX(gpio17)
 void setup()
