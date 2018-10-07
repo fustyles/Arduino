13de4a14f331f89e39915f0c3264d91dc3de0825
fustyles <34397289+fustyles@users.noreply.github.com>
2018-10-07 22:55:38 +0800

    Update ESP_getCommandParameter.ino

--- ESP_getCommandParameter.ino
+++ ESP_getCommandParameter.ino
@@ -15,7 +15,7 @@ http://192.168.4.1/?tcp=domain;port;request;wait
 http://192.168.4.1/?ifttt=event;key;value1;value2;value3
 
 STAIP：
-Query： http://192.168.4.1/?ip
+Query：http://192.168.4.1/?ip
 Link：http://192.168.4.1/?resetwifi=ssid;password
 */
 
@@ -54,7 +54,7 @@ void ExecuteCommand()
   }  
   else if (cmd=="mac")
   {
-    Feedback+="STA MAC: "+WiFi.macAddress();
+    Feedback="STA MAC: "+WiFi.macAddress();
   }  
   else if (cmd=="restart")
   {
