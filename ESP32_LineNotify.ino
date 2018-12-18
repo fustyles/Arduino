df6d99eaf71e0eb254c7b21725be872cca87f51c
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-19 01:58:07 +0800

    Add files via upload

---
+++ ESP32_LineNotify.ino
@@ -0,0 +1,102 @@
+#include <WiFi.h>
+#include "WiFiClientSecure.h"
+
+const char* ssid     = "";   // your network SSID
+const char* password = "";   // your network password
+String token = "";  // LineNotify Token
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  delay(1000);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  long int StartTime=millis();
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+      delay(500);
+      if ((StartTime+10000) < millis()) break;
+  } 
+
+  if (WiFi.localIP().toString()!="0.0.0.0")
+  {
+    pinMode(2, OUTPUT);
+    for (int i=0;i<5;i++)
+    {
+      digitalWrite(2,HIGH);
+      delay(100);
+      digitalWrite(2,LOW);
+      delay(100);
+    }
+      
+    Serial.println();
+    Serial.println("STAIP address: ");
+    Serial.println(WiFi.localIP());
+    Serial.println();     
+  }  
+  else
+     Serial.println("Unable to connect!"); 
+
+  //LineNotify push a message
+  if (WiFi.localIP().toString()!="0.0.0.0") {
+    String request = "message=Hello&stickerPackageId=1&stickerId=2";
+    String Response = LineNotify(token, request, 1);
+    Serial.println(Response);
+  }
+}
+
+void loop()
+{
+}
+
+String LineNotify(String token, String request, byte wait)
+{
+  WiFiClientSecure client_tcp;
+  
+  if (client_tcp.connect("notify-api.line.me", 443)) 
+  {
+    client_tcp.println("POST /api/notify HTTP/1.1");
+    client_tcp.println("Connection: close"); 
+    client_tcp.println("Host: notify-api.line.me");
+    client_tcp.println("User-Agent: ESP8266/1.0");
+    client_tcp.println("Authorization: Bearer " + token);
+    client_tcp.println("Content-Type: application/x-www-form-urlencoded");
+    client_tcp.println("Content-Length: " + String(request.length()));
+    client_tcp.println();
+    client_tcp.println(request);
+    client_tcp.println();
+    
+    String getResponse="",Feedback="";
+    boolean state = false;
+    int waitTime = 3000;   // timeout 3 seconds
+    long startTime = millis();
+    while ((startTime + waitTime) > millis())
+    {
+      while (client_tcp.available()) 
+      {
+          char c = client_tcp.read();
+          if (c == '\n') 
+          {
+            if (getResponse.length()==0) state=true; 
+            getResponse = "";
+          } 
+          else if (c != '\r')
+            getResponse += String(c);
+          if (state==true) Feedback += String(c);
+          if (wait==1)
+            startTime = millis();
+       }
+       if (wait==0)
+        if ((state==true)&&(Feedback.length()!= 0)) break;
+    }
+    client_tcp.stop();
+    return Feedback;
+  }
+  else
+    return "Connection failed";  
+}
