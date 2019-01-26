260cbeb90b81adb4d6561c36eb6457024d30b7ab
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-26 18:31:00 +0800

    Update ESP32_MyBlockly_JSON.ino

--- ESP32_MyBlockly_JSON.ino
+++ ESP32_MyBlockly_JSON.ino
@@ -1,9 +1,9 @@
 /* 
 NodeMCU (ESP32)
-Author : ChungYi Fu (Kaohsiung, Taiwan)  2019-1-26 10:00
+Author : ChungYi Fu (Kaohsiung, Taiwan)  2019-1-26 18:30
 Command Format :  
-http://APIP/?cmd=str1;str2;str3;str4;str5;str6;str7;str8;str9
-http://STAIP/?cmd=str1;str2;str3;str4;str5;str6;str7;str8;str9
+http://APIP/?cmd=P1;P2;P3;P4;P5;P6;P7;P8;P9
+http://STAIP/?cmd=P1;P2;P3;P4;P5;P6;P7;P8;P9
 Default APIP： 192.168.4.1
 http://192.168.4.1/?ip
 http://192.168.4.1/?mac
@@ -21,12 +21,16 @@ http://192.168.4.1/?ifttt=event;key;value1;value2;value3
 http://192.168.4.1/?thingspeakupdate=key;field1;field2;field3;field4;field5;field6;field7;field8
 http://192.168.4.1/?thingspeakread=request
 http://192.168.4.1/?car=pinL1;pinL2;pinR1;pinR2;L_speed;R_speed;Delay;state
+http://192.168.4.1/?i2cLcd=address;pinSDA;pinSCL;text1;text2
 STAIP：
 Query：http://192.168.4.1/?ip
 Link：http://192.168.4.1/?resetwifi=ssid;password
 Control Page (http)
 https://github.com/fustyles/Arduino/blob/master/ESP8266_MyFirmata.html
 */
+
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
 
 #include <WiFi.h>
 #include <WiFiClientSecure.h>
@@ -40,14 +44,14 @@ const char* appassword = "12345678";         //AP password require at least 8 ch
 
 WiFiServer server(80);
 
-String Feedback="", Command="",cmd="",str1="",str2="",str3="",str4="",str5="",str6="",str7="",str8="",str9="";
+String Feedback="", Command="",cmd="",P1="",P2="",P3="",P4="",P5="",P6="",P7="",P8="",P9="";
 byte ReceiveState=0,cmdState=1,strState=1,questionstate=0,equalstate=0,semicolonstate=0;
 
 void ExecuteCommand()
 {
   Serial.println("");
   //Serial.println("Command: "+Command);
-  Serial.println("cmd= "+cmd+" ,str1= "+str1+" ,str2= "+str2+" ,str3= "+str3+" ,str4= "+str4+" ,str5= "+str5+" ,str6= "+str6+" ,str7= "+str7+" ,str8= "+str8+" ,str9= "+str9);
+  Serial.println("cmd= "+cmd+" ,P1= "+P1+" ,P2= "+P2+" ,P3= "+P3+" ,P4= "+P4+" ,P5= "+P5+" ,P6= "+P6+" ,P7= "+P7+" ,P8= "+P8+" ,P9= "+P9);
   Serial.println("");
   
   if (cmd=="your cmd") {
@@ -65,9 +69,9 @@ void ExecuteCommand()
     Feedback="{\"data\":\""+Command+"\"}";
   }    
   else if (cmd=="resetwifi") {
-    WiFi.begin(str1.c_str(), str2.c_str());
+    WiFi.begin(P1.c_str(), P2.c_str());
     Serial.print("Connecting to ");
-    Serial.println(str1);
+    Serial.println(P1);
     long int StartTime=millis();
     while (WiFi.status() != WL_CONNECTED) 
     {
@@ -81,50 +85,51 @@ void ExecuteCommand()
     if (WiFi.localIP().toString()!="0.0.0.0") 
     {
       cmd="ifttt";
-      str1="eventname";
-      str2="key";
-      str3=WiFi.localIP().toString();
+      P1="eventname";
+      P2="key";
+      P3=WiFi.localIP().toString();
       ExecuteCommand();
     }
     */
   }    
   else if (cmd=="inputpullup") {
-    pinMode(str1.toInt(), INPUT_PULLUP);
+    pinMode(P1.toInt(), INPUT_PULLUP);
     Feedback="{\"data\":\""+Command+"\"}";
   }  
   else if (cmd=="pinmode") {
-    if (str2.toInt()==1)
-      pinMode(str1.toInt(), OUTPUT);
+    if (P2.toInt()==1)
+      pinMode(P1.toInt(), OUTPUT);
     else
-      pinMode(str1.toInt(), INPUT);
+      pinMode(P1.toInt(), INPUT);
     Feedback="{\"data\":\""+Command+"\"}";
   }        
   else if (cmd=="digitalwrite") {
-    ledcDetachPin(str1.toInt());
-    pinMode(str1.toInt(), OUTPUT);
-    digitalWrite(str1.toInt(), str2.toInt());
+    ledcDetachPin(P1.toInt());
+    pinMode(P1.toInt(), OUTPUT);
+    digitalWrite(P1.toInt(), P2.toInt());
     Feedback="{\"data\":\""+Command+"\"}";
   }   
   else if (cmd=="digitalread") {
-    Feedback="{\"data\":\""+String(digitalRead(str1.toInt()))+"\"}";
+    Feedback="{\"data\":\""+String(digitalRead(P1.toInt()))+"\"}";
   }
   else if (cmd=="analogwrite") {
-    ledcAttachPin(str1.toInt(), 1);
+    ledcAttachPin(P1.toInt(), 1);
     ledcSetup(1, 5000, 8);
-    ledcWrite(1,str2.toInt());
+    ledcWrite(1,P2.toInt());
     Feedback="{\"data\":\""+Command+"\"}";
   }       
   else if (cmd=="analogread") {
-    Feedback="{\"data\":\""+String(analogRead(str1.toInt()))+"\"}";
+    Feedback="{\"data\":\""+String(analogRead(P1.toInt()))+"\"}";
   }
   else if (cmd=="touchread") {
-    Feedback="{\"data\":\""+String(touchRead(str1.toInt()))+"\"}";
+    Feedback="{\"data\":\""+String(touchRead(P1.toInt()))+"\"}";
   }  
   else if (cmd=="tcp") {
-    String domain=str1;
-    int port=str2.toInt();
-    String request=str3;
-    int wait=str4.toInt();      // wait = 0 or 1
+    String domain=P1;
+    int port=P2.toInt();
+    String request=P3;
+    int wait=P4.toInt();      // wait = 0 or 1
+
     if ((port==443)||(domain.indexOf("https")==0)||(domain.indexOf("HTTPS")==0))
       Feedback="{\"data\":\""+tcp_https(domain,request,port,wait)+"\"}";
     else
@@ -132,113 +137,124 @@ void ExecuteCommand()
   }
   else if (cmd=="ifttt") {
     String domain="maker.ifttt.com";
-    String request = "/trigger/" + str1 + "/with/key/" + str2;
-    request += "?value1="+str3+"&value2="+str4+"&value3="+str5;
+    String request = "/trigger/" + P1 + "/with/key/" + P2;
+    request += "?value1="+P3+"&value2="+P4+"&value3="+P5;
     Feedback="{\"data\":\""+tcp_https(domain,request,443,0)+"\"}";
   }
   else if (cmd=="thingspeakupdate") {
     String domain="api.thingspeak.com";
-    String request = "/update?api_key=" + str1;
-    request += "&field1="+str2+"&field2="+str3+"&field3="+str4+"&field4="+str5+"&field5="+str6+"&field6="+str7+"&field7="+str8+"&field8="+str9;
+    String request = "/update?api_key=" + P1;
+    request += "&field1="+P2+"&field2="+P3+"&field3="+P4+"&field4="+P5+"&field5="+P6+"&field6="+P7+"&field7="+P8+"&field8="+P9;
     Feedback="{\"data\":\""+tcp_https(domain,request,443,0)+"\"}";
   }    
   else if (cmd=="thingspeakread") {
     String domain="api.thingspeak.com";
-    String request = str1;
+    String request = P1;
     Feedback="{\"data\":\""+tcp_https(domain,request,443,1)+"\"}";
   } 
   else if (cmd=="linenotify") {
-    String token = str1;
-    String request = str2;
+    String token = P1;
+    String request = P2;
     Feedback="{\"data\":\""+LineNotify(token,request,1)+"\"}";
   } 
   else if (cmd=="car") {
-    ledcAttachPin(str1.toInt(), 1);
+    ledcAttachPin(P1.toInt(), 1);
     ledcSetup(1, 5000, 8);
     ledcWrite(1,0);
-    ledcAttachPin(str2.toInt(), 2);
+    ledcAttachPin(P2.toInt(), 2);
     ledcSetup(2, 5000, 8);
     ledcWrite(2,0);  
-    ledcAttachPin(str3.toInt(), 3);
+    ledcAttachPin(P3.toInt(), 3);
     ledcSetup(3, 5000, 8);
     ledcWrite(3,0); 
-    ledcAttachPin(str4.toInt(), 4);
+    ledcAttachPin(P4.toInt(), 4);
     ledcSetup(4, 5000, 8);
     ledcWrite(4,0);
     delay(10);
     
-    if (str8=="S") {
+    if (P8=="S") {
       //
     }
-    else if  (str8=="F") {
-      ledcAttachPin(str1.toInt(), 1);
+    else if  (P8=="F") {
+      ledcAttachPin(P1.toInt(), 1);
       ledcSetup(1, 5000, 8);
-      ledcWrite(1,str5.toInt());
-      ledcAttachPin(str4.toInt(), 4);
+      ledcWrite(1,P5.toInt());
+      ledcAttachPin(P4.toInt(), 4);
       ledcSetup(4, 5000, 8);
-      ledcWrite(4,str6.toInt());
-      if ((str7!="")&&(str7!="0")) {
-        delay(str7.toInt());
-        ledcAttachPin(str1.toInt(), 1);
+      ledcWrite(4,P6.toInt());
+      if ((P7!="")&&(P7!="0")) {
+        delay(P7.toInt());
+        ledcAttachPin(P1.toInt(), 1);
         ledcSetup(1, 5000, 8);
         ledcWrite(1,0);
-        ledcAttachPin(str4.toInt(), 4);
+        ledcAttachPin(P4.toInt(), 4);
         ledcSetup(4, 5000, 8);
         ledcWrite(4,0);          
       }     
     }
-    else if  (str8=="B") {
-      ledcAttachPin(str2.toInt(), 2);
+    else if  (P8=="B") {
+      ledcAttachPin(P2.toInt(), 2);
       ledcSetup(2, 5000, 8);
-      ledcWrite(2,str5.toInt());  
-      ledcAttachPin(str3.toInt(), 3);
+      ledcWrite(2,P5.toInt());  
+      ledcAttachPin(P3.toInt(), 3);
       ledcSetup(3, 5000, 8);
-      ledcWrite(3,str6.toInt());  
-      if ((str7!="")&&(str7!="0")) {
-        delay(str7.toInt());
-        ledcAttachPin(str2.toInt(), 2);
+      ledcWrite(3,P6.toInt());  
+      if ((P7!="")&&(P7!="0")) {
+        delay(P7.toInt());
+        ledcAttachPin(P2.toInt(), 2);
         ledcSetup(2, 5000, 8);
         ledcWrite(2,0); 
-        ledcAttachPin(str3.toInt(), 3);
+        ledcAttachPin(P3.toInt(), 3);
         ledcSetup(3, 5000, 8);
         ledcWrite(3,0); 
       }     
     }
-    else if  (str8=="L") {
-      ledcAttachPin(str2.toInt(), 2);
+    else if  (P8=="L") {
+      ledcAttachPin(P2.toInt(), 2);
       ledcSetup(2, 5000, 8);
-      ledcWrite(2,str5.toInt());  
-      ledcAttachPin(str4.toInt(), 4);
+      ledcWrite(2,P5.toInt());  
+      ledcAttachPin(P4.toInt(), 4);
       ledcSetup(4, 5000, 8);
-      ledcWrite(4,str6.toInt());   
-      if ((str7!="")&&(str7!="0")) {
-        delay(str7.toInt());
-        ledcAttachPin(str2.toInt(), 2);
+      ledcWrite(4,P6.toInt());   
+      if ((P7!="")&&(P7!="0")) {
+        delay(P7.toInt());
+        ledcAttachPin(P2.toInt(), 2);
         ledcSetup(2, 5000, 8);
         ledcWrite(2,0); 
-        ledcAttachPin(str4.toInt(), 4);
+        ledcAttachPin(P4.toInt(), 4);
         ledcSetup(4, 5000, 8);
         ledcWrite(4,0);          
       }
     }
-    else if  (str8=="R") {
-      ledcAttachPin(str1.toInt(), 1);
+    else if  (P8=="R") {
+      ledcAttachPin(P1.toInt(), 1);
       ledcSetup(1, 5000, 8);
-      ledcWrite(1,str5.toInt());
-      ledcAttachPin(str3.toInt(), 3);
+      ledcWrite(1,P5.toInt());
+      ledcAttachPin(P3.toInt(), 3);
       ledcSetup(3, 5000, 8);
-      ledcWrite(3,str6.toInt());  
-      if ((str7!="")&&(str7!="0")) {
-        delay(str7.toInt());
-        ledcAttachPin(str1.toInt(), 1);
+      ledcWrite(3,P6.toInt());  
+      if ((P7!="")&&(P7!="0")) {
+        delay(P7.toInt());
+        ledcAttachPin(P1.toInt(), 1);
         ledcSetup(1, 5000, 8);
         ledcWrite(1,0);
-        ledcAttachPin(str3.toInt(), 3);
+        ledcAttachPin(P3.toInt(), 3);
         ledcSetup(3, 5000, 8);
         ledcWrite(3,0); 
       }        
     }
-  }    
+  } 
+  else if (cmd=="i2cLcd") {
+    LiquidCrystal_I2C lcd(P1.toInt(),16,2);
+    lcd.begin(P2.toInt(), P3.toInt());
+    lcd.backlight();
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print(P4);
+    lcd.setCursor(0,1);
+    lcd.print(P5);
+    Feedback="{\"data\":\""+Command+"\"}";
+  }
   else {
     Feedback="{\"data\":\"Command is not defined\"}";
   }
@@ -288,9 +304,9 @@ void setup()
       WiFi.softAP((WiFi.localIP().toString()+"_"+(String)apssid).c_str(), appassword);
       /*
       cmd="ifttt";
-      str1="eventname";
-      str2="key";
-      str3=WiFi.localIP().toString();
+      P1="eventname";
+      P2="key";
+      P3=WiFi.localIP().toString();
       ExecuteCommand();
       */
     }
@@ -306,7 +322,7 @@ void setup()
 
 void loop()
 {
-Command="";cmd="";str1="";str2="";str3="";str4="";str5="";str6="";str7="";str8="";str9="";
+Command="";cmd="";P1="";P2="";P3="";P4="";P5="";P6="";P7="";P8="";P9="";
 ReceiveState=0,cmdState=1,strState=1,questionstate=0,equalstate=0,semicolonstate=0;
 
  WiFiClient client = server.available();
@@ -333,10 +349,10 @@ ReceiveState=0,cmdState=1,strState=1,questionstate=0,equalstate=0,semicolonstate
             client.println("Access-Control-Allow-Methods: GET,POST,PUT,DELETE,OPTIONS");
             client.println("Content-Type: application/json;charset=utf-8");
             client.println("Access-Control-Allow-Origin: *");
-            //client.println("Connection: close");
+            client.println("Connection: close");
             client.println();
             if (Feedback=="")
-              client.println("[{\"data\":\"Welcome to myBlockly\"}]");
+              client.println("[{\"data\":\"Welcome to MyBlockly\"}]");
             else
               client.println("["+Feedback+"]");
             client.println();
@@ -363,6 +379,19 @@ ReceiveState=0,cmdState=1,strState=1,questionstate=0,equalstate=0,semicolonstate
     delay(1);
     client.stop();
   }
+  /*
+  if (SensorValue>LimitValue)
+  {
+    cmd="yourcmd";
+    P1="yourP1";
+    P2="yourP2";
+    P3="yourP3";
+    ...
+    P9="yourP9";
+    ExecuteCommand();
+    delay(10000);
+  }
+  */
 }
 
 void getCommand(char c)
@@ -378,15 +407,15 @@ void getCommand(char c)
     if (c==';') strState++;
   
     if ((cmdState==1)&&((c!='?')||(questionstate==1))) cmd=cmd+String(c);
-    if ((cmdState==0)&&(strState==1)&&((c!='=')||(equalstate==1))) str1=str1+String(c);
-    if ((cmdState==0)&&(strState==2)&&(c!=';')) str2=str2+String(c);
-    if ((cmdState==0)&&(strState==3)&&(c!=';')) str3=str3+String(c);
-    if ((cmdState==0)&&(strState==4)&&(c!=';')) str4=str4+String(c);
-    if ((cmdState==0)&&(strState==5)&&(c!=';')) str5=str5+String(c);
-    if ((cmdState==0)&&(strState==6)&&(c!=';')) str6=str6+String(c);
-    if ((cmdState==0)&&(strState==7)&&(c!=';')) str7=str7+String(c);
-    if ((cmdState==0)&&(strState==8)&&(c!=';')) str8=str8+String(c);
-    if ((cmdState==0)&&(strState>=9)&&((c!=';')||(semicolonstate==1))) str9=str9+String(c);
+    if ((cmdState==0)&&(strState==1)&&((c!='=')||(equalstate==1))) P1=P1+String(c);
+    if ((cmdState==0)&&(strState==2)&&(c!=';')) P2=P2+String(c);
+    if ((cmdState==0)&&(strState==3)&&(c!=';')) P3=P3+String(c);
+    if ((cmdState==0)&&(strState==4)&&(c!=';')) P4=P4+String(c);
+    if ((cmdState==0)&&(strState==5)&&(c!=';')) P5=P5+String(c);
+    if ((cmdState==0)&&(strState==6)&&(c!=';')) P6=P6+String(c);
+    if ((cmdState==0)&&(strState==7)&&(c!=';')) P7=P7+String(c);
+    if ((cmdState==0)&&(strState==8)&&(c!=';')) P8=P8+String(c);
+    if ((cmdState==0)&&(strState>=9)&&((c!=';')||(semicolonstate==1))) P9=P9+String(c);
     
     if (c=='?') questionstate=1;
     if (c=='=') equalstate=1;
