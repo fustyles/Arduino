7d04b7f11ae4b2c089bf67c35a0e188cc5ff7036
fustyles <34397289+fustyles@users.noreply.github.com>
2019-02-01 00:47:51 +0800

    Update ESP32_MyBlockly_JSON.ino

--- ESP32_MyBlockly_JSON.ino
+++ ESP32_MyBlockly_JSON.ino
@@ -1,6 +1,8 @@
 /* 
 NodeMCU (ESP32)
 Author : ChungYi Fu (Kaohsiung, Taiwan)  2019-1-28 10:00
+https://www.facebook.com/francefu
+
 Command Format :  
 http://APIP/?cmd=P1;P2;P3;P4;P5;P6;P7;P8;P9
 http://STAIP/?cmd=P1;P2;P3;P4;P5;P6;P7;P8;P9
