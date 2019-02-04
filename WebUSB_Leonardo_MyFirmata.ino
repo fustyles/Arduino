539134b061d7d7e33611a17804d66affcfc1da69
fustyles <34397289+fustyles@users.noreply.github.com>
2019-02-04 19:58:33 +0800

    Update WebUSB_Leonardo_MyFirmata.ino

--- WebUSB_Leonardo_MyFirmata.ino
+++ WebUSB_Leonardo_MyFirmata.ino
@@ -2,14 +2,17 @@
 WebUSB WebUSBSerial(1 /* https:// */, "github.com/fustyles/webduino/tree/master/myBlockly");
 #define Serial WebUSBSerial
 
-String ReceiveData="", command="",cmd="",str1="",str2="",str3="",str4="",str5="",str6="",str7="",str8="",str9="";
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+String ReceiveData="", Command="",cmd="",P1="",P2="",P3="",P4="",P5="",P6="",P7="",P8="",P9="";
 boolean debug = true;
 
 void setup()
 {
   while (!Serial) {;}
   Serial.begin(9600);
-  SendData("Sketch begins.\r\n");
+  SendData("Sketch begins.");
 }
 
 void loop() 
@@ -21,41 +24,44 @@ void loop()
   if (cmd=="yourcmd")
     {
       //you can do anything
-      //if (debug == true) SendData(cmd+"="+str1+";"+str2);
+      //if (debug == true) SendData("{\"data\":\""+Command+"\"}");
     } 
   else if (cmd=="inputpullup")
     {
-      pinMode(str1.toInt(), INPUT_PULLUP);
-      if (debug == true) SendData(command);
+      pinMode(P1.toInt(), INPUT_PULLUP);
+      if (debug == true) SendData("{\"data\":\""+Command+"\"}");
     }  
   else if (cmd=="pinmode")
     {
-      pinMode(str1.toInt(), str2.toInt());
-      if (debug == true) SendData(command);
+      pinMode(P1.toInt(), P2.toInt());
+      if (debug == true) SendData("{\"data\":\""+Command+"\"}");
     }        
   else if (cmd=="digitalwrite")
     {
-      pinMode(str1.toInt(), OUTPUT);
-      digitalWrite(str1.toInt(),str2.toInt());
-      if (debug == true) SendData(command);
+      pinMode(P1.toInt(), OUTPUT);
+      digitalWrite(P1.toInt(),P2.toInt());
+      if (debug == true) SendData("{\"data\":\""+Command+"\"}");
     }   
   else if (cmd=="digitalread")
     {
-      SendData(String(digitalRead(str1.toInt())));
+      SendData("{\"data\":\""+String(digitalRead(P1.toInt()))+"\"}");
     }    
   else if (cmd=="analogwrite")
     {
-      pinMode(str1.toInt(), OUTPUT);
-      analogWrite(str1.toInt(),str2.toInt());
-      if (debug == true) SendData(command);
+      pinMode(P1.toInt(), OUTPUT);
+      analogWrite(P1.toInt(),P2.toInt());
+      if (debug == true) SendData("{\"data\":\""+Command+"\"}");
     }       
   else if (cmd=="analogread")
     {
-      SendData(String(analogRead(str1.toInt())));
-    }    
-  else if (cmd=="i2cLcd") {
-    if (P1toLowerCase()=="0x27") P1="39";
-    if (P1toLowerCase()=="0x3f") P1="63";
+      SendData("{\"data\":\""+String(analogRead(P1.toInt()))+"\"}");
+    } 
+   else if (cmd=="i2cLcd") {
+    P1.toLowerCase();
+    if (P1=="0x27") 
+      P1="39";
+    else if (P1=="0x3f") 
+      P1="63";
     LiquidCrystal_I2C lcd(P1.toInt(),16,2);
     lcd.begin();
     lcd.backlight();
@@ -64,11 +70,11 @@ void loop()
     lcd.print(P4);
     lcd.setCursor(0,1);
     lcd.print(P5);
-    if (debug == true) SendData(P4+"; "+P5);
-  }    
+    if (debug == true) SendData(P4+"<br>"+P5);
+  }
   else 
     {
-      SendData("command is not defined");
+      SendData("{\"data\":\"Command is not defined\"}");
     }  
   }
 }
@@ -81,8 +87,8 @@ void SendData(String data)
 
 void getCommand()
 {
-  ReceiveData="";command="";cmd="";str1="";str2="";str3="";str4="";str5="";str6="";str7="";str8="";str9="";
-  byte ReceiveState=0,cmdState=1,strState=1,questionstate=0,equalstate=0,semicolonstate=0;
+  ReceiveData="";Command="";cmd="";P1="";P2="";P3="";P4="";P5="";P6="";P7="";P8="";P9="";
+  byte ReceiveState=0,cmdState=1,PState=1,questionstate=0,equalstate=0,semicolonstate=0;
   
   if (Serial.available())
   {
@@ -96,25 +102,25 @@ void getCommand()
       
       if (ReceiveState==1)
       {
-        command=command+String(c);
+        Command=Command+String(c);
         
         if (c=='=') cmdState=0;
-        if (c==';') strState++;
+        if (c==';') PState++;
 
         if ((cmdState==1)&&((c!='?')||(questionstate==1))) cmd=cmd+String(c);
-        if ((cmdState==0)&&(strState==1)&&((c!='=')||(equalstate==1))) str1=str1+String(c);
-        if ((cmdState==0)&&(strState==2)&&(c!=';')) str2=str2+String(c);
-        if ((cmdState==0)&&(strState==3)&&(c!=';')) str3=str3+String(c);
-        if ((cmdState==0)&&(strState==4)&&(c!=';')) str4=str4+String(c);
-        if ((cmdState==0)&&(strState==5)&&(c!=';')) str5=str5+String(c);
-        if ((cmdState==0)&&(strState==6)&&(c!=';')) str6=str6+String(c);
-        if ((cmdState==0)&&(strState==7)&&(c!=';')) str7=str7+String(c);
-        if ((cmdState==0)&&(strState==8)&&(c!=';')) str8=str8+String(c);
-        if ((cmdState==0)&&(strState>=9)&&((c!=';')||(semicolonstate==1))) str9=str9+String(c);
+        if ((cmdState==0)&&(PState==1)&&((c!='=')||(equalstate==1))) P1=P1+String(c);
+        if ((cmdState==0)&&(PState==2)&&(c!=';')) P2=P2+String(c);
+        if ((cmdState==0)&&(PState==3)&&(c!=';')) P3=P3+String(c);
+        if ((cmdState==0)&&(PState==4)&&(c!=';')) P4=P4+String(c);
+        if ((cmdState==0)&&(PState==5)&&(c!=';')) P5=P5+String(c);
+        if ((cmdState==0)&&(PState==6)&&(c!=';')) P6=P6+String(c);
+        if ((cmdState==0)&&(PState==7)&&(c!=';')) P7=P7+String(c);
+        if ((cmdState==0)&&(PState==8)&&(c!=';')) P8=P8+String(c);
+        if ((cmdState==0)&&(PState>=9)&&((c!=';')||(semicolonstate==1))) P9=P9+String(c);
         
         if (c=='?') questionstate=1;
         if (c=='=') equalstate=1;
-        if ((strState>=9)&&(c==';')) semicolonstate=1;
+        if ((PState>=9)&&(c==';')) semicolonstate=1;
       }
     }  
   }
