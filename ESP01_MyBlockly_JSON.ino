35221d97348b2d5d487c581f3d5bb1539f60b8c9
fustyles <34397289+fustyles@users.noreply.github.com>
2019-02-22 20:00:46 +0800

    Update ESP01_MyBlockly_JSON.ino

--- ESP01_MyBlockly_JSON.ino
+++ ESP01_MyBlockly_JSON.ino
@@ -110,7 +110,6 @@ void ExecuteCommand()
     Feedback="{\"data\":\""+Command+"\"}";
   }   
   else if (cmd=="digitalread") {
-    pinMode(P1.toInt(), INPUT);
     Feedback="{\"data\":\""+String(digitalRead(P1.toInt()))+"\"}";
   }
   else if (cmd=="analogwrite") {
@@ -119,7 +118,6 @@ void ExecuteCommand()
     Feedback="{\"data\":\""+Command+"\"}";
   }       
   else if (cmd=="analogread") {
-    pinMode(P1.toInt(), INPUT);
     Feedback="{\"data\":\""+String(analogRead(P1.toInt()))+"\"}";
   }
   else if (cmd=="tcp") {
