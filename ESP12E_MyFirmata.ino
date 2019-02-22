b19bc7a5b9b7aa2e7d0a6b225acef1e7acbcfc0f
fustyles <34397289+fustyles@users.noreply.github.com>
2019-02-22 19:57:50 +0800

    Update ESP12E_MyFirmata.ino

--- ESP12E_MyFirmata.ino
+++ ESP12E_MyFirmata.ino
@@ -118,7 +118,6 @@ void ExecuteCommand()
   }   
   else if (cmd=="digitalread")
   {
-    pinMode(str1.toInt(), INPUT);
     Feedback=String(digitalRead(str1.toInt()));
   }
   else if (cmd=="analogwrite")
@@ -129,7 +128,6 @@ void ExecuteCommand()
   }       
   else if (cmd=="analogread")
   {
-    pinMode(str1.toInt(), INPUT);
     Feedback=String(analogRead(str1.toInt()));
   }
   else if (cmd=="tcp")
