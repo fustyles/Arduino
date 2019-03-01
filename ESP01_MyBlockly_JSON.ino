3dcddb739393a636132eae8b51835a1e10b2f655
fustyles <34397289+fustyles@users.noreply.github.com>
2019-03-01 13:51:24 +0800

    Update ESP01_MyBlockly_JSON.ino

--- ESP01_MyBlockly_JSON.ino
+++ ESP01_MyBlockly_JSON.ino
@@ -160,7 +160,7 @@ void ExecuteCommand()
     Feedback.replace("\":",",");
     Feedback="{\"data\":\""+Feedback+"\"}";
   }  
-  else if (cmd=="linenotify") {
+  else if (cmd=="linenotify") {    //message=xxx&stickerPackageId=xxx&stickerId=xxx
     String token = P1;
     String request = P2;
     Feedback=LineNotify(token,request,1);
@@ -399,7 +399,7 @@ String tcp(String domain,String request,int port,byte wait)
       return "Connection failed";  
 }
 
-String LineNotify(String token, String message, byte wait)  //message=xxx&stickerPackageId=xxx&stickerId=xxx
+String LineNotify(String token, String message, byte wait)
 {
   message.replace(" ","%20");
   message.replace("&","%20");
