1d6e8151352af7bacc094aaf377ed7c9a4661a01
fustyles <34397289+fustyles@users.noreply.github.com>
2019-03-29 22:08:32 +0800

    Update app_httpd.cpp

--- ESP32-CAM_CAR/app_httpd.cpp
+++ ESP32-CAM_CAR/app_httpd.cpp
@@ -534,11 +534,7 @@ static esp_err_t cmd_handler(httpd_req_t *req){
       ledcSetup(3, 5000, 8);
       ledcWrite(3,val);
     }      
-    else if(!strcmp(variable, "car")) {
-      //ledcDetachPin(12);
-      //ledcDetachPin(13);
-      //ledcDetachPin(15);
-      //ledcDetachPin(14);                  
+    else if(!strcmp(variable, "car")) {               
       pinMode(12, OUTPUT);
       pinMode(13, OUTPUT); 
       pinMode(15, OUTPUT); 
