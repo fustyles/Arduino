61053ec1d7b45cc3e65285f4dfa9eefb9ec40f3a
fustyles <34397289+fustyles@users.noreply.github.com>
2019-04-01 18:25:28 +0800

    Update app_httpd.cpp

--- ESP32-CAM_CAR/app_httpd.cpp
+++ ESP32-CAM_CAR/app_httpd.cpp
@@ -549,6 +549,7 @@ static esp_err_t cmd_handler(httpd_req_t *req){
     }       
     else if(!strcmp(variable, "car")) {  
       //Don't use channel 1 and channel 2
+      //If output PWM to GPIO 15 using ledcWrite, it will lose control.
       if (val==1) {
         Serial.println("Forward");     
         ledcWrite(5,speed);
