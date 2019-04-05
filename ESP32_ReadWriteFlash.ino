2ee59169a1ef9deb6eb221905beb0968b215333a
fustyles <34397289+fustyles@users.noreply.github.com>
2019-04-06 02:09:09 +0800

    Update ESP32_ReadWriteFlash.ino

--- ESP32_ReadWriteFlash.ino
+++ ESP32_ReadWriteFlash.ino
@@ -1,5 +1,5 @@
 /* 
-NodeMCU (ESP32) write WIFI SSID and password to SPI FLASH and read from SPI FLASH.
+NodeMCU (ESP32) write WIFI SSID and password to SPI FLASH and read data from SPI FLASH.
 Author : ChungYi Fu (Kaohsiung, Taiwan)  2019-4-6 01:00
 https://www.facebook.com/francefu
 
