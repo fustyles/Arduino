b682aed3d6460d1c09ef751ff08cec8e685ca712
fustyles <34397289+fustyles@users.noreply.github.com>
2019-04-05 19:37:28 +0800

    Update ESP12E_MyBlockly_JSON.ino

--- ESP12E_MyBlockly_JSON.ino
+++ ESP12E_MyBlockly_JSON.ino
@@ -230,7 +230,7 @@ void setup()
         if ((StartTime+10000) < millis()) break;
     } 
   
-    if (WiFi.localIP().toString()!="0.0.0.0")
+    if (WiFi.status() == WL_CONNECTED)
     {
       pinMode(2, OUTPUT);
       for (int i=0;i<5;i++)
@@ -246,7 +246,7 @@ void setup()
     Serial.println("STAIP address: ");
     Serial.println(WiFi.localIP());
     
-    if (WiFi.localIP().toString()!="0.0.0.0")
+    if (WiFi.status() == WL_CONNECTED)
     {
       WiFi.softAP((WiFi.localIP().toString()+"_"+(String)apssid).c_str(), appassword);
       /*
