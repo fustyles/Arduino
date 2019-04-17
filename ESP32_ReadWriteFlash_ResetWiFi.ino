40cf6239419d053782f026db107c27b4e87d3022
fustyles <34397289+fustyles@users.noreply.github.com>
2019-04-17 22:58:19 +0800

    Add files via upload

---
+++ ESP32_ReadWriteFlash_ResetWiFi.ino
@@ -0,0 +1,280 @@
+/* 
+NodeMCU (ESP32)
+Author : ChungYi Fu (Kaohsiung, Taiwan)  2019-04-17 23:00
+https://www.facebook.com/francefu
+
+Command Format :  
+http://APIP/?resetwifi=ssid;password
+http://STAIP/?resetwifi=ssid;password
+*/
+
+#include <WiFi.h>
+#include <ESP.h>
+
+const int len = 64;    // flashWrite, flashRead -> i = 0 to 127
+
+// Enter your WiFi ssid and password
+char ssid[len]    = "";
+char password[len]    = "";
+
+const char* apssid = "MyFirmata ESP32";
+const char* appassword = "12345678";         //AP password require at least 8 characters.
+
+const uint32_t addressStart = 0x3FA000; 
+const uint32_t addressEnd   = 0x3FAFFF;
+
+WiFiServer server(80);
+
+String Feedback="", Command="",cmd="",str1="",str2="",str3="",str4="",str5="",str6="",str7="",str8="",str9="";
+byte ReceiveState=0,cmdState=1,strState=1,questionstate=0,equalstate=0,semicolonstate=0;
+
+void ExecuteCommand()
+{
+  Serial.println("");
+  //Serial.println("Command: "+Command);
+  Serial.println("cmd= "+cmd+" ,str1= "+str1+" ,str2= "+str2+" ,str3= "+str3+" ,str4= "+str4+" ,str5= "+str5+" ,str6= "+str6+" ,str7= "+str7+" ,str8= "+str8+" ,str9= "+str9);
+  Serial.println("");
+  
+  if (cmd=="your cmd") {
+    // You can do anything
+    // Feedback="<font color=\"red\">Hello World</font>";
+  }
+  else if (cmd=="resetwifi") {
+    WiFi.begin(str1.c_str(), str2.c_str());
+    Serial.print("Connecting to ");
+    Serial.println(str1);
+    long int StartTime=millis();
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+      if ((StartTime+5000) < millis()) break;
+    } 
+    
+    if (WiFi.status() == WL_CONNECTED) {
+      char buff_ssid[len], buff_password[len]; 
+      strcpy(buff_ssid, str1.c_str());
+      strcpy(buff_password, str2.c_str());
+      flashErase();
+      flashWrite(buff_ssid, 0);  
+      flashWrite(buff_password, 1);
+      pinMode(2, OUTPUT);
+      for (int i=0;i<5;i++) {
+        digitalWrite(2,HIGH);
+        delay(100);
+        digitalWrite(2,LOW);
+        delay(100);
+      }      
+      ESP.restart();
+    }
+    else {
+      Serial.println("");
+      Serial.println("Connection failed.");
+      Feedback="Connection failed.";
+    }
+  }    
+  else
+    Feedback="Command is not defined";
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(10);
+  
+  WiFi.mode(WIFI_AP_STA);
+
+  //WiFi.config(IPAddress(192, 168, 201, 100), IPAddress(192, 168, 201, 2), IPAddress(255, 255, 255, 0));
+
+  WiFi.begin(ssid, password);
+
+  delay(1000);
+  Serial.println("");
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  long int StartTime=millis();
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print("."); 
+    if ((StartTime+5000) < millis()) break;
+  } 
+
+  if (WiFi.status() == WL_CONNECTED) {
+    pinMode(2, OUTPUT);
+    for (int i=0;i<5;i++) {
+      digitalWrite(2,HIGH);
+      delay(100);
+      digitalWrite(2,LOW);
+      delay(100);
+    }
+  } 
+  else {
+    // Read WIFI SSID and password from SPI FLASH.
+    char buff_ssid[len], buff_password[len];
+    strcpy(buff_ssid, flashRead(0));
+    strcpy(buff_password, flashRead(1));
+    if ((buff_password[0]>=32)&&(buff_password[0]<=126)) {
+      WiFi.begin(buff_ssid, buff_password);
+      delay(1000);
+      Serial.println("");
+      Serial.print("Connecting to ");
+      Serial.println(buff_ssid);
+      
+      long int StartTime=millis();
+      while(WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");    
+        if ((StartTime+5000) < millis()) break;
+      }       
+    }
+  }
+
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("");
+    Serial.println("STAIP address: ");
+    Serial.println(WiFi.localIP());      
+    WiFi.softAP((WiFi.localIP().toString()+"_"+(String)apssid).c_str(), appassword);
+    pinMode(2, OUTPUT);
+    for (int i=0;i<5;i++) {
+      digitalWrite(2,HIGH);
+      delay(100);
+      digitalWrite(2,LOW);
+      delay(100);
+    }
+  }
+  else
+    WiFi.softAP(apssid, appassword);
+    
+  //WiFi.softAPConfig(IPAddress(192, 168, 4, 1), IPAddress(192, 168, 4, 1), IPAddress(255, 255, 255, 0)); 
+  Serial.println("");
+  Serial.println("APIP address: ");
+  Serial.println(WiFi.softAPIP());    
+  server.begin(); 
+}
+
+void loop()
+{
+  Command="";cmd="";str1="";str2="";str3="";str4="";str5="";str6="";str7="";str8="";str9="";
+  ReceiveState=0,cmdState=1,strState=1,questionstate=0,equalstate=0,semicolonstate=0;
+
+  WiFiClient client = server.available();
+
+  if (client) 
+  { 
+    String currentLine = "";
+
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();             
+        
+        getCommand(c);
+                
+        if (c == '\n') {
+          if (currentLine.length() == 0) {    
+            client.println("HTTP/1.1 200 OK");
+            client.println("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+            client.println("Access-Control-Allow-Methods: GET,POST,PUT,DELETE,OPTIONS");
+            client.println("Content-Type: text/html; charset=utf-8");
+            client.println("Access-Control-Allow-Origin: *");
+            client.println("Connection: close");
+            client.println();
+            client.println("<!DOCTYPE HTML>");
+            client.println("<html><head>");
+            client.println("<meta charset=\"UTF-8\">");
+            client.println("<meta http-equiv=\"Access-Control-Allow-Origin\" content=\"*\">");
+            client.println("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
+            client.println("</head><body>");
+            client.println(Feedback);
+            client.println("</body></html>");
+            client.println();
+                        
+            Feedback="";
+            break;
+          } else {
+            currentLine = "";
+          }
+        } 
+        else if (c != '\r') 
+        {
+          currentLine += c;
+        }
+
+        if ((currentLine.indexOf("/?")!=-1)&&(currentLine.indexOf(" HTTP")!=-1))
+        {
+          if (Command.indexOf("stop")!=-1) {
+            client.println();
+            client.println();
+            client.stop();
+          }
+          currentLine="";
+          Feedback="";
+          ExecuteCommand();
+        }
+      }
+    }
+    delay(1);
+    client.stop();
+  }
+}
+
+void getCommand(char c)
+{
+  if (c=='?') ReceiveState=1;
+  if ((c==' ')||(c=='\r')||(c=='\n')) ReceiveState=0;
+  
+  if (ReceiveState==1)
+  {
+    Command=Command+String(c);
+    
+    if (c=='=') cmdState=0;
+    if (c==';') strState++;
+  
+    if ((cmdState==1)&&((c!='?')||(questionstate==1))) cmd=cmd+String(c);
+    if ((cmdState==0)&&(strState==1)&&((c!='=')||(equalstate==1))) str1=str1+String(c);
+    if ((cmdState==0)&&(strState==2)&&(c!=';')) str2=str2+String(c);
+    if ((cmdState==0)&&(strState==3)&&(c!=';')) str3=str3+String(c);
+    if ((cmdState==0)&&(strState==4)&&(c!=';')) str4=str4+String(c);
+    if ((cmdState==0)&&(strState==5)&&(c!=';')) str5=str5+String(c);
+    if ((cmdState==0)&&(strState==6)&&(c!=';')) str6=str6+String(c);
+    if ((cmdState==0)&&(strState==7)&&(c!=';')) str7=str7+String(c);
+    if ((cmdState==0)&&(strState==8)&&(c!=';')) str8=str8+String(c);
+    if ((cmdState==0)&&(strState>=9)&&((c!=';')||(semicolonstate==1))) str9=str9+String(c);
+    
+    if (c=='?') questionstate=1;
+    if (c=='=') equalstate=1;
+    if ((strState>=9)&&(c==';')) semicolonstate=1;
+  }
+}
+
+void flashWrite(char data[len], int i) {      // i = 0 to 63
+  uint32_t flashAddress = addressStart + i*len;
+  char buff_write[len];
+  strcpy(buff_write, data);
+  if (ESP.flashWrite(flashAddress,(uint32_t*)buff_write, sizeof(buff_write)-1))
+    Serial.printf("address: %p write \"%s\" [ok]\n", flashAddress, buff_write);
+  else 
+    Serial.printf("address: %p write \"%s\" [error]\n", flashAddress, buff_write);
+}
+
+char* flashRead(int i) {      // i = 0 to 63
+  uint32_t flashAddress = addressStart + i*len;
+  static char buff_read[len];
+  if (ESP.flashRead(flashAddress,(uint32_t*)buff_read, sizeof(buff_read)-1)) {
+    //Serial.printf("data: \"%s\"\n", buff_read);
+    return buff_read;
+  } else  
+    return "";  
+}
+
+void flashErase() {
+  if (ESP.flashEraseSector(addressStart / 4096))
+    Serial.println("\nErase [ok]");
+  else
+    Serial.println("\nErase [error]");
+}
+
+/*
+bool flashEraseSector(uint32_t sector);
+bool flashWrite(uint32_t offset, uint32_t *data, size_t size);
+bool flashRead(uint32_t offset, uint32_t *data, size_t size);
+*/
