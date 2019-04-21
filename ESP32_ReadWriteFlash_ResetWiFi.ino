4e826cc38f8acd9ffd699a0d516b00c1fea34a8a
fustyles <34397289+fustyles@users.noreply.github.com>
2019-04-21 11:52:44 +0800

    Update ESP32_ReadWriteFlash_ResetWiFi.ino

--- ESP32_ReadWriteFlash_ResetWiFi.ino
+++ ESP32_ReadWriteFlash_ResetWiFi.ino
@@ -131,9 +131,9 @@ void setup()
     
     pinMode(2, OUTPUT);
     for (int i=0;i<5;i++) {
-      digitalWrite(2,HIGH);
-      delay(400);
       digitalWrite(2,LOW);
+      delay(400);
+      digitalWrite(2,HIGH);
       delay(400);
     }
   }
