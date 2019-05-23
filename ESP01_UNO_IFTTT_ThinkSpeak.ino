1e60575a83ea7b7436deb78297831eaa0bc576c2
fustyles <34397289+fustyles@users.noreply.github.com>
2019-05-23 23:47:50 +0800

    Update ESP01_UNO_IFTTT_ThinkSpeak.ino

--- ESP01_UNO_IFTTT_ThinkSpeak.ino
+++ ESP01_UNO_IFTTT_ThinkSpeak.ino
@@ -79,7 +79,7 @@ void loop()
     SendData(request, 4000);
     SendData("AT+CIPCLOSE",2000);
     
-    // alarm
+    // alarm (IFTTT: Webhooks+Line)
     if ((SensorTemperature<10)||(SensorHumidity<20))
     {  
       delay(1000);
