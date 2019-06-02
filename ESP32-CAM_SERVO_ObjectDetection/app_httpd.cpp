61967d527ae4ff80436982da7a044181bffb3c93
fustyles <34397289+fustyles@users.noreply.github.com>
2019-06-02 19:05:50 +0800

    Update app_httpd.cpp

--- ESP32-CAM_SERVO_ObjectDetection/app_httpd.cpp
+++ ESP32-CAM_SERVO_ObjectDetection/app_httpd.cpp
@@ -1,6 +1,6 @@
 /*
 ESP32-CAM OBJECT DETECTION
-Author : ChungYi Fu (Kaohsiung, Taiwan)  2019-6-1 18:00
+Author : ChungYi Fu (Kaohsiung, Taiwan)  2019-6-2 19:00
 https://www.facebook.com/francefu
 Servo -> VCC, GND, gpio2
 */
