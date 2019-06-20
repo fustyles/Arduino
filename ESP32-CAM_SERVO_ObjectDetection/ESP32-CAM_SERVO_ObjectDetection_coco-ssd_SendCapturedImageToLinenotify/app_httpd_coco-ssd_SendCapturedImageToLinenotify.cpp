59c4fe68c4ee712dfac9ff3244104cd6e58227e2
fustyles <34397289+fustyles@users.noreply.github.com>
2019-06-20 17:18:17 +0800

    Update app_httpd_coco-ssd_SendCapturedImageToLinenotify.cpp

--- ESP32-CAM_SERVO_ObjectDetection/ESP32-CAM_SERVO_ObjectDetection_coco-ssd_SendCapturedImageToLinenotify/app_httpd_coco-ssd_SendCapturedImageToLinenotify.cpp
+++ ESP32-CAM_SERVO_ObjectDetection/ESP32-CAM_SERVO_ObjectDetection_coco-ssd_SendCapturedImageToLinenotify/app_httpd_coco-ssd_SendCapturedImageToLinenotify.cpp
@@ -9,6 +9,10 @@ Google Script
 https://github.com/fustyles/webduino/blob/gs/SendCapturedImageToGoogleDriveAndLinenotify.gs
 You should allow anyone to upload files to google drive folder and allow anyone to execute the google script.
 
+https://script.google.com/home
+https://script.google.com/home/executions
+https://drive.google.com/drive/my-drive
+
 How to enable WebGL in Chrome.
 https://superuser.com/questions/836832/how-can-i-enable-webgl-in-my-browser
 */
