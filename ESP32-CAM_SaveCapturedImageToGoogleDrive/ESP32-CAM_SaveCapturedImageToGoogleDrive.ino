127e800e25de0e7ab256bbc91b6046ab04e0b625
fustyles <34397289+fustyles@users.noreply.github.com>
2019-06-22 11:27:48 +0800

    Update ESP32-CAM_SaveCapturedImageToGoogleDrive.ino

--- ESP32-CAM_SaveCapturedImageToGoogleDrive/ESP32-CAM_SaveCapturedImageToGoogleDrive.ino
+++ ESP32-CAM_SaveCapturedImageToGoogleDrive/ESP32-CAM_SaveCapturedImageToGoogleDrive.ino
@@ -63,7 +63,7 @@ const char* appassword = "12345678";         //AP password require at least 8 ch
 
 const char *post_domain = "script.google.com";
 /*
-https://github.com/fustyles/webduino/blob/gs/SendCapturedImageToGoogleDriveAndLinenotify.gs
+https://github.com/fustyles/webduino/blob/gs/SendCapturedImageToGoogleDriveAndLinenotify_doGet.gs
 Create your Google Apps Script and replace the "post_path" value.
 */
 String post_path = "/macros/s/AKfycbxQFDRkhSSsNp-3oL_TjcCStIykkLGb5n_MEPlS9dPw1053vUo123/exec?myFoldername=TEST&myFilename=test&myToken=lHaUbj8vv1ZCvoxzwhpoarxNYR4PKYIHtVOS72qS6Lt&myFile=data:image/jpeg;base64,";
