ee9145be4c1a7472896784922aa92fec81e4f973
fustyles <34397289+fustyles@users.noreply.github.com>
2019-06-22 10:04:39 +0800

    Update ESP32-CAM_SaveCapturedImageToGoogleDrive.ino

--- ESP32-CAM_SaveCapturedImageToGoogleDrive/ESP32-CAM_SaveCapturedImageToGoogleDrive.ino
+++ ESP32-CAM_SaveCapturedImageToGoogleDrive/ESP32-CAM_SaveCapturedImageToGoogleDrive.ino
@@ -62,7 +62,7 @@ const char* apssid = "MyFirmata ESP32";
 const char* appassword = "12345678";         //AP password require at least 8 characters.
 
 const char *post_domain = "script.google.com"; // Location to send POSTed data
-String post_path = "/macros/s/AKfycbxQFDRkhSSsNp-3oL_TjcCStIykkLGb5n_MEPlS9dPw1053vUo123/exec?myFoldername=TEST&myFilename=test&myToken=lHaUbj8vv1ZCvoxzwhpoarxNYR4PKYIHtVOS72qS6Lt&myFile=data:image/jpeg;base64,";
+String post_path = "/macros/s/AKfycbxQFDRkhSSsNp-3oL_TjcCStIykkLGb5n_MEPlS9dPw1053vUo123/exec" + "?myFoldername=TEST&myFilename=test&myToken=lHaUbj8vv1ZCvoxzwhpoarxNYR4PKYIHtVOS72qS6Lt&myFile=data:image/jpeg;base64,";
 
 void setup()
 {
