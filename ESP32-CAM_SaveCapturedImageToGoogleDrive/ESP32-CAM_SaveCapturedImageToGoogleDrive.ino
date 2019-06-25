e7b968213f84166f4336033f63534cbef104fd5d
fustyles <34397289+fustyles@users.noreply.github.com>
2019-06-25 18:59:48 +0800

    Update ESP32-CAM_SaveCapturedImageToGoogleDrive.ino

--- ESP32-CAM_SaveCapturedImageToGoogleDrive/ESP32-CAM_SaveCapturedImageToGoogleDrive.ino
+++ ESP32-CAM_SaveCapturedImageToGoogleDrive/ESP32-CAM_SaveCapturedImageToGoogleDrive.ino
@@ -68,7 +68,7 @@ String myImage = "&myFile=data:image/jpeg;base64,";
 
 void setup()
 {
-  //WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
   
   Serial.begin(115200);
   delay(10);
