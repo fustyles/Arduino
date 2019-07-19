0ad4085ac5b033d3ffe5a68326bcee3d5954ff9e
fustyles <34397289+fustyles@users.noreply.github.com>
2019-07-20 07:47:30 +0800

    Update ESP32-CAM_CAR_2PWM.ino

--- ESP32-CAM_CAR_2PWM/ESP32-CAM_CAR_2PWM.ino
+++ ESP32-CAM_CAR_2PWM/ESP32-CAM_CAR_2PWM.ino
@@ -1,6 +1,6 @@
 /*
 ESP32-CAM Remote Control Car 
-Author : ChungYi Fu (Kaohsiung, Taiwan)  2019-6-21 19:00
+Author : ChungYi Fu (Kaohsiung, Taiwan)  2019-7-19 19:00
 https://www.facebook.com/francefu
 
 Motor Driver IC -> gpio12, gpio13, gpio14, gpio15
