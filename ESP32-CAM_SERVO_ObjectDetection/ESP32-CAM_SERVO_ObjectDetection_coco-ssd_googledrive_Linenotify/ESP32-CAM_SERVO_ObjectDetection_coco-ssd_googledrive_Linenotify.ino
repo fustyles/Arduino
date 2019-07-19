f25f0e6e9c3fd0b0ca0dbb3e0dba4700384d0e92
fustyles <34397289+fustyles@users.noreply.github.com>
2019-07-20 07:50:14 +0800

    Update ESP32-CAM_SERVO_ObjectDetection_coco-ssd_googledrive_Linenotify.ino

--- ESP32-CAM_SERVO_ObjectDetection/ESP32-CAM_SERVO_ObjectDetection_coco-ssd_googledrive_Linenotify/ESP32-CAM_SERVO_ObjectDetection_coco-ssd_googledrive_Linenotify.ino
+++ ESP32-CAM_SERVO_ObjectDetection/ESP32-CAM_SERVO_ObjectDetection_coco-ssd_googledrive_Linenotify/ESP32-CAM_SERVO_ObjectDetection_coco-ssd_googledrive_Linenotify.ino
@@ -30,50 +30,6 @@ const char* password = "xxxxx";
 //            or another board which has PSRAM enabled
 //
 
-// Select camera model
-//#define CAMERA_MODEL_WROVER_KIT
-//#define CAMERA_MODEL_M5STACK_PSRAM
-#define CAMERA_MODEL_AI_THINKER
-
-#if defined(CAMERA_MODEL_WROVER_KIT)
-#define PWDN_GPIO_NUM    -1
-#define RESET_GPIO_NUM   -1
-#define XCLK_GPIO_NUM    21
-#define SIOD_GPIO_NUM    26
-#define SIOC_GPIO_NUM    27
-
-#define Y9_GPIO_NUM      35
-#define Y8_GPIO_NUM      34
-#define Y7_GPIO_NUM      39
-#define Y6_GPIO_NUM      36
-#define Y5_GPIO_NUM      19
-#define Y4_GPIO_NUM      18
-#define Y3_GPIO_NUM       5
-#define Y2_GPIO_NUM       4
-#define VSYNC_GPIO_NUM   25
-#define HREF_GPIO_NUM    23
-#define PCLK_GPIO_NUM    22
-
-#elif defined(CAMERA_MODEL_M5STACK_PSRAM)
-#define PWDN_GPIO_NUM     -1
-#define RESET_GPIO_NUM    15
-#define XCLK_GPIO_NUM     27
-#define SIOD_GPIO_NUM     25
-#define SIOC_GPIO_NUM     23
-
-#define Y9_GPIO_NUM       19
-#define Y8_GPIO_NUM       36
-#define Y7_GPIO_NUM       18
-#define Y6_GPIO_NUM       39
-#define Y5_GPIO_NUM        5
-#define Y4_GPIO_NUM       34
-#define Y3_GPIO_NUM       35
-#define Y2_GPIO_NUM       32
-#define VSYNC_GPIO_NUM    22
-#define HREF_GPIO_NUM     26
-#define PCLK_GPIO_NUM     21
-
-#elif defined(CAMERA_MODEL_AI_THINKER)
 #define PWDN_GPIO_NUM     32
 #define RESET_GPIO_NUM    -1
 #define XCLK_GPIO_NUM      0
@@ -91,10 +47,6 @@ const char* password = "xxxxx";
 #define VSYNC_GPIO_NUM    25
 #define HREF_GPIO_NUM     23
 #define PCLK_GPIO_NUM     22
-
-#else
-#error "Camera model not selected"
-#endif
 
 void startCameraServer();
 
