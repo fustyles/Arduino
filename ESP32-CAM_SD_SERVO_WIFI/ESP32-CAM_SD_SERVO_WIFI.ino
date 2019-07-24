3cfbc87c4b21cdc9a406159996836909b3b85c42
fustyles <34397289+fustyles@users.noreply.github.com>
2019-07-24 16:01:59 +0800

    Update ESP32-CAM_SD_SERVO_WIFI.ino

--- ESP32-CAM_SD_SERVO_WIFI/ESP32-CAM_SD_SERVO_WIFI.ino
+++ ESP32-CAM_SD_SERVO_WIFI/ESP32-CAM_SD_SERVO_WIFI.ino
@@ -1,5 +1,5 @@
 /*
-ESP32-CAM Save to SD card and control Servo
+ESP32-CAM Save to SD card and control Servo (Something wrong, debug...)
 Author : ChungYi Fu (Kaohsiung, Taiwan)  2019-7-23 14:00
 https://www.facebook.com/francefu
 
