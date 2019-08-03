cc4ee40c8de4a11b83df170826cb53b0f00e166f
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-08-03 22:24:13 +0800

    Update ESP32_SpeechRecognition.ino

--- ESP32_SpeechRecognition.ino
+++ ESP32_SpeechRecognition.ino
@@ -59,12 +59,12 @@ void ExecuteCommand()
   }
   else if (cmd=="speech")
   {
-    if (str1.indexOf("on")!=-1) {
+    if (str1.indexOf("on")!=-1) {    //Turn on the light
       ledcDetachPin(2);
       pinMode(2, OUTPUT);
       digitalWrite(2, 1);
     }
-    else if (str1.indexOf("off")!=-1) {
+    else if (str1.indexOf("off")!=-1) {    //Turn off the light
       ledcDetachPin(2);
       pinMode(2, OUTPUT);
       digitalWrite(2, 0);
