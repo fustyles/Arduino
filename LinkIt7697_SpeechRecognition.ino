161e9fac94691c3cf168cd75b42ad0efde68d46a
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-08-07 20:45:35 +0800

    Add files via upload

---
+++ LinkIt7697_SpeechRecognition.ino
@@ -0,0 +1,257 @@
+/* 
+LinkIt 7697 (Open in Chrome)
+Author : ChungYi Fu (Kaohsiung, Taiwan)  2019-08-07 20:00
+https://www.facebook.com/francefu
+
+Command Format :  
+http://APIP/?cmd=str1;str2;str3;str4;str5;str6;str7;str8;str9
+http://STAIP/?cmd=str1;str2;str3;str4;str5;str6;str7;str8;str9
+
+http://192.168.4.1/?speech=text;language;checkbox
+*/
+
+#include <LWiFi.h>
+WiFiServer server(80);
+
+// Enter your WiFi ssid and password
+char ssid[]     = "xxxxx";   //your network SSID
+char password[] = "xxxxx";   //your network password
+
+String page = "https://fustyles.github.io/webduino/ESP32_SpeechRecognition.html";
+
+String Feedback="", Command="",cmd="",str1="",str2="",str3="",str4="",str5="",str6="",str7="",str8="",str9="";
+byte ReceiveState=0,cmdState=1,strState=1,questionstate=0,equalstate=0,semicolonstate=0;
+
+void ExecuteCommand()
+{
+  str1 = urldecode(str1);
+  
+  Serial.println("");
+  //Serial.println("Command: "+Command);
+  Serial.println("cmd= "+cmd+" ,str1= "+str1+" ,str2= "+str2+" ,str3= "+str3+" ,str4= "+str4+" ,str5= "+str5+" ,str6= "+str6+" ,str7= "+str7+" ,str8= "+str8+" ,str9= "+str9);
+  Serial.println("");
+  
+  if (cmd=="your cmd") {
+    // You can do anything
+    // Feedback="<font color=\"red\">Hello World</font>";
+  }
+  else if (cmd=="speech") {
+    if (str1.indexOf("on")!=-1||str1.indexOf("開")!=-1) {    //Turn on the LED
+      pinMode(LED_BUILTIN, OUTPUT);
+      digitalWrite(LED_BUILTIN, 1);
+      Feedback="<font color=\"red\">Turn on the LED</font>";
+    }
+    else if (str1.indexOf("off")!=-1||str1.indexOf("關")!=-1) {    //Turn off the LED
+      pinMode(LED_BUILTIN, OUTPUT);
+      digitalWrite(LED_BUILTIN, 0);
+      Feedback="<font color=\"red\">Turn off the LED</font>";
+    }
+    else
+      Feedback="<font color=\"red\">No defined => "+str1+"</font>";
+  } 
+  else {
+    Feedback="Command is not defined";
+  }
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  delay(10);
+  
+  //WiFi.config(IPAddress(192, 168, 201, 100), IPAddress(192, 168, 201, 2), IPAddress(255, 255, 255, 0));
+
+  WiFi.begin(ssid, password);
+
+  delay(1000);
+  Serial.println("");
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  long int StartTime=millis();
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    if ((StartTime+10000) < millis()) break;
+  }  
+
+  Serial.println("");
+  Serial.println("STAIP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (WiFi.status() == WL_CONNECTED) {
+    pinMode(LED_BUILTIN, OUTPUT);
+    for (int i=0;i<5;i++) {
+      digitalWrite(LED_BUILTIN,HIGH);
+      delay(100);
+      digitalWrite(LED_BUILTIN,LOW);
+      delay(100);
+    }
+  } 
+  else {
+    pinMode(LED_BUILTIN, OUTPUT);
+    for (int i=0;i<3;i++) {
+      digitalWrite(LED_BUILTIN,HIGH);
+      delay(500);
+      digitalWrite(LED_BUILTIN,LOW);
+      delay(500);
+    }
+  }     
+
+  server.begin(); 
+}
+
+void loop()
+{
+  Command="";cmd="";str1="";str2="";str3="";str4="";str5="";str6="";str7="";str8="";str9="";
+  ReceiveState=0,cmdState=1,strState=1,questionstate=0,equalstate=0,semicolonstate=0;
+
+  WiFiClient client = server.available();
+  
+  if (client) { 
+    String currentLine = "";
+
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();             
+        
+        getCommand(c);
+                
+        if (c == '\n') {
+          if (currentLine.length() == 0) {          
+            /*
+            client.println("HTTP/1.1 200 OK");
+            client.println("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+            client.println("Access-Control-Allow-Methods: GET,POST,PUT,DELETE,OPTIONS");
+            client.println("Content-Type: application/json;charset=utf-8");
+            client.println("Access-Control-Allow-Origin: *");
+            client.println("Connection: close");
+            client.println();
+            client.println("[{\"esp8266\":\""+Feedback+"\"}]");
+            */
+            
+            /*
+            client.println("HTTP/1.1 200 OK");
+            client.println("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+            client.println("Access-Control-Allow-Methods: GET,POST,PUT,DELETE,OPTIONS");
+            client.println("Content-Type: text/xml; charset=utf-8");
+            client.println("Access-Control-Allow-Origin: *");
+            client.println("Connection: close");
+            client.println();
+            client.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+            client.println("<esp8266><feedback>"+Feedback+"</feedback></esp8266>");
+           */
+
+            if (str2=="") str2="en-US";
+            if (Feedback=="") Feedback="Redirect..."; 
+            Feedback+="<script>setTimeout(() => location.href = \""+page+"?"+WiFi.localIP().toString()+"&"+str2+"&"+str3+"\", 3000);</script>";
+            
+            client.println("HTTP/1.1 200 OK");
+            client.println("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+            client.println("Access-Control-Allow-Methods: GET,POST,PUT,DELETE,OPTIONS");
+            client.println("Content-Type: text/html; charset=utf-8");
+            client.println("Access-Control-Allow-Origin: *");
+            client.println("Connection: close");
+            client.println();
+            client.println("<!DOCTYPE HTML>");
+            client.println("<html><head>");
+            client.println("<meta charset=\"UTF-8\">");
+            client.println("<meta http-equiv=\"Access-Control-Allow-Origin\" content=\"*\">");
+            client.println("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
+            client.println("</head><body>");
+            client.println(Feedback);
+            client.println("</body></html>");
+            client.println();
+                        
+            Feedback="";
+            break;
+          } else {
+            currentLine = "";
+          }
+        } 
+        else if (c != '\r') {
+          currentLine += c;
+        }
+
+        if ((currentLine.indexOf("/?")!=-1)&&(currentLine.indexOf(" HTTP")!=-1)) {
+          currentLine="";
+          Feedback="";
+          ExecuteCommand();
+        }
+      }
+    }
+    delay(1);
+    client.stop();
+  }
+}
+
+void getCommand(char c)
+{
+  if (c=='?') ReceiveState=1;
+  if ((c==' ')||(c=='\r')||(c=='\n')) ReceiveState=0;
+  
+  if (ReceiveState==1)
+  {
+    Command=Command+String(c);
+    
+    if (c=='=') cmdState=0;
+    if (c==';') strState++;
+  
+    if ((cmdState==1)&&((c!='?')||(questionstate==1))) cmd=cmd+String(c);
+    if ((cmdState==0)&&(strState==1)&&((c!='=')||(equalstate==1))) str1=str1+String(c);
+    if ((cmdState==0)&&(strState==2)&&(c!=';')) str2=str2+String(c);
+    if ((cmdState==0)&&(strState==3)&&(c!=';')) str3=str3+String(c);
+    if ((cmdState==0)&&(strState==4)&&(c!=';')) str4=str4+String(c);
+    if ((cmdState==0)&&(strState==5)&&(c!=';')) str5=str5+String(c);
+    if ((cmdState==0)&&(strState==6)&&(c!=';')) str6=str6+String(c);
+    if ((cmdState==0)&&(strState==7)&&(c!=';')) str7=str7+String(c);
+    if ((cmdState==0)&&(strState==8)&&(c!=';')) str8=str8+String(c);
+    if ((cmdState==0)&&(strState>=9)&&((c!=';')||(semicolonstate==1))) str9=str9+String(c);
+    
+    if (c=='?') questionstate=1;
+    if (c=='=') equalstate=1;
+    if ((strState>=9)&&(c==';')) semicolonstate=1;
+  }
+}
+
+//https://github.com/zenmanenergy/ESP8266-Arduino-Examples/blob/master/helloWorld_urlencoded/urlencode.ino
+String urldecode(String str)
+{
+    String encodedString="";
+    char c;
+    char code0;
+    char code1;
+    for (int i =0; i < str.length(); i++){
+        c=str.charAt(i);
+      if (c == '+'){
+        encodedString+=' ';  
+      }else if (c == '%') {
+        i++;
+        code0=str.charAt(i);
+        i++;
+        code1=str.charAt(i);
+        c = (h2int(code0) << 4) | h2int(code1);
+        encodedString+=c;
+      } else{
+        
+        encodedString+=c;  
+      }
+      
+      yield();
+    }
+    
+   return encodedString;
+}
+
+unsigned char h2int(char c)
+{
+    if (c >= '0' && c <='9'){
+        return((unsigned char)c - '0');
+    }
+    if (c >= 'a' && c <='f'){
+        return((unsigned char)c - 'a' + 10);
+    }
+    if (c >= 'A' && c <='F'){
+        return((unsigned char)c - 'A' + 10);
+    }
+    return(0);
+}
