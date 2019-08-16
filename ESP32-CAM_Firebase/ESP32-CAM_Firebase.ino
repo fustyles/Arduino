75d82b9bf1fa3e886a5fca43a1ae3a5330fce263
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-08-16 23:16:28 +0800

    Add files via upload

---
+++ ESP32-CAM_Firebase/ESP32-CAM_Firebase.ino
@@ -0,0 +1,188 @@
+/*
+ESP32-CAM Save a captured photo(Base64) to firebase. 
+Author : ChungYi Fu (Kaohsiung, Taiwan)  2019-8-16 23:00
+https://www.facebook.com/francefu
+*/
+
+const char* ssid = "xxxxxxxxxx";
+const char* password = "xxxxxxxxxx";
+
+//xxxxxxxxxx.firebaseio.com
+String FIREBASE_HOST = "xxxxxxxxxx.firebaseio.com";
+
+//https://console.firebase.google.com/project/xxxxxxxxxx/settings/serviceaccounts/databasesecrets
+String FIREBASE_AUTH = "xxxxxxxxxx";
+
+//Library: https://github.com/mobizt/Firebase-ESP32
+#include "FirebaseESP32.h"
+FirebaseData firebaseData;
+
+#include <WiFi.h>
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
+#include "Base64.h"
+
+#include "esp_camera.h"
+
+// WARNING!!! Make sure that you have either selected ESP32 Wrover Module,
+//            or another board which has PSRAM enabled
+
+//CAMERA_MODEL_AI_THINKER
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
+void setup() { 
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
+    
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  Serial.println();
+  Serial.println("ssid: " + (String)ssid);
+  Serial.println("password: " + (String)password);
+  
+  WiFi.begin(ssid, password);
+
+  long int StartTime=millis();
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      if ((StartTime+10000) < millis()) break;
+  } 
+
+  char* apssid = "ESP32-CAM";
+  char* appassword = "12345678";         //AP password require at least 8 characters.
+  Serial.println("");
+  Serial.println("WiFi connected");    
+  Serial.print("Camera Ready! Use 'http://");
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.print(WiFi.localIP());
+    Serial.println("' to connect");
+    WiFi.softAP((WiFi.localIP().toString()+"_"+(String)apssid).c_str(), appassword);            
+  }
+  else {
+    Serial.println("Connection failed");
+    return;
+  } 
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  config.frame_size = FRAMESIZE_QQVGA;  // UXGA|SXGA|XGA|SVGA|VGA|CIF|QVGA|HQVGA|QQVGA
+  config.jpeg_quality = 10;
+  config.fb_count = 1;
+  
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    delay(1000);
+    ESP.restart();
+  }
+ 
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
+  Firebase.setMaxRetry(firebaseData, 3);
+  Firebase.setMaxErrorQueue(firebaseData, 30); 
+  Firebase.enableClassicRequest(firebaseData, true);
+
+  String jsonData = "{\"photo\":\"" + Photo2Base64() + "\"}";
+  String photoPath = "/esp32-cam";
+  if (Firebase.pushJSON(firebaseData, photoPath, jsonData)) {
+    Serial.println(firebaseData.dataPath());
+    Serial.println(firebaseData.pushName());
+    Serial.println(firebaseData.dataPath() + "/"+ firebaseData.pushName());
+  } else {
+    Serial.println(firebaseData.errorReason());
+  }
+} 
+ 
+void loop() { 
+  delay(10000);
+}
+
+String Photo2Base64() {
+    camera_fb_t * fb = NULL;
+    fb = esp_camera_fb_get();  
+    if(!fb) {
+      Serial.println("Camera capture failed");
+      return "";
+    }
+  
+    String imageFile = "data:image/jpeg;base64,";
+    char *input = (char *)fb->buf;
+    char output[base64_enc_len(3)];
+    for (int i=0;i<fb->len;i++) {
+      base64_encode(output, (input++), 3);
+      if (i%3==0) imageFile += urlencode(String(output));
+    }
+
+    esp_camera_fb_return(fb);
+    
+    return imageFile;
+}
+
+//https://github.com/zenmanenergy/ESP8266-Arduino-Examples/
+String urlencode(String str)
+{
+    String encodedString="";
+    char c;
+    char code0;
+    char code1;
+    char code2;
+    for (int i =0; i < str.length(); i++){
+      c=str.charAt(i);
+      if (c == ' '){
+        encodedString+= '+';
+      } else if (isalnum(c)){
+        encodedString+=c;
+      } else{
+        code1=(c & 0xf)+'0';
+        if ((c & 0xf) >9){
+            code1=(c & 0xf) - 10 + 'A';
+        }
+        c=(c>>4)&0xf;
+        code0=c+'0';
+        if (c > 9){
+            code0=c - 10 + 'A';
+        }
+        code2='\0';
+        encodedString+='%';
+        encodedString+=code0;
+        encodedString+=code1;
+        //encodedString+=code2;
+      }
+      yield();
+    }
+    return encodedString;
+}
