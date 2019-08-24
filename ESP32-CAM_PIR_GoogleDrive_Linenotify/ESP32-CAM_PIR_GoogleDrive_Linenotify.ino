7477515f9551097e222168f7de330cca5b16bc5b
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-08-24 18:13:13 +0800

    Update ESP32-CAM_PIR_GoogleDrive_Linenotify.ino

--- ESP32-CAM_PIR_GoogleDrive_Linenotify/ESP32-CAM_PIR_GoogleDrive_Linenotify.ino
+++ ESP32-CAM_PIR_GoogleDrive_Linenotify/ESP32-CAM_PIR_GoogleDrive_Linenotify.ino
@@ -171,7 +171,6 @@ void saveCapturedImage() {
       return;
     }
   
-    // UXGA|SXGA|XGA|SVGA|VGA|CIF|QVGA|HQVGA|QQVGA
     char *input = (char *)fb->buf;
     char output[base64_enc_len(3)];
     String imageFile = "data:image/jpeg;base64,";
