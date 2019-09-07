3bbf58a9aca37ba8cfb5e34696ec38a28b2dd127
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-09-07 21:30:57 +0800

    Update ESP32-CAM_Linenotify.ino

--- ESP32-CAM_Linenotify/ESP32-CAM_Linenotify.ino
+++ ESP32-CAM_Linenotify/ESP32-CAM_Linenotify.ino
@@ -168,7 +168,10 @@ void sendCapturedImage2LineNotify()
     client_tcp.println("Content-Type: multipart/form-data; boundary=Taiwan");
     client_tcp.println();
     client_tcp.print(head);
+    
+    // How to modify the code to upload a big file?
     client_tcp.write(fb->buf, fb->len);
+    
     client_tcp.print(tail);
     client_tcp.println();
     
