0c209713ccdb6b770fafb8c7b52f90f19d519d5d
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-09-07 20:26:51 +0800

    Update ESP32-CAM_Linenotify.ino

--- ESP32-CAM_Linenotify/ESP32-CAM_Linenotify.ino
+++ ESP32-CAM_Linenotify/ESP32-CAM_Linenotify.ino
@@ -117,7 +117,7 @@ void setup()
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
   config.pixel_format = PIXFORMAT_JPEG;
-  config.frame_size = FRAMESIZE_CIF;  // UXGA|SXGA|XGA|SVGA|VGA|CIF|QVGA|HQVGA|QQVGA
+  config.frame_size = FRAMESIZE_CIF;  // VGA|CIF|QVGA|HQVGA|QQVGA
   config.jpeg_quality = 10;
   config.fb_count = 1;
   
