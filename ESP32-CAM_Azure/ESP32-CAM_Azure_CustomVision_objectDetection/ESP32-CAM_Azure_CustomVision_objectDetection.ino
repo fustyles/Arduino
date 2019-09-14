bd4a9967e06a8af1c6e352b6658337aa477a2c32
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-09-14 23:20:13 +0800

    Update ESP32-CAM_Azure_CustomVision_objectDetection.ino

--- ESP32-CAM_Azure/ESP32-CAM_Azure_CustomVision_objectDetection/ESP32-CAM_Azure_CustomVision_objectDetection.ino
+++ ESP32-CAM_Azure/ESP32-CAM_Azure_CustomVision_objectDetection/ESP32-CAM_Azure_CustomVision_objectDetection.ino
@@ -3,7 +3,7 @@ ESP32-CAM Custom Vision (Object Detection)
 Author : ChungYi Fu (Kaohsiung, Taiwan)  2019-8-26 00:30
 https://www.facebook.com/francefu
 
-Buy me a coffee
+If you like my content, please consider buying me a coffee.
 https://www.buymeacoffee.com/e4LnbiU
 */
 
