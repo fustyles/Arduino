f81b7394a3051ef0b7f0c10e6eaca5739ba3006e
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-09-14 23:24:03 +0800

    Update ESP32-CAM_coco-ssd.ino

--- ESP32-CAM_Tensorflow.js/ESP32-CAM_coco-ssd/ESP32-CAM_coco-ssd.ino
+++ ESP32-CAM_Tensorflow.js/ESP32-CAM_coco-ssd/ESP32-CAM_coco-ssd.ino
@@ -6,7 +6,7 @@ https://www.facebook.com/francefu
 Class
 https://github.com/tensorflow/tfjs-models/blob/master/coco-ssd/src/classes.ts
 
-Buy me a coffee
+If you like my content, please consider buying me a coffee.
 https://www.buymeacoffee.com/e4LnbiU
 */
 
