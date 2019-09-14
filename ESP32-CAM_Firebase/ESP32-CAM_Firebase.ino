f8b26d8be001820fc2f254be0ac1da5c49e296c9
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-09-14 23:21:13 +0800

    Update ESP32-CAM_Firebase.ino

--- ESP32-CAM_Firebase/ESP32-CAM_Firebase.ino
+++ ESP32-CAM_Firebase/ESP32-CAM_Firebase.ino
@@ -9,7 +9,7 @@ https://youtu.be/Hx7bdpev1ug
 How to set up Firebase
 https://iotdesignpro.com/projects/iot-controlled-led-using-firebase-database-and-esp32
 
-Buy me a coffee
+If you like my content, please consider buying me a coffee.
 https://www.buymeacoffee.com/e4LnbiU
 */
 
