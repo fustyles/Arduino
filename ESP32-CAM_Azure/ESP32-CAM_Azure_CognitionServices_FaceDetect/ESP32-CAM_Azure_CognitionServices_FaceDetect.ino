b53ab083dc9a5cc5688820a78b34cadcb02a091e
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-09-14 13:00:00 +0800

    Update ESP32-CAM_Azure_CognitionServices_FaceDetect.ino

--- ESP32-CAM_Azure/ESP32-CAM_Azure_CognitionServices_FaceDetect/ESP32-CAM_Azure_CognitionServices_FaceDetect.ino
+++ ESP32-CAM_Azure/ESP32-CAM_Azure_CognitionServices_FaceDetect/ESP32-CAM_Azure_CognitionServices_FaceDetect.ino
@@ -2,6 +2,9 @@
 ESP32-CAM Azure Cognition Services (Face Detect)
 Author : ChungYi Fu (Kaohsiung, Taiwan)  2019-8-31 00:00
 https://www.facebook.com/francefu
+
+Buy me a coffee
+https://www.buymeacoffee.com/e4LnbiU
 */
 
 // Enter your WiFi ssid and password
