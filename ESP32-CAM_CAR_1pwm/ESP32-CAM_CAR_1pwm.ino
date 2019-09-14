1e92a89de2076b562434c0da31013b0abd8b33f5
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-09-14 23:19:52 +0800

    Update ESP32-CAM_CAR_1pwm.ino

--- ESP32-CAM_CAR_1pwm/ESP32-CAM_CAR_1pwm.ino
+++ ESP32-CAM_CAR_1pwm/ESP32-CAM_CAR_1pwm.ino
@@ -12,7 +12,7 @@ https://github.com/fustyles/Arduino/tree/master/ESP32-CAM_CAR_1pwm1digitalwrite
 http://APIP
 http://STAIP
 
-Buy me a coffee
+If you like my content, please consider buying me a coffee.
 https://www.buymeacoffee.com/e4LnbiU
 */
 
