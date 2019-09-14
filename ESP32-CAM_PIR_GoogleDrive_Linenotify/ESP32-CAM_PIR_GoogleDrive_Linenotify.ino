bb9f9c400a3b38a1a0cb02eed622cd5f05278bcc
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-09-14 12:56:00 +0800

    Update ESP32-CAM_PIR_GoogleDrive_Linenotify.ino

--- ESP32-CAM_PIR_GoogleDrive_Linenotify/ESP32-CAM_PIR_GoogleDrive_Linenotify.ino
+++ ESP32-CAM_PIR_GoogleDrive_Linenotify/ESP32-CAM_PIR_GoogleDrive_Linenotify.ino
@@ -14,6 +14,9 @@ https://www.youtube.com/watch?v=f46VBqWwUuI
 https://script.google.com/home
 https://script.google.com/home/executions
 https://drive.google.com/drive/my-drive
+
+Buy me a coffee
+https://www.buymeacoffee.com/e4LnbiU
 */
 
 // Enter your WiFi ssid and password
