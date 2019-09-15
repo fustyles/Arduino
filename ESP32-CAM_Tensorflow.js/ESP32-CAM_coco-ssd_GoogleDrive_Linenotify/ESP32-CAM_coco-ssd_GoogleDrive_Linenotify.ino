aca4efe4010924ecc852b92832cc376493053f0b
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-09-15 22:08:42 +0800

    Update ESP32-CAM_coco-ssd_GoogleDrive_Linenotify.ino

--- ESP32-CAM_Tensorflow.js/ESP32-CAM_coco-ssd_GoogleDrive_Linenotify/ESP32-CAM_coco-ssd_GoogleDrive_Linenotify.ino
+++ ESP32-CAM_Tensorflow.js/ESP32-CAM_coco-ssd_GoogleDrive_Linenotify/ESP32-CAM_coco-ssd_GoogleDrive_Linenotify.ino
@@ -15,9 +15,6 @@ https://www.youtube.com/watch?v=f46VBqWwUuI
 https://script.google.com/home
 https://script.google.com/home/executions
 https://drive.google.com/drive/my-drive
-
-If you like my content, please consider buying me a coffee.
-https://www.buymeacoffee.com/e4LnbiU
 */
 
 const char* ssid = "xxxxx";
