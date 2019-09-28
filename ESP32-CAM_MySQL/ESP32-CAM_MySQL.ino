6a612fc83a634a4742f3f1ce949d6c3249b21573
fustyles <34397289+fustyles@users.noreply.github.com>
2019-09-28 19:37:42 +0800

    Add files via upload

---
+++ ESP32-CAM_MySQL/ESP32-CAM_MySQL.ino
@@ -0,0 +1,212 @@
+/*
+ESP32-CAM Save a captured photo(Base64) to MySQL. 
+Author : ChungYi Fu (Kaohsiung, Taiwan)  2019-9-28 19:00
+https://www.facebook.com/francefu
+
+Library
+https://github.com/ChuckBell/MySQL_Connector_Arduino
+*/
+
+const char* ssid = "xxxxx";
+const char* password = "xxxxx";
+
+IPAddress server_addr(xxx,xxx,xxx,xxx);
+int port = 3306;
+char dbUser[] = "xxx";
+char dbPassword[] = "xxx";
+String databaseName = "xxx";
+String tableName = "xxx";
+String columnName = "xxx";
+
+#include <WiFi.h>
+#include <MySQL_Connection.h>
+#include <MySQL_Cursor.h>
+WiFiClient client;
+MySQL_Connection conn((Client *)&client);  
+  
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
+#include "Base64.h"
+
+#include "esp_camera.h"
+
+// WARNING!!! Make sure that you have either selected ESP32 Wrover Module,
+//            or another board which has PSRAM enabled
+
+//CAMERA_MODEL_AI_THINKER
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
+void setup() { 
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
+    
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  
+  Serial.println("");
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+
+  long int StartTime=millis();
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      if ((StartTime+10000) < millis()) break;
+  } 
+
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println(""); 
+    Serial.print("Camera Ready! Use 'http://");
+    Serial.print(WiFi.localIP());
+    Serial.println("' to connect");
+    Serial.println("");
+  }
+  else {
+    Serial.println("Connection failed");
+    delay(1000);
+    ESP.restart();    
+    return;
+  }
+   
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  config.frame_size = FRAMESIZE_VGA;  // VGA|CIF|QVGA|HQVGA|QQVGA
+  config.jpeg_quality = 10;
+  config.fb_count = 1;
+  
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    delay(1000);
+    ESP.restart();
+  }
+
+  SaveCapaturedPhoto2mySQL();
+} 
+ 
+void loop() { 
+  delay(10000);
+}
+
+void SaveCapaturedPhoto2mySQL() {
+  Serial.println("Connect to mySQL");
+
+  if (conn.connect(server_addr, port, dbUser, dbPassword)) {
+    Serial.println("Connection successful");
+    
+    camera_fb_t * fb = NULL;
+    fb = esp_camera_fb_get();  
+    if(!fb) {
+      Serial.println("Camera capture failed");
+      delay(1000);
+      ESP.restart();
+      return;
+    }
+  
+    char *input = (char *)fb->buf;
+    char output[base64_enc_len(3)];
+    String imageFile = "data:image/jpeg;base64,";
+    for (int i=0;i<fb->len;i++) {
+      base64_encode(output, (input++), 3);
+      if (i%3==0) imageFile += urlencode(String(output));
+    }
+
+    esp_camera_fb_return(fb);
+
+    Serial.println("Image size = " + String(imageFile.length()));
+    if (imageFile.length()>=65000) {  //TYPE: TEXT
+      Serial.println("Image size is too large. ("+String(imageFile.length())+")");
+      return;
+    }    
+
+    MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn);
+    String INSERT_SQL = "INSERT INTO "+databaseName+"."+tableName+" ("+columnName+") VALUES ('"+imageFile+"')";
+    cur_mem->execute(INSERT_SQL.c_str());  // How to modify the code to upload a big image? (UXGA|SXGA|XGA|SVG)
+    delete cur_mem;
+    
+    Serial.println("Data stored!");
+    Serial.println("");
+  }
+  else {
+    ledcAttachPin(4, 3);
+    ledcSetup(3, 5000, 8);
+    ledcWrite(3,10);
+    delay(200);
+    ledcWrite(3,0);
+    delay(200);    
+    ledcDetachPin(3);
+          
+    Serial.println("Connected to " + String(server_addr) + " failed.");
+  }
+  client.stop();
+}
+
+//https://github.com/zenmanenergy/ESP8266-Arduino-Examples/
+String urlencode(String str)
+{
+    String encodedString="";
+    char c;
+    char code0;
+    char code1;
+    char code2;
+    for (int i =0; i < str.length(); i++){
+      c=str.charAt(i);
+      if (c == ' '){
+        encodedString+= '+';
+      } else if (isalnum(c)){
+        encodedString+=c;
+      } else{
+        code1=(c & 0xf)+'0';
+        if ((c & 0xf) >9){
+            code1=(c & 0xf) - 10 + 'A';
+        }
+        c=(c>>4)&0xf;
+        code0=c+'0';
+        if (c > 9){
+            code0=c - 10 + 'A';
+        }
+        code2='\0';
+        encodedString+='%';
+        encodedString+=code0;
+        encodedString+=code1;
+        //encodedString+=code2;
+      }
+      yield();
+    }
+    return encodedString;
+}
