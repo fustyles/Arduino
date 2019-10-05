bd0f094b61b286a87260ea220bdb2c75bd7cea22
fustyles <34397289+fustyles@users.noreply.github.com>
2019-10-05 23:30:25 +0800

    Update ESP32-CAM_coco-ssd_ObjectTracking_mqtt.ino

--- ESP32-CAM_Webduino/ESP32-CAM_coco-ssd_ObjectTracking_mqtt/ESP32-CAM_coco-ssd_ObjectTracking_mqtt.ino
+++ ESP32-CAM_Webduino/ESP32-CAM_coco-ssd_ObjectTracking_mqtt/ESP32-CAM_coco-ssd_ObjectTracking_mqtt.ino
@@ -1,6 +1,6 @@
 /*
-ESP32-CAM People Tracking with Webduino MQTT (tfjs coco-ssd)
-Author : ChungYi Fu (Kaohsiung, Taiwan)  2019-10-2 21:00
+ESP32-CAM Object Tracking with Webduino MQTT (tfjs coco-ssd)
+Author : ChungYi Fu (Kaohsiung, Taiwan)  2019-10-5 21:00
 https://www.facebook.com/francefu
 
 Class
