9387fafd7079cdd85bc647c30f02a62d5c2c1397
fustyles <34397289+fustyles@users.noreply.github.com>
2019-10-06 23:41:36 +0800

    Update ESP32-CAM_coco-ssd_PeopleTracking.ino

--- ESP32-CAM_Tensorflow.js/ESP32-CAM_coco-ssd_PeopleTracking/ESP32-CAM_coco-ssd_PeopleTracking.ino
+++ ESP32-CAM_Tensorflow.js/ESP32-CAM_coco-ssd_PeopleTracking/ESP32-CAM_coco-ssd_PeopleTracking.ino
@@ -8,6 +8,8 @@ Servo2(Vertical) -> gpio13 (common ground)
 
 Class
 https://github.com/tensorflow/tfjs-models/blob/master/coco-ssd/src/classes.ts
+
+Open the page in Chrome.
 */
 
 const char* ssid = "xxxxx";
