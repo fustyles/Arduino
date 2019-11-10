9cd4bf4acdb383cb150fb15dd7ce147c22fac600
fustyles <34397289+fustyles@users.noreply.github.com>
2019-11-10 09:38:30 +0800

    Update ESP32-CAM_SD.ino

--- ESP32-CAM_SD/ESP32-CAM_SD.ino
+++ ESP32-CAM_SD/ESP32-CAM_SD.ino
@@ -63,7 +63,7 @@ void setup() {
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
   config.pixel_format = PIXFORMAT_JPEG;
-  config.frame_size = FRAMESIZE_UXGA;
+  config.frame_size = FRAMESIZE_UXGA;  // UXGA|SXGA|XGA|SVGA|VGA|CIF|QVGA|HQVGA|QQVGA
   config.jpeg_quality = 10;
   config.fb_count = 1;
 
