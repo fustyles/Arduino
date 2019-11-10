a1db6b4992ddf513b8d438e98c682f1856bcc297
fustyles <34397289+fustyles@users.noreply.github.com>
2019-11-10 09:12:08 +0800

    Add files via upload

---
+++ ESP32-CAM_SD_PhotoManager/ESP32-CAM_SD_PhotoManager.ino
@@ -0,0 +1,660 @@
+/*
+ESP32-CAM (SD Card Photo Manager)
+Author : ChungYi Fu (Kaohsiung, Taiwan)  2019-11-10 09:00
+https://www.facebook.com/francefu
+*/
+
+// Enter your WiFi ssid and password
+const char* ssid     = "xxxxx";   //your network SSID
+const char* password = "xxxxx";   //your network password
+
+const char* apssid = "ESP32-CAM";
+const char* appassword = "12345678";         //AP password require at least 8 characters.
+
+#include <WiFi.h>
+#include "esp_camera.h"
+#include "Base64.h"
+#include "FS.h"
+#include "SD_MMC.h"
+#include "esp_camera.h"
+#include <EEPROM.h>
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
+
+// WARNING!!! Make sure that you have either selected ESP32 Wrover Module,
+//            or another board which has PSRAM enabled
+
+//CAMERA_MODEL_AI_THINKER
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
+WiFiServer server(80);
+
+String Feedback="", Command="",cmd="",P1="",P2="",P3="",P4="",P5="",P6="",P7="",P8="",P9="";
+byte ReceiveState=0,cmdState=1,strState=1,questionstate=0,equalstate=0,semicolonstate=0;
+
+void ExecuteCommand()
+{
+  Serial.println("");
+  //Serial.println("Command: "+Command);
+  Serial.println("cmd= "+cmd+" ,P1= "+P1+" ,P2= "+P2+" ,P3= "+P3+" ,P4= "+P4+" ,P5= "+P5+" ,P6= "+P6+" ,P7= "+P7+" ,P8= "+P8+" ,P9= "+P9);
+  Serial.println("");
+
+  if (cmd=="your cmd")
+  {
+    // You can do anything
+    // Feedback="<font color=\"red\">Hello World</font>";
+  }   
+  else if (cmd=="saveimage")
+  {
+    EEPROM.begin(sizeof(int)*4);
+    EEPROM.write(0, EEPROM.read(0)+1);
+    EEPROM.commit(); 
+    Feedback=saveCapturedImage(String(EEPROM.read(0)))+"<br>"+showimage("/"+String(EEPROM.read(0))+".html")+"<br>"+ListImages(); 
+  } 
+  else if (cmd=="getstill")
+  { 
+    Feedback=getstill();
+  }   
+  else if (cmd=="listimages")
+  {
+    Feedback=ListImages();
+  }  
+  else if (cmd=="showimage")
+  {
+    Feedback=showimage(P1)+"<br>"+ListImages(); 
+  }  
+  else if (cmd=="deleteimage")
+  {
+    Feedback=deleteimage(P1)+"<br>"+ListImages(); 
+  }     
+  else {
+    Feedback="Command is not defined.";
+  }
+}
+
+void setup() {
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
+    
+  Serial.begin(115200);
+  delay(10);
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  config.frame_size = FRAMESIZE_CIF;
+  config.jpeg_quality = 10;
+  config.fb_count = 1;
+
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+  }
+
+  //SD Card
+  if(!SD_MMC.begin()){
+    Serial.println("Card Mount Failed");
+    return;
+  }
+
+  uint8_t cardType = SD_MMC.cardType();
+
+  if(cardType == CARD_NONE){
+    Serial.println("No SD_MMC card attached");
+    SD_MMC.end();
+    return;
+  }
+  Serial.println("");
+  Serial.print("SD_MMC Card Type: ");
+  if(cardType == CARD_MMC){
+      Serial.println("MMC");
+  } else if(cardType == CARD_SD){
+      Serial.println("SDSC");
+  } else if(cardType == CARD_SDHC){
+      Serial.println("SDHC");
+  } else {
+      Serial.println("UNKNOWN");
+  }  
+  Serial.printf("SD_MMC Card Size: %lluMB\n", SD_MMC.cardSize() / (1024 * 1024));
+  Serial.printf("Total space: %lluMB\n", SD_MMC.totalBytes() / (1024 * 1024));
+  Serial.printf("Used space: %lluMB\n", SD_MMC.usedBytes() / (1024 * 1024));
+  Serial.println();
+  
+  SD_MMC.end(); 
+  
+  
+  WiFi.mode(WIFI_AP_STA);
+
+  //WiFi.config(IPAddress(192, 168, 201, 100), IPAddress(192, 168, 201, 2), IPAddress(255, 255, 255, 0));
+
+  WiFi.begin(ssid, password);
+
+  delay(1000);
+  Serial.println("");
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  long int StartTime=millis();
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+      delay(500);
+      if ((StartTime+10000) < millis()) break;
+  } 
+
+  if (WiFi.status() == WL_CONNECTED) {
+    WiFi.softAP((WiFi.localIP().toString()+"_"+(String)apssid).c_str(), appassword);         
+    Serial.println("");
+    Serial.println("STAIP address: ");
+    Serial.println(WiFi.localIP()); 
+  }
+  else {
+    WiFi.softAP((WiFi.softAPIP().toString()+"_"+(String)apssid).c_str(), appassword);    
+  }     
+    
+  //WiFi.softAPConfig(IPAddress(192, 168, 4, 1), IPAddress(192, 168, 4, 1), IPAddress(255, 255, 255, 0)); 
+  Serial.println("");
+  Serial.println("APIP address: ");
+  Serial.println(WiFi.softAPIP());    
+  
+  server.begin();  
+
+  pinMode(4, OUTPUT);
+  digitalWrite(4, LOW);      
+}
+
+void loop() {
+  Feedback="";Command="";cmd="";P1="";P2="";P3="";P4="";P5="";P6="";P7="";P8="";P9="";
+  ReceiveState=0,cmdState=1,strState=1,questionstate=0,equalstate=0,semicolonstate=0;
+  
+   WiFiClient client = server.available();
+
+  if (client) { 
+    String currentLine = "";
+
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();             
+        
+        getCommand(c);
+                
+        if (c == '\n') {
+          if (currentLine.length() == 0) {    
+            if (Feedback=="") {
+              Feedback="Command";
+              Feedback+="<select name=\"cmd\" id=\"cmd\">";
+              Feedback+="<option value=\"getstill\">Get Still</option>";
+              Feedback+="<option value=\"getstilltimer\">Get Still (Timer)</option>";
+              Feedback+="<option value=\"listimages\">List Images</option>"; 
+              Feedback+="<option value=\"saveimage\">Save Image</option>";  
+              Feedback+="</select>";  
+              Feedback+="<script>var myVar;</script>";   
+              Feedback+="<input type=\"button\" value=\"Execute\" onclick=\"clearInterval(myVar);if(document.getElementById(\'cmd\').value==\'getstilltimer\'){myVar = setInterval(function(){document.getElementById(\'execute\').src=\'?getstill\';}, 2000);} else document.getElementById(\'execute\').src=\'?\'+document.getElementById(\'cmd\').value;\">"; 
+              Feedback+="<br><br><iframe id=\"execute\" frameborder=\"0\" width=\"100%\" height=\"500\">";
+            }
+          
+            client.println("HTTP/1.1 200 OK");
+            client.println("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+            client.println("Access-Control-Allow-Methods: GET,POST,PUT,DELETE,OPTIONS");
+            client.println("Content-Type: text/html; charset=utf-8");
+            client.println("Access-Control-Allow-Origin: *");
+            client.println("Connection: close");
+            client.println();
+            client.println("<!DOCTYPE HTML>");
+            client.println("<html><head>");
+            client.println("<meta charset=\"UTF-8\">");
+            client.println("<meta http-equiv=\"Access-Control-Allow-Origin\" content=\"*\">");
+            client.println("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
+            client.println("</head><body>");
+            int Index;
+            for (Index = 0; Index < Feedback.length(); Index = Index+1000) {
+              client.print(Feedback.substring(Index, Index+1000));
+            }
+            client.println("</body></html>");
+            client.println();
+                        
+            Feedback="";
+            break;
+          } else {
+            currentLine = "";
+          }
+        } 
+        else if (c != '\r') {
+          currentLine += c;
+        }
+
+        if ((currentLine.indexOf("/?")!=-1)&&(currentLine.indexOf(" HTTP")!=-1)) {
+          if (Command.indexOf("stop")!=-1) {
+            
+            client.println();
+            client.println();
+            client.stop();
+          }
+          currentLine="";
+          Feedback="";
+          ExecuteCommand();
+        }
+      }
+    }
+    delay(1);
+    client.stop();
+  }
+}
+
+void getCommand(char c) {
+  if (c=='?') ReceiveState=1;
+  if ((c==' ')||(c=='\r')||(c=='\n')) ReceiveState=0;
+  
+  if (ReceiveState==1)
+  {
+    Command=Command+String(c);
+    
+    if (c=='=') cmdState=0;
+    if (c==';') strState++;
+  
+    if ((cmdState==1)&&((c!='?')||(questionstate==1))) cmd=cmd+String(c);
+    if ((cmdState==0)&&(strState==1)&&((c!='=')||(equalstate==1))) P1=P1+String(c);
+    if ((cmdState==0)&&(strState==2)&&(c!=';')) P2=P2+String(c);
+    if ((cmdState==0)&&(strState==3)&&(c!=';')) P3=P3+String(c);
+    if ((cmdState==0)&&(strState==4)&&(c!=';')) P4=P4+String(c);
+    if ((cmdState==0)&&(strState==5)&&(c!=';')) P5=P5+String(c);
+    if ((cmdState==0)&&(strState==6)&&(c!=';')) P6=P6+String(c);
+    if ((cmdState==0)&&(strState==7)&&(c!=';')) P7=P7+String(c);
+    if ((cmdState==0)&&(strState==8)&&(c!=';')) P8=P8+String(c);
+    if ((cmdState==0)&&(strState>=9)&&((c!=';')||(semicolonstate==1))) P9=P9+String(c);
+    
+    if (c=='?') questionstate=1;
+    if (c=='=') equalstate=1;
+    if ((strState>=9)&&(c==';')) semicolonstate=1;
+  }
+}
+
+String getstill() { 
+  camera_fb_t * fb = NULL;
+  fb = esp_camera_fb_get();  
+  if(!fb) {
+    Serial.println("Camera capture failed");
+    return "Camera capture failed";
+  }
+
+  String imageFile = "data:image/jpeg;base64,";
+  char *input = (char *)fb->buf;
+  char output[base64_enc_len(3)];
+  for (int i=0;i<fb->len;i++) {
+    base64_encode(output, (input++), 3);
+    if (i%3==0) imageFile += urlencode(String(output));
+  }
+  esp_camera_fb_return(fb);
+
+  pinMode(4, OUTPUT);
+  digitalWrite(4, LOW); 
+  
+  return "<img src=\'"+imageFile+"\'>";
+}
+
+String ListImages() {
+  //SD Card
+  if(!SD_MMC.begin()){
+    Serial.println("Card Mount Failed");
+    return "Failed";
+  }  
+  
+  fs::FS &fs = SD_MMC; 
+  File root = fs.open("/");
+  if(!root){
+      Serial.println("Failed to open directory");
+      return "Failed";
+  }
+
+  String list="";
+  File file = root.openNextFile();
+  while(file){
+    if(!file.isDirectory()){
+        Serial.print("  FILE: ");
+        Serial.print(file.name());
+        Serial.print("  SIZE: ");
+        Serial.println(file.size());
+        list = "<tr><td><a href=\'?deleteimage="+String(file.name())+"\'>Delete</td><td><a href=\'?showimage="+String(file.name())+"\'>"+String(file.name())+"</td><td>"+String(file.size())+" B</td></tr>"+list;
+    }
+    file = root.openNextFile();
+  }
+  list="<table border=1>"+list+"</table>";
+
+  file.close();
+  SD_MMC.end();
+
+  pinMode(4, OUTPUT);
+  digitalWrite(4, LOW);  
+  
+  return list;
+}
+
+String showimage(String filename) {
+  Serial.printf("Reading file: %s\n", filename);
+
+  //SD Card
+  if(!SD_MMC.begin()){
+    Serial.println("Card Mount Failed");
+    return "Card Mount Failed";
+  }  
+  
+  fs::FS &fs = SD_MMC;
+  File file = fs.open(filename);
+  if(!file){
+    Serial.println("Failed to open file for reading");
+    return "Failed to open file for reading";
+  }
+  Serial.print("Read from file: ");
+  String imageFile="";
+  while(file.available()){
+      char c = file.read();
+      imageFile+=String(c);
+  }
+  file.close();
+  SD_MMC.end();
+
+  pinMode(4, OUTPUT);
+  digitalWrite(4, LOW);
+    
+  return imageFile;
+}
+
+String deleteimage(String filename) {
+  Serial.printf("Reading file: %s\n", filename);
+
+  //SD Card
+  if(!SD_MMC.begin()){
+    Serial.println("Card Mount Failed");
+    return "Card Mount Failed";
+  }  
+  
+  fs::FS &fs = SD_MMC;
+  File file = fs.open(filename);
+  String message="";
+  if(fs.remove(filename)){
+      message=filename + " File deleted";
+  } else {
+      message=filename + " Delete failed";
+  }
+  file.close();
+  SD_MMC.end();
+
+  pinMode(4, OUTPUT);
+  digitalWrite(4, LOW);
+    
+  return message;
+}
+
+String saveCapturedImage(String filename) {
+  String response = ""; 
+  String path = "/"+filename+".html";
+
+  camera_fb_t * fb = NULL;
+  fb = esp_camera_fb_get();  
+  if(!fb) {
+    Serial.println("Camera capture failed");
+    return "";
+  }
+
+  String imageFile = "data:image/jpeg;base64,";
+  char *input = (char *)fb->buf;
+  char output[base64_enc_len(3)];
+  for (int i=0;i<fb->len;i++) {
+    base64_encode(output, (input++), 3);
+    if (i%3==0) imageFile += urlencode(String(output));
+  }
+  imageFile = "<img src=\'"+imageFile+"\'>";
+  esp_camera_fb_return(fb);
+
+  //SD Card
+  if(!SD_MMC.begin()){
+    response = "Card Mount Failed";
+  }  
+  
+  fs::FS &fs = SD_MMC; 
+  Serial.printf("Picture file name: %s\n", path.c_str());
+
+  File file = fs.open(path.c_str(), FILE_WRITE);
+  if(!file){
+    response = "Failed to open file in writing mode";
+  } 
+  else {
+    int Index;
+    for (Index=0;Index<imageFile.length();Index=Index+1000) {
+      file.print(imageFile.substring(Index, Index+1000));
+    }    
+    Serial.printf("Saved file to path: %s\n", path.c_str());
+  }
+  file.close();
+  SD_MMC.end();
+
+  Serial.println("");  
+  esp_camera_fb_return(fb);
+
+  pinMode(4, OUTPUT);
+  digitalWrite(4, LOW);
+
+  return response;
+}
+
+void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
+    Serial.printf("Listing directory: %s\n", dirname);
+
+    File root = fs.open(dirname);
+    if(!root){
+        Serial.println("Failed to open directory");
+        return;
+    }
+    if(!root.isDirectory()){
+        Serial.println("Not a directory");
+        return;
+    }
+
+    File file = root.openNextFile();
+    while(file){
+        if(file.isDirectory()){
+            Serial.print("  DIR : ");
+            Serial.println(file.name());
+            if(levels){
+                listDir(fs, file.name(), levels -1);
+            }
+        } else {
+            Serial.print("  FILE: ");
+            Serial.print(file.name());
+            Serial.print("  SIZE: ");
+            Serial.println(file.size());
+        }
+        file = root.openNextFile();
+    }
+}
+
+void createDir(fs::FS &fs, const char * path){
+    Serial.printf("Creating Dir: %s\n", path);
+    if(fs.mkdir(path)){
+        Serial.println("Dir created");
+    } else {
+        Serial.println("mkdir failed");
+    }
+}
+
+void removeDir(fs::FS &fs, const char * path){
+    Serial.printf("Removing Dir: %s\n", path);
+    if(fs.rmdir(path)){
+        Serial.println("Dir removed");
+    } else {
+        Serial.println("rmdir failed");
+    }
+}
+
+void readFile(fs::FS &fs, const char * path){
+    Serial.printf("Reading file: %s\n", path);
+
+    File file = fs.open(path);
+    if(!file){
+        Serial.println("Failed to open file for reading");
+        return;
+    }
+
+    Serial.print("Read from file: ");
+    while(file.available()){
+        Serial.write(file.read());
+    }
+}
+
+void writeFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Writing file: %s\n", path);
+
+    File file = fs.open(path, FILE_WRITE);
+    if(!file){
+        Serial.println("Failed to open file for writing");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("File written");
+    } else {
+        Serial.println("Write failed");
+    }
+}
+
+void appendFile(fs::FS &fs, const char * path, const char * message){
+    Serial.printf("Appending to file: %s\n", path);
+
+    File file = fs.open(path, FILE_APPEND);
+    if(!file){
+        Serial.println("Failed to open file for appending");
+        return;
+    }
+    if(file.print(message)){
+        Serial.println("Message appended");
+    } else {
+        Serial.println("Append failed");
+    }
+}
+
+void renameFile(fs::FS &fs, const char * path1, const char * path2){
+    Serial.printf("Renaming file %s to %s\n", path1, path2);
+    if (fs.rename(path1, path2)) {
+        Serial.println("File renamed");
+    } else {
+        Serial.println("Rename failed");
+    }
+}
+
+void deleteFile(fs::FS &fs, const char * path){
+    Serial.printf("Deleting file: %s\n", path);
+    if(fs.remove(path)){
+        Serial.println("File deleted");
+    } else {
+        Serial.println("Delete failed");
+    }
+}
+
+void testFileIO(fs::FS &fs, const char * path){
+    File file = fs.open(path);
+    static uint8_t buf[512];
+    size_t len = 0;
+    uint32_t start = millis();
+    uint32_t end = start;
+    if(file){
+        len = file.size();
+        size_t flen = len;
+        start = millis();
+        while(len){
+            size_t toRead = len;
+            if(toRead > 512){
+                toRead = 512;
+            }
+            file.read(buf, toRead);
+            len -= toRead;
+        }
+        end = millis() - start;
+        Serial.printf("%u bytes read for %u ms\n", flen, end);
+        file.close();
+    } else {
+        Serial.println("Failed to open file for reading");
+    }
+
+
+    file = fs.open(path, FILE_WRITE);
+    if(!file){
+        Serial.println("Failed to open file for writing");
+        return;
+    }
+
+    size_t i;
+    start = millis();
+    for(i=0; i<2048; i++){
+        file.write(buf, 512);
+    }
+    end = millis() - start;
+    Serial.printf("%u bytes written for %u ms\n", 2048 * 512, end);
+    file.close();
+}
+
+//https://github.com/zenmanenergy/ESP8266-Arduino-Examples/
+String urlencode(String str)
+{
+    String encodedString="";
+    char c;
+    char code0;
+    char code1;
+    char code2;
+    for (int i =0; i < str.length(); i++){
+      c=str.charAt(i);
+      if (c == ' '){
+        encodedString+= '+';
+      } else if (isalnum(c)){
+        encodedString+=c;
+      } else{
+        code1=(c & 0xf)+'0';
+        if ((c & 0xf) >9){
+            code1=(c & 0xf) - 10 + 'A';
+        }
+        c=(c>>4)&0xf;
+        code0=c+'0';
+        if (c > 9){
+            code0=c - 10 + 'A';
+        }
+        code2='\0';
+        encodedString+='%';
+        encodedString+=code0;
+        encodedString+=code1;
+        //encodedString+=code2;
+      }
+      yield();
+    }
+    return encodedString;
+}
