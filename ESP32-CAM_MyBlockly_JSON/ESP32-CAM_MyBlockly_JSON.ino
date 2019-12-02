4aa64a29c2ef1642ec315aaa27c3e60724c2c2ff
fustyles <34397289+fustyles@users.noreply.github.com>
2019-12-02 23:13:00 +0800

    Update ESP32-CAM_MyBlockly_JSON.ino

--- ESP32-CAM_MyBlockly_JSON/ESP32-CAM_MyBlockly_JSON.ino
+++ ESP32-CAM_MyBlockly_JSON/ESP32-CAM_MyBlockly_JSON.ino
@@ -429,8 +429,7 @@ void ExecuteCommand()
     else if (P1=="CIF")
       s->set_framesize(s, FRAMESIZE_CIF);
     else 
-      s->set_framesize(s, FRAMESIZE_QVGA);      
-    Feedback="{\"data\":\""+P1+"\"}";
+      s->set_framesize(s, FRAMESIZE_QVGA);   
   }   
   else if (cmd=="sendCapturedImageToLineNotify") { 
     Feedback=sendCapturedImageToLineNotify(P1);
