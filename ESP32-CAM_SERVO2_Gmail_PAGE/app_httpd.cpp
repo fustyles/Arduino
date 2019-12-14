4cae69d242e7b2050d2e85cf9273a5eb657a4bc2
fustyles <34397289+fustyles@users.noreply.github.com>
2019-12-14 18:38:09 +0800

    Update app_httpd.cpp

--- ESP32-CAM_SERVO2_Gmail_PAGE/app_httpd.cpp
+++ ESP32-CAM_SERVO2_Gmail_PAGE/app_httpd.cpp
@@ -44,7 +44,7 @@ https://drive.google.com/drive/my-drive
 
 #include "fb_gfx.h"
 #include "fd_forward.h"
-#include "dl_lib.h"
+#include "dl_lib.h"  //delete
 #include "fr_forward.h"
 
 #define ENROLL_CONFIRM_TIMES 5
