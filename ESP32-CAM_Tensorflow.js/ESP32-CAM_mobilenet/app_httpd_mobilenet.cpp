289d44c2536dce590782f16fa9adead8cdd66fbc
fustyles <34397289+fustyles@users.noreply.github.com>
2019-12-14 18:41:01 +0800

    Update app_httpd_mobilenet.cpp

--- ESP32-CAM_Tensorflow.js/ESP32-CAM_mobilenet/app_httpd_mobilenet.cpp
+++ ESP32-CAM_Tensorflow.js/ESP32-CAM_mobilenet/app_httpd_mobilenet.cpp
@@ -27,7 +27,7 @@ Open the page in Chrome.
 
 #include "fb_gfx.h"
 #include "fd_forward.h"
-#include "dl_lib.h"
+#include "dl_lib.h"  //delete
 #include "fr_forward.h"
 
 #define ENROLL_CONFIRM_TIMES 5
