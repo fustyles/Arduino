f7a2cbc51698a6b77982ebbe33a2a68c12a80775
fustyles <34397289+fustyles@users.noreply.github.com>
2019-12-30 14:03:11 +0800

    Update ESP32-CAM_FaceRecognition_PAGE.ino

--- ESP32-CAM_FaceRecognition_PAGE/ESP32-CAM_FaceRecognition_PAGE.ino
+++ ESP32-CAM_FaceRecognition_PAGE/ESP32-CAM_FaceRecognition_PAGE.ino
@@ -11,7 +11,7 @@ http://192.168.xxx.xxx/status      //取得視訊參數值
 //自訂指令格式
 http://192.168.xxx.xxx/control?facename=matched_id;name  //設定姓名
 
-//設定視訊參數(官方指令格式)
+//官方指令格式 (設定視訊參數)
 http://192.168.xxx.xxx/control?var=framesize&val=value    // value = 10->UXGA(1600x1200), 9->SXGA(1280x1024), 8->XGA(1024x768) ,7->SVGA(800x600), 6->VGA(640x480), 5 selected=selected->CIF(400x296), 4->QVGA(320x240), 3->HQVGA(240x176), 0->QQVGA(160x120)
 http://192.168.xxx.xxx/control?var=quality&val=value    // value = 10 ~ 63
 http://192.168.xxx.xxx/control?var=brightness&val=value    // value = -2 ~ 2
