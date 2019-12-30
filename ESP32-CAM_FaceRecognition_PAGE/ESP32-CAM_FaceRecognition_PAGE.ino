3f357957607c9627640304b0ca75dd45bd435df5
fustyles <34397289+fustyles@users.noreply.github.com>
2019-12-30 22:11:31 +0800

    Update ESP32-CAM_FaceRecognition_PAGE.ino

--- ESP32-CAM_FaceRecognition_PAGE/ESP32-CAM_FaceRecognition_PAGE.ino
+++ ESP32-CAM_FaceRecognition_PAGE/ESP32-CAM_FaceRecognition_PAGE.ino
@@ -13,10 +13,10 @@ http://192.168.xxx.xxx/control?facename=matched_id;name  //設定姓名
 
 //官方指令格式 (設定視訊參數)
 http://192.168.xxx.xxx/control?var=framesize&val=value    // value = 10->UXGA(1600x1200), 9->SXGA(1280x1024), 8->XGA(1024x768) ,7->SVGA(800x600), 6->VGA(640x480), 5 selected=selected->CIF(400x296), 4->QVGA(320x240), 3->HQVGA(240x176), 0->QQVGA(160x120)
-http://192.168.xxx.xxx/control?var=quality&val=value    // value = 10 ~ 63
-http://192.168.xxx.xxx/control?var=brightness&val=value    // value = -2 ~ 2
-http://192.168.xxx.xxx/control?var=contrast&val=value    // value = -2 ~ 2 
-http://192.168.xxx.xxx/control?var=flash&val=value    // value = 0 ~ 255
+http://192.168.xxx.xxx/control?var=quality&val=value    // value = 10 to 63
+http://192.168.xxx.xxx/control?var=brightness&val=value    // value = -2 to 2
+http://192.168.xxx.xxx/control?var=contrast&val=value    // value = -2 to 2 
+http://192.168.xxx.xxx/control?var=flash&val=value    // value = 0 to 255
 */
 
 //輸入WIFI連線帳號密碼
