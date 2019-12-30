5a89de7f8659528dcc1c3931894899bdd12b4658
fustyles <34397289+fustyles@users.noreply.github.com>
2019-12-30 22:10:42 +0800

    Update ESP32-CAM_FaceRecognition_PAGE.ino

--- ESP32-CAM_FaceRecognition_PAGE/ESP32-CAM_FaceRecognition_PAGE.ino
+++ ESP32-CAM_FaceRecognition_PAGE/ESP32-CAM_FaceRecognition_PAGE.ino
@@ -1,6 +1,6 @@
 /*
 ESP32-CAM Face Recognition
-Author : ChungYi Fu (Kaohsiung, Taiwan)  2019-12-26 01:00
+Author : ChungYi Fu (Kaohsiung, Taiwan)  2019-12-30 22:00
 https://www.facebook.com/francefu
 
 http://192.168.xxx.xxx             //網頁首頁管理介面
