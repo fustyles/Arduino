ca5167bd66d1bbca61c97107508d3cf06906acf4
fustyles <34397289+fustyles@users.noreply.github.com>
2020-01-02 22:20:12 +0800

    Add files via upload

--- ESP32-CAM_SD_Manager_PAGE/ESP32-CAM_SD_Manager_PAGE.ino
+++ ESP32-CAM_SD_Manager_PAGE/ESP32-CAM_SD_Manager_PAGE.ino
@@ -1,6 +1,6 @@
 /*
 ESP32-CAM (SD Card Manager)
-Author : ChungYi Fu (Kaohsiung, Taiwan)  2020-1-2 18:00
+Author : ChungYi Fu (Kaohsiung, Taiwan)  2020-1-2 22:00
 https://www.facebook.com/francefu
 
 Arduino IDE settings
