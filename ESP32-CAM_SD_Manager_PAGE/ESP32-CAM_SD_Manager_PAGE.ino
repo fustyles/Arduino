39597798bb0302e1aa5d39cbae1fc4ce94a0c9f3
fustyles <34397289+fustyles@users.noreply.github.com>
2020-01-02 13:51:40 +0800

    Add files via upload

--- ESP32-CAM_SD_Manager_PAGE/ESP32-CAM_SD_Manager_PAGE.ino
+++ ESP32-CAM_SD_Manager_PAGE/ESP32-CAM_SD_Manager_PAGE.ino
@@ -85,7 +85,7 @@ void ExecuteCommand()
   Serial.println("");
   
   if (cmd=="your cmd") {
-    // You can do anything
+    // You can do anything.
     // Feedback="<font color=\"red\">Hello World</font>";
   }
   else if (cmd=="ip") {
@@ -635,41 +635,6 @@ String saveCapturedImage(String filename) {
   digitalWrite(4, LOW);  
   
   return response;
-}
-
-//https://github.com/zenmanenergy/ESP8266-Arduino-Examples/
-String urlencode(String str)
-{
-    String encodedString="";
-    char c;
-    char code0;
-    char code1;
-    char code2;
-    for (int i =0; i < str.length(); i++){
-      c=str.charAt(i);
-      if (c == ' '){
-        encodedString+= '+';
-      } else if (isalnum(c)){
-        encodedString+=c;
-      } else{
-        code1=(c & 0xf)+'0';
-        if ((c & 0xf) >9){
-            code1=(c & 0xf) - 10 + 'A';
-        }
-        c=(c>>4)&0xf;
-        code0=c+'0';
-        if (c > 9){
-            code0=c - 10 + 'A';
-        }
-        code2='\0';
-        encodedString+='%';
-        encodedString+=code0;
-        encodedString+=code1;
-        //encodedString+=code2;
-      }
-      yield();
-    }
-    return encodedString;
 }
 
 //拆解命令字串置入變數
