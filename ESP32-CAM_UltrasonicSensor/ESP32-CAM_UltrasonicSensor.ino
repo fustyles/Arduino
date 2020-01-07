c0d225dd511a0b0e22b9d61b193319421571c7c6
fustyles <34397289+fustyles@users.noreply.github.com>
2020-01-07 20:51:25 +0800

    Add files via upload

---
+++ ESP32-CAM_UltrasonicSensor/ESP32-CAM_UltrasonicSensor.ino
@@ -0,0 +1,123 @@
+/*
+ESP32-CAM Ping Ultrasonic Sensor (Control Flash)
+Author : ChungYi Fu (Kaohsiung, Taiwan)  2020-1-7 21:00
+https://www.facebook.com/francefu
+*/
+
+//Ping Ultrasonic Sensor
+int trigPin = 2;   //Trig
+int echoPin = 13 ;   //Echo
+int limitDistance = 15;
+//Flash
+boolean flashState = false;
+
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
+
+#include "esp_camera.h"
+
+// WARNING!!! Make sure that you have either selected ESP32 Wrover Module,
+//            or another board which has PSRAM enabled
+
+//CAMERA_MODEL_AI_THINKER
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
+void setup()
+{
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
+  
+  Serial.begin(115200);
+  delay(10);
+  
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  //init with high specs to pre-allocate larger buffers
+  if(psramFound()){
+    config.frame_size = FRAMESIZE_UXGA;
+    config.jpeg_quality = 10;  //0-63 lower number means higher quality
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;  //0-63 lower number means higher quality
+    config.fb_count = 1;
+  }
+  
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    delay(1000);
+    ESP.restart();
+  }
+
+  //drop down frame size for higher initial frame rate
+  sensor_t * s = esp_camera_sensor_get();
+  s->set_framesize(s, FRAMESIZE_VGA);  // UXGA|SXGA|XGA|SVGA|VGA|CIF|QVGA|HQVGA|QQVGA
+  
+  //Flash
+  ledcAttachPin(4, 4);  
+  ledcSetup(4, 5000, 8);
+
+  pinMode(trigPin, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  unsigned long distance_cm = pulseIn(echoPin, HIGH)/58;  
+  unsigned long distance_in = distance_cm*2.54;  
+  Serial.println(String(distance_cm)+" cm, " + String(distance_in)+" inch");
+  
+  if (distance_cm>0&&distance_cm<=limitDistance) {
+    if (flashState==false) {
+      flashState = true;
+      ledcWrite(4,20); //Turn on the flash.
+    }
+    
+  }
+  else {
+    if (flashState==true) {
+      flashState = false;
+      ledcWrite(4,0); //Turn off the flash.
+    }
+  }  
+
+  delay(100);
+}
