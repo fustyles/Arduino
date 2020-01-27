d570cf3462b3c9e1b3dcb0cf8639d20309e7a1b8
fustyles <34397289+fustyles@users.noreply.github.com>
2020-01-28 00:58:24 +0800

    Add files via upload

--- ESP32-CAM_Tensorflow.js/ESP32-CAM_bodypix/ESP32-CAM_bodypix.ino
+++ ESP32-CAM_Tensorflow.js/ESP32-CAM_bodypix/ESP32-CAM_bodypix.ino
@@ -1,7 +1,7 @@
 /*
 ESP32-CAM BodyPix
 Open the page in Chrome.
-Author : ChungYi Fu (Kaohsiung, Taiwan)  2019-12-24 01:00
+Author : ChungYi Fu (Kaohsiung, Taiwan)  2020-1-28 01:00
 https://www.facebook.com/francefu
 
 自訂指令格式 :  
@@ -683,7 +683,16 @@ static const char PROGMEM INDEX_HTML[] = R"rawliteral(<!doctype html>
         <section class="main">
             <section id="buttons">
                 <table>
-                <tr><td><button id="get-still" style="display:none">Restart</button></td><td>Style<select id="kind"><option value="1">backgroundDarkeningMask</option><option value="2">segmentation</option><option value="3">coloredPartImageData</option><option value="4">coloredPartImage</option></select></td><td style="display:none"><button id="toggle-stream"></button></td></tr>
+                <tr><td><button id="get-still">Restart</button></td>
+                  <td>Style
+                  <select id="kind">
+                  <option value="1">backgroundDarkeningMask</option>
+                  <option value="2">segmentation</option>
+                  <option value="3">coloredPartImageData</option>
+                  <option value="4">coloredPartImage</option>
+                  <option value="5">drawPixelatedMask</option>
+                  </select>
+                  </td><td style="display:none"><button id="toggle-stream"></button></td></tr>
                 <tr><td>Flash</td><td align="center" colspan="2"><input type="range" id="flash" min="0" max="255" value="0" onchange="try{fetch(document.location.origin+'/control?flash='+this.value);}catch(e){}"></td></tr>
                 </table>
             </section>
@@ -691,11 +700,12 @@ static const char PROGMEM INDEX_HTML[] = R"rawliteral(<!doctype html>
               <div id="stream-container" class="image-container hidden">
                 <div class="close" id="close-stream">×</div>
                 <img id="stream" src="" style="display:none"><br>
-                <canvas id="canvas"></canvas><br>
-                <canvas id="canvas1"></canvas><br>
-                <canvas id="canvas2"></canvas><br>
-                <canvas id="canvas3"></canvas><br>
-                <canvas id="canvas4"></canvas>                 
+                <canvas id="canvas" style="display:none"></canvas>
+                <canvas id="canvas1"></canvas>
+                <canvas id="canvas2"></canvas>
+                <canvas id="canvas3"></canvas>
+                <canvas id="canvas4"></canvas> 
+                <canvas id="canvas5"></canvas>                                
               </div>
             </figure>         
             <div id="logo">
@@ -1061,10 +1071,9 @@ static const char PROGMEM INDEX_HTML[] = R"rawliteral(<!doctype html>
     var canvas1 = document.getElementById('canvas1'); 
     var canvas2 = document.getElementById('canvas2');
     var canvas3 = document.getElementById('canvas3');
-    var canvas4 = document.getElementById('canvas4');     
+    var canvas4 = document.getElementById('canvas4');  
+    var canvas5 = document.getElementById('canvas5'); 
     var Model;
-    var outputStride = 16;
-    var segmentationThreshold = 0.5;
     
     function ObjectDetect() {
       result.innerHTML = "Please wait for loading model.";
@@ -1087,67 +1096,140 @@ static const char PROGMEM INDEX_HTML[] = R"rawliteral(<!doctype html>
       canvas3.setAttribute("height", ShowImage.height);     
       canvas4.setAttribute("width", ShowImage.width);
       canvas4.setAttribute("height", ShowImage.height);
+      canvas5.setAttribute("width", ShowImage.width);
+      canvas5.setAttribute("height", ShowImage.height);      
       context.drawImage(ShowImage, 0, 0, ShowImage.width, ShowImage.height); 
       
-      Model.estimatePersonSegmentation(canvas, outputStride, segmentationThreshold).then(segmentation => {
-        
-        const maskBackground = true;
-        const backgroundDarkeningMask = bodyPix.toMaskImageData(segmentation, maskBackground);
-        const opacity = 0.7;
-        const maskBlurAmount = 3;
-        const flipHorizontal = false;
-        const backgroundBlurAmount = 3;
-        const edgeBlurAmount = 3;
-
+      const rainbow = [
+      [110, 64, 170], [143, 61, 178], [178, 60, 178], [210, 62, 167],
+      [238, 67, 149], [255, 78, 125], [255, 94, 99],  [255, 115, 75],
+      [255, 140, 56], [239, 167, 47], [217, 194, 49], [194, 219, 64],
+      [175, 240, 91], [135, 245, 87], [96, 247, 96],  [64, 243, 115],
+      [40, 234, 141], [28, 219, 169], [26, 199, 194], [33, 176, 213],
+      [47, 150, 224], [65, 125, 224], [84, 101, 214], [99, 81, 195]
+      ];
+    
+      const warm = [
+      [110, 64, 170], [106, 72, 183], [100, 81, 196], [92, 91, 206],
+      [84, 101, 214], [75, 113, 221], [66, 125, 224], [56, 138, 226],
+      [48, 150, 224], [40, 163, 220], [33, 176, 214], [29, 188, 205],
+      [26, 199, 194], [26, 210, 182], [28, 219, 169], [33, 227, 155],
+      [41, 234, 141], [51, 240, 128], [64, 243, 116], [79, 246, 105],
+      [96, 247, 97],  [115, 246, 91], [134, 245, 88], [155, 243, 88]
+      ];
+      
+      const spectral = [
+      [158, 1, 66],    [181, 26, 71],   [202, 50, 74],   [219, 73, 74],
+      [232, 94, 73],   [242, 117, 75],  [248, 142, 83],  [251, 167, 96],
+      [253, 190, 112], [254, 210, 129], [254, 227, 149], [254, 240, 166],
+      [251, 248, 176], [243, 249, 172], [231, 245, 163], [213, 238, 159],
+      [190, 229, 160], [164, 218, 163], [137, 207, 165], [110, 192, 168],
+      [86, 173, 174],  [70, 150, 179],  [67, 127, 180],  [77, 103, 173]
+      ];
+    
+      if (kind.value==1||kind.value==2||kind.value==3||kind.value==4) {
+        var outputStride = 16;
+        var segmentationThreshold = 0.5;
+        Model.estimatePersonSegmentation(ShowImage, outputStride, segmentationThreshold).then(segmentation => {
+        //console.log(segmentation);
+          
         if (kind.value=="1") {
           canvas1.style.display="block";
+    
+          const maskBackground = true;
+          const backgroundDarkeningMask = bodyPix.toMaskImageData(segmentation, maskBackground);
+          const opacity = 0.8;
+          const maskBlurAmount = 3;
+          const flipHorizontal = false;
           bodyPix.drawMask(canvas1, canvas, backgroundDarkeningMask, opacity, maskBlurAmount, flipHorizontal);
-        }
-        else
+        } else
           canvas1.style.display="none";
-          
+        
         if (kind.value=="2") {
           canvas2.style.display="block";
+    
+          const backgroundBlurAmount = 5;
+          const edgeBlurAmount = 3;
+          const flipHorizontal = false;
           bodyPix.drawBokehEffect(canvas2, canvas, segmentation, backgroundBlurAmount, edgeBlurAmount, flipHorizontal);
-        }
-        else
-          canvas2.style.display="none";   
-               
+        } else
+          canvas2.style.display="none";
+        
         if (kind.value=="3") {
           canvas3.style.display="block";
-          const rainbow = [
-            [110, 64, 170], [143, 61, 178], [178, 60, 178], [210, 62, 167],
-            [238, 67, 149], [255, 78, 125], [255, 94, 99],  [255, 115, 75],
-            [255, 140, 56], [239, 167, 47], [217, 194, 49], [194, 219, 64],
-            [175, 240, 91], [135, 245, 87], [96, 247, 96],  [64, 243, 115],
-            [40, 234, 141], [28, 219, 169], [26, 199, 194], [33, 176, 213],
-            [47, 150, 224], [65, 125, 224], [84, 101, 214], [99, 81, 195]
-          ];
-          const coloredPartImageData = bodyPix.toColoredPartImageData(segmentation, rainbow);
+          const coloredPartImage = bodyPix.toColoredPartImageData(segmentation, rainbow);
+          //const coloredPartImage = bodyPix.toColoredPartImageData(segmentation, warm);
+          //const coloredPartImage = bodyPix.toColoredPartImageData(segmentation, spectral);
+    
+          const maskBackground = true;
+          const maskImage = bodyPix.toMaskImageData(segmentation, maskBackground);
+    
+          const opacity = 0.7;
+          const maskBlurAmount = 3;
+          const flipHorizontal = false;
           const pixelCellWidth = 10.0;
-          bodyPix.drawPixelatedMask(canvas3, canvas, coloredPartImageData, opacity, maskBlurAmount, flipHorizontal, pixelCellWidth);
-        }
-        else
-          canvas3.style.display="none";   
-               
+          //bodyPix.drawPixelatedMask(canvas3, canvas, coloredPartImageData, opacity, maskBlurAmount, flipHorizontal, pixelCellWidth);
+          bodyPix.drawPixelatedMask(canvas3, canvas, maskImage, opacity, maskBlurAmount, flipHorizontal, pixelCellWidth);
+        } else
+          canvas3.style.display="none";
+    
         if (kind.value=="4") {
           canvas4.style.display="block";
-          const warm = [
-            [110, 64, 170], [106, 72, 183], [100, 81, 196], [92, 91, 206],
-            [84, 101, 214], [75, 113, 221], [66, 125, 224], [56, 138, 226],
-            [48, 150, 224], [40, 163, 220], [33, 176, 214], [29, 188, 205],
-            [26, 199, 194], [26, 210, 182], [28, 219, 169], [33, 227, 155],
-            [41, 234, 141], [51, 240, 128], [64, 243, 116], [79, 246, 105],
-            [96, 247, 97],  [115, 246, 91], [134, 245, 88], [155, 243, 88]
-          ];
-          const coloredPartImage = bodyPix.toColoredPartImageData(segmentation, warm);
-          bodyPix.drawMask(canvas4, canvas, coloredPartImage, 1.0, maskBlurAmount, flipHorizontal);
-        }
-        else
-          canvas4.style.display="none";
           
-        getStill.click();
-      });   
+          const coloredPartImage = bodyPix.toColoredPartImageData(segmentation, rainbow);
+          //const coloredPartImage = bodyPix.toColoredPartImageData(segmentation, warm);
+          //const coloredPartImage = bodyPix.toColoredPartImageData(segmentation, spectral);
+          const invert = true;
+          const opacity = 1;
+          const flipHorizontal = false;
+          const maskBlurAmount = 0;
+          bodyPix.drawMask(canvas4, canvas, coloredPartImage, opacity, maskBlurAmount, flipHorizontal);
+        } else
+          canvas4.style.display="none";
+        
+        canvas5.style.display="none";
+        try { 
+          document.createEvent("TouchEvent");
+          segmentationThreshold = 0.5;
+        }
+        catch(e) { 
+          segmentationThreshold = 0.75;
+        }   
+        });
+      }
+      else {
+        var outputStride = 16;
+        var segmentationThreshold = 0.5;
+        Model.estimatePartSegmentation(ShowImage, outputStride, segmentationThreshold).then(segmentation => {
+    
+        if (kind.value=="5") {
+          canvas5.style.display="block";
+    
+          const coloredPartImage = bodyPix.toColoredPartImageData(segmentation, rainbow);
+          //const coloredPartImage = bodyPix.toColoredPartImageData(segmentation, warm);
+          //const coloredPartImage = bodyPix.toColoredPartImageData(segmentation, spectral);
+          const opacity = 0.7;
+          const flipHorizontal = false;
+          const maskBlurAmount = 0;
+          const pixelCellWidth = 10.0;
+          bodyPix.drawPixelatedMask(canvas5, canvas, coloredPartImage, opacity, maskBlurAmount, flipHorizontal, pixelCellWidth);
+        } else
+          canvas5.style.display="none";
+    
+          canvas1.style.display="none";
+          canvas2.style.display="none";
+          canvas3.style.display="none";
+          canvas4.style.display="none";
+          try { 
+            document.createEvent("TouchEvent");
+            segmentationThreshold = 0.5;
+          }
+          catch(e) { 
+            segmentationThreshold = 0.75;
+          }   
+        });
+      }
+      setTimeout(function(){getStill.click(); }, 100); 
     }
     
     ShowImage.onload = function (event) {
