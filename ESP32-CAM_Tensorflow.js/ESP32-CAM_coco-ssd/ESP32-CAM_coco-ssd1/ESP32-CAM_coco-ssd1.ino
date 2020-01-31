65b6d7fd46a59ae7f35ebbec766000e7efa1e05c
fustyles <34397289+fustyles@users.noreply.github.com>
2020-01-31 23:06:43 +0800

    Update ESP32-CAM_coco-ssd1.ino

--- ESP32-CAM_Tensorflow.js/ESP32-CAM_coco-ssd/ESP32-CAM_coco-ssd1/ESP32-CAM_coco-ssd1.ino
+++ ESP32-CAM_Tensorflow.js/ESP32-CAM_coco-ssd/ESP32-CAM_coco-ssd1/ESP32-CAM_coco-ssd1.ino
@@ -37,8 +37,8 @@ http://192.168.xxx.xxx?sendCapturedImageToLineNotify=token  //傳送影像截圖
 */
 
 //輸入WIFI連線帳號密碼
-const char* ssid     = "3COM";   //your network SSID
-const char* password = "godblessyou";   //your network password
+const char* ssid     = "*****";   //your network SSID
+const char* password = "*****";   //your network password
 
 //輸入AP端連線帳號密碼
 const char* apssid = "ESP32-CAM";
