f2a1488714c70d50f5abe4cf099f656f393cb83d
fustyles <34397289+fustyles@users.noreply.github.com>
2020-02-04 14:55:43 +0800

    Add files via upload

--- ESP32-CAM_Tensorflow.js/ESP32-CAM_mobilenet/ESP32-CAM_mobilenet1/ESP32-CAM_mobilenet1.ino
+++ ESP32-CAM_Tensorflow.js/ESP32-CAM_mobilenet/ESP32-CAM_mobilenet1/ESP32-CAM_mobilenet1.ino
@@ -1,6 +1,6 @@
 /*
 ESP32-CAM (tfjs mobilenet)
-Author : ChungYi Fu (Kaohsiung, Taiwan)  2020-2-1 16:00
+Author : ChungYi Fu (Kaohsiung, Taiwan)  2020-2-4 18:00
 https://www.facebook.com/francefu
 
 物件列表
@@ -422,15 +422,30 @@ static const char PROGMEM INDEX_HTML[] = R"rawliteral(
     var ifr = document.getElementById('ifr');
     var lastValue="";
     var myTimer;   
+    var restartCount=0;    
     var Model;    
 
-    getStill.onclick = function (event) {  
-      myTimer = setInterval(function(){getStill.click();},5000);
+    getStill.onclick = function (event) {
+      clearInterval(myTimer);  
+      myTimer = setInterval(function(){error_handle();},5000);
       ShowImage.src=location.origin+'/?getstill='+Math.random();
     }
 
+    function error_handle() {
+      restartCount++;
+      clearInterval(myTimer);
+      if (restartCount<=2) {
+        result.innerHTML = "Get still error. <br>Restart ESP32-CAM "+restartCount+" times.";
+        myTimer = setInterval(function(){getStill.click();},10000);
+        ifr.src = document.location.origin+'?restart';
+      }
+      else
+        result.innerHTML = "Get still error. <br>Please close the page and check ESP32-CAM.";
+    }
+    
     ShowImage.onload = function (event) {
       clearInterval(myTimer);
+      restartCount=0;      
       canvas.setAttribute("width", ShowImage.width);
       canvas.setAttribute("height", ShowImage.height);
       if (mirrorimage.value==1) {
