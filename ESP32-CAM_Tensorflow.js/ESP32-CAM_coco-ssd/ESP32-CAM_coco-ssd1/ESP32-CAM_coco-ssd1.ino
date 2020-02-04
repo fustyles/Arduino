714435195b62b16a883d48251a4953d272670dcc
fustyles <34397289+fustyles@users.noreply.github.com>
2020-02-04 14:15:25 +0800

    Add files via upload

--- ESP32-CAM_Tensorflow.js/ESP32-CAM_coco-ssd/ESP32-CAM_coco-ssd1/ESP32-CAM_coco-ssd1.ino
+++ ESP32-CAM_Tensorflow.js/ESP32-CAM_coco-ssd/ESP32-CAM_coco-ssd1/ESP32-CAM_coco-ssd1.ino
@@ -1,6 +1,6 @@
 /*
 ESP32-CAM (tfjs coco-ssd)
-Author : ChungYi Fu (Kaohsiung, Taiwan)  2020-2-1 15:00
+Author : ChungYi Fu (Kaohsiung, Taiwan)  2020-2-4 18:00
 https://www.facebook.com/francefu
 
 物件類別
@@ -18,6 +18,8 @@ http://STAIP/control?cmd=P1;P2;P3;P4;P5;P6;P7;P8;P9
 http://192.168.xxx.xxx?ip
 http://192.168.xxx.xxx?mac
 http://192.168.xxx.xxx?restart
+http://192.168.xxx.xxx?digitalwrite=pin;value
+http://192.168.xxx.xxx?analogwrite=pin;value
 http://192.168.xxx.xxx?flash=value        //value= 0~255 閃光燈
 http://192.168.xxx.xxx?getstill                 //取得視訊影像
 http://192.168.xxx.xxx?framesize=size     //size= UXGA|SXGA|XGA|SVGA|VGA|CIF|QVGA|HQVGA|QQVGA 改變影像解析度
@@ -114,6 +116,23 @@ void ExecuteCommand()
   }    
   else if (cmd=="restart") {
     ESP.restart();
+  }    
+  else if (cmd=="digitalwrite") {
+    ledcDetachPin(P1.toInt());
+    pinMode(P1.toInt(), OUTPUT);
+    digitalWrite(P1.toInt(), P2.toInt());
+  }   
+  else if (cmd=="analogwrite") {
+    if (P1="4") {
+      ledcAttachPin(4, 4);  
+      ledcSetup(4, 5000, 8);   
+      ledcWrite(4,P2.toInt());  
+    }
+    else {
+      ledcAttachPin(P1.toInt(), 5);
+      ledcSetup(5, 5000, 8);
+      ledcWrite(5,P2.toInt());
+    }
   }    
   else if (cmd=="flash") {
     ledcAttachPin(4, 4);  
@@ -503,16 +522,31 @@ static const char PROGMEM INDEX_HTML[] = R"rawliteral(
     var flash = document.getElementById('flash'); 
     var ifr = document.getElementById('ifr');
     var lastValue="";
-    var myTimer;   
+    var myTimer;
+    var restartCount=0;
     var Model;    
 
-    getStill.onclick = function (event) {  
-      myTimer = setInterval(function(){getStill.click();},5000);
+    getStill.onclick = function (event) {
+      clearInterval(myTimer);  
+      myTimer = setInterval(function(){error_handle();},5000);
       ShowImage.src=location.origin+'/?getstill='+Math.random();
     }
 
+    function error_handle() {
+      restartCount++;
+      clearInterval(myTimer);
+      if (restartCount<=2) {
+        result.innerHTML = "Get still error. <br>Restart ESP32-CAM "+restartCount+" times.";
+        myTimer = setInterval(function(){getStill.click();},10000);
+        ifr.src = document.location.origin+'?restart';
+      }
+      else
+        result.innerHTML = "Get still error. <br>Please close the page and check ESP32-CAM.";
+    }    
+
     ShowImage.onload = function (event) {
       clearInterval(myTimer);
+      restartCount=0;
       canvas.setAttribute("width", ShowImage.width);
       canvas.setAttribute("height", ShowImage.height);
       if (mirrorimage.value==1) {
