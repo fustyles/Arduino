8cfa664af8a570b28667936f4b3c8fd84f0f65ad
fustyles <34397289+fustyles@users.noreply.github.com>
2020-02-17 21:21:51 +0800

    Update ESP32-CAM_OTTO_FaceRecognition.ino

--- ESP32-CAM_OTTO/OttoRobot_20161204/ESP32-CAM_OTTO_FaceRecognition/ESP32-CAM_OTTO_FaceRecognition.ino
+++ ESP32-CAM_OTTO/OttoRobot_20161204/ESP32-CAM_OTTO_FaceRecognition/ESP32-CAM_OTTO_FaceRecognition.ino
@@ -3,8 +3,8 @@ ESP32-CAM OTTO ROBOT (Face Recognition)
 Author : ChungYi Fu (Kaohsiung, Taiwan)  2020-2-18 20:00
 https://www.facebook.com/francefu
 
-ESP32-CAM RX -> Arduino NANO pin 7
-ESP32-CAM TX -> Arduino NANO pin 6
+ESP32-CAM RX -> Arduino NANO TX
+ESP32-CAM TX -> Arduino NANO RX
 
 http://APIP
 http://STAIP
