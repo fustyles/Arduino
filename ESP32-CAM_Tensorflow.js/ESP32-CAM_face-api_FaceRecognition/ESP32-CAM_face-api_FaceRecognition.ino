cb50df3f56532ddb6af3d0482949eed831a6af22
fustyles <34397289+fustyles@users.noreply.github.com>
2020-02-18 08:03:33 +0800

    Update ESP32-CAM_face-api_FaceRecognition.ino

--- ESP32-CAM_Tensorflow.js/ESP32-CAM_face-api_FaceRecognition/ESP32-CAM_face-api_FaceRecognition.ino
+++ ESP32-CAM_Tensorflow.js/ESP32-CAM_face-api_FaceRecognition/ESP32-CAM_face-api_FaceRecognition.ino
@@ -3,6 +3,8 @@ ESP32-CAM Face Recognition (face-api.js)
 Author : ChungYi Fu (Kaohsiung, Taiwan)  2020-2-18 00:30
 https://www.facebook.com/francefu
 
+The canvas in Chrome will cause memory leak.
+
 Easy Face Recognition Tutorial With JavaScript
 https://www.youtube.com/watch?v=AZ4PdALMqx0
 
