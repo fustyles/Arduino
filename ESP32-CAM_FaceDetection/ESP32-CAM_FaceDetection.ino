ee58ec4a592a9c84c200d1ba05fa99178bdb1e03
fustyles <34397289+fustyles@users.noreply.github.com>
2020-05-04 01:16:43 +0800

    Update ESP32-CAM_FaceDetection.ino

--- ESP32-CAM_FaceDetection/ESP32-CAM_FaceDetection.ino
+++ ESP32-CAM_FaceDetection/ESP32-CAM_FaceDetection.ino
@@ -1,6 +1,6 @@
 /*
 ESP32-CAM Face detection
-Author : ChungYi Fu (Kaohsiung, Taiwan)  2020-5-4 00:00
+Author : ChungYi Fu (Kaohsiung, Taiwan)  2020-5-4 01:00
 https://www.facebook.com/francefu
 */
 
