6b7aece1ab575a84683abd141d22972815ce1d22
fustyles <34397289+fustyles@users.noreply.github.com>
2020-05-05 21:36:02 +0800

    Update ESP32-CAM_FaceDetection_SD.ino

--- ESP32-CAM_FaceDetection_SD/ESP32-CAM_FaceDetection_SD.ino
+++ ESP32-CAM_FaceDetection_SD/ESP32-CAM_FaceDetection_SD.ino
@@ -99,7 +99,7 @@ void setup() {
   
   fs::FS &fs = SD_MMC;
   String filename[2] = {"/1.jpg", "/2.jpg"};   //FRAMESIZE = CIF (width:400, height:296)
-  //Save the captured Images with your face to SD card:  http://192.168.xxx.xxx/capture
+  //Save the captured Images with your face to SD card:  http://192.168.xxx.xxx/capture  (FRAMESIZE_CIF)
   
   for (int j=0;j<sizeof(filename)/sizeof(*filename);j++) {
     //讀取照片
