a0316ef46c45f9d38f69cbf5bd9065c27d917344
fustyles <34397289+fustyles@users.noreply.github.com>
2020-05-20 21:25:25 +0800

    Update ESP32-CAM_FaceRecognition_PAGE.ino

--- ESP32-CAM_FaceRecognition_PAGE/ESP32-CAM_FaceRecognition_PAGE.ino
+++ ESP32-CAM_FaceRecognition_PAGE/ESP32-CAM_FaceRecognition_PAGE.ino
@@ -716,7 +716,7 @@ static esp_err_t status_handler(httpd_req_t *req){
     p+=sprintf(p, "\"face_detect\":%u,", detection_enabled);
     p+=sprintf(p, "\"face_enroll\":%u,", is_enrolling);
     p+=sprintf(p, "\"face_recognize\":%u,", recognition_enabled);
-    p+=sprintf(p, "\"falsh\":%u", flash_value);
+    p+=sprintf(p, "\"flash\":%u", flash_value);
     *p++ = '}';
     *p++ = 0;
     httpd_resp_set_type(req, "application/json");
