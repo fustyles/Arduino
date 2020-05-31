9a230642bca53648a8772defd8e712dc706b736c
fustyles <34397289+fustyles@users.noreply.github.com>
2020-05-31 17:30:25 +0800

    Update ESP32-CAM_LCD1602.ino

--- ESP32-CAM_LCD1602/ESP32-CAM_LCD1602.ino
+++ ESP32-CAM_LCD1602/ESP32-CAM_LCD1602.ino
@@ -9,15 +9,14 @@ You must delete other LiquidCrystal_I2C libraries.
 */
 
 byte pinSDA = 2;
-byte pinSCL = 13;
-int addr = 0x27;  //0x27 or ox3F
+byte pinSCL = 13; 
 
 #include "LiquidCrystal_I2C.h"
+LiquidCrystal_I2C lcd(0x27, 16, 2);  //0x27 or ox3F
 #include <Wire.h>
 TwoWire Wire0 = TwoWire(0);
 
 void setup() {
-  LiquidCrystal_I2C lcd(addr, 16, 2);
   Wire.begin(pinSDA, pinSCL);
   Wire.setClock(10000);
   
