55ab8dcd7ad7e1332388b075480e6789f0d72a7a
fustyles <34397289+fustyles@users.noreply.github.com>
2020-06-26 11:51:03 +0800

    Add files via upload

---
+++ ESP32-CAM_DHT11/ESP32-CAM_DHT11.ino
@@ -0,0 +1,34 @@
+/*
+ESP32-CAM DHT11
+Author : ChungYi Fu (Kaohsiung, Taiwan)  2020-6-25 01:00
+https://www.facebook.com/francefu
+
+DHT11 Library
+https://github.com/fustyles/Arduino/blob/master/ESP32-CAM_Linenotify_DHT11/DHT.zip
+*/
+
+#include <dht.h>   
+#define dht_dpin 2   //DHT11 IO2
+dht DHT;  
+
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
+
+void setup()
+{
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
+  
+  Serial.begin(115200);
+  delay(10);
+}
+  
+void loop(){   
+  DHT.read11(dht_dpin);
+  Serial.print("Humidity = ");   
+  Serial.print(DHT.humidity);   
+  Serial.print("% ");   
+  Serial.print("temperature = ");   
+  Serial.print(DHT.temperature);   
+  Serial.println("C ");   
+  delay(2000);  
+} 
