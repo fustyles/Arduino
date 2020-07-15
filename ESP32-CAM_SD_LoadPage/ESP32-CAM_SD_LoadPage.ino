387ac4a0062c6d856d6f80b3038e86e57bafccf0
fustyles <34397289+fustyles@users.noreply.github.com>
2020-07-15 09:08:58 +0800

    Update ESP32-CAM_SD_LoadPage.ino

--- ESP32-CAM_SD_LoadPage/ESP32-CAM_SD_LoadPage.ino
+++ ESP32-CAM_SD_LoadPage/ESP32-CAM_SD_LoadPage.ino
@@ -13,6 +13,8 @@ http://APIP/control?cmd=P1;P2;P3;P4;P5;P6;P7;P8;P9
 http://STAIP/control?cmd=P1;P2;P3;P4;P5;P6;P7;P8;P9
 
 預設AP端IP： 192.168.4.1
+
+自訂指令格式 http://192.168.xxx.xxx/control?cmd=P1;P2;P3;P4;P5;P6;P7;P8;P9
 http://192.168.xxx.xxx/control?ip                  //取得APIP, STAIP
 http://192.168.xxx.xxx/control?mac                 //取得MAC位址
 http://192.168.xxx.xxx/control?restart             //重啟ESP32-CAM
@@ -20,7 +22,7 @@ http://192.168.xxx.xxx/control?flash=value         //value= 0~255
 http://192.168.xxx.xxx/control?loadpage=/filepath  //載入SD卡網頁(.htm, .html), 路徑開端要加"/"
 http://192.168.xxx.xxx/control?getimage=/filepath  //載入SD卡圖檔, 路徑開端要加"/"
 
-設定視訊參數(官方指令格式)
+官方指令格式 http://192.168.xxx.xxx/control?var=***&val=***
 http://192.168.xxx.xxx/control?var=framesize&val=value    // value = 10->UXGA(1600x1200), 9->SXGA(1280x1024), 8->XGA(1024x768) ,7->SVGA(800x600), 6->VGA(640x480), 5 selected=selected->CIF(400x296), 4->QVGA(320x240), 3->HQVGA(240x176), 0->QQVGA(160x120)
 http://192.168.xxx.xxx/control?var=quality&val=value    // value = 10 ~ 63
 http://192.168.xxx.xxx/control?var=brightness&val=value    // value = -2 ~ 2
