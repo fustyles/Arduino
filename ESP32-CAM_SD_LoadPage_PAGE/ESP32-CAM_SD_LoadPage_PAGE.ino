a48352dc492b4431882792300a825481ff5f0962
fustyles <34397289+fustyles@users.noreply.github.com>
2020-07-30 22:13:24 +0800

    Add files via upload

--- ESP32-CAM_SD_LoadPage_PAGE/ESP32-CAM_SD_LoadPage_PAGE.ino
+++ ESP32-CAM_SD_LoadPage_PAGE/ESP32-CAM_SD_LoadPage_PAGE.ino
@@ -698,12 +698,15 @@ static esp_err_t status_handler(httpd_req_t *req){
     return httpd_resp_send(req, json_response, strlen(json_response));
 }
 
+//自訂網頁首頁
+static const char PROGMEM INDEX_HTML[] = R"rawliteral(
+
+)rawliteral";
+
 //網頁首頁   http://192.168.xxx.xxx
 static esp_err_t index_handler(httpd_req_t *req){
     httpd_resp_set_type(req, "text/html");
-    String html = getList();
-    const char *buf = html.c_str();
-    return httpd_resp_send(req, buf, strlen(buf));
+    return httpd_resp_send(req, (const char *)INDEX_HTML, strlen(INDEX_HTML));
 }
 
 //自訂網址路徑要執行的函式
