780aa113979939f604544ef4342fa339a836f291
fustyles <34397289+fustyles@users.noreply.github.com>
2020-08-23 17:33:38 +0800

    Update ESP32-CAM_knn-classifier.ino

--- ESP32-CAM_Tensorflow.js/ESP32-CAM_knn-classifier/ESP32-CAM_knn-classifier.ino
+++ ESP32-CAM_Tensorflow.js/ESP32-CAM_knn-classifier/ESP32-CAM_knn-classifier.ino
@@ -373,6 +373,7 @@ static esp_err_t stream_handler(httpd_req_t *req){
     if(res != ESP_OK){
         return res;
     }
+    httpd_resp_set_hdr(req, "Access-Control-Allow-Origin", "*");
 
     while(true){
         fb = esp_camera_fb_get();
@@ -649,7 +650,7 @@ static const char PROGMEM INDEX_HTML[] = R"rawliteral(<!doctype html>
     <figure>
       <div id="stream-container" class="image-container hidden">
         <div class="close" id="close-stream">×</div>
-        <img id="stream" src="">
+        <img id="stream" src="" crossorigin="anonymous">
         <canvas id="canvas" width="320" height="240" style="background-color:#000000;display:none;">
         <img id="example" width="320" height="240" style="display:none">
       </div>
