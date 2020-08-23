5b82ed5fa0c4e7336f4c1151def661561314a1c2
fustyles <34397289+fustyles@users.noreply.github.com>
2020-08-23 17:33:19 +0800

    Update ESP32-CAM_deeplabV3.ino

--- ESP32-CAM_Tensorflow.js/ESP32-CAM_deeplabV3/ESP32-CAM_deeplabV3.ino
+++ ESP32-CAM_Tensorflow.js/ESP32-CAM_deeplabV3/ESP32-CAM_deeplabV3.ino
@@ -373,6 +373,7 @@ static esp_err_t stream_handler(httpd_req_t *req){
     if(res != ESP_OK){
         return res;
     }
+    httpd_resp_set_hdr(req, "Access-Control-Allow-Origin", "*");
 
     while(true){
         fb = esp_camera_fb_get();
@@ -652,7 +653,7 @@ static const char PROGMEM INDEX_HTML[] = R"rawliteral(<!doctype html>
             <figure>
               <div id="stream-container" class="image-container hidden">
                 <div class="close" id="close-stream" style="display:none">×</div>
-                <img id="stream" src="" style="display:none">
+                <img id="stream" src="" style="display:none" crossorigin="anonymous">
                 <canvas id="canvas" width="0" height="0"></canvas><div id="legendList"></div><br><br>
               </div>
             </figure> 
