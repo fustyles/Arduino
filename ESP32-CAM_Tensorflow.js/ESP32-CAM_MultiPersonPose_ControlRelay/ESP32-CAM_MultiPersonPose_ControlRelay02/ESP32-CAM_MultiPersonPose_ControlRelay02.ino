4b427fd060b88e1834ca9d544890b6855d4fc9c5
fustyles <34397289+fustyles@users.noreply.github.com>
2020-08-23 17:28:39 +0800

    Update ESP32-CAM_MultiPersonPose_ControlRelay02.ino

--- ESP32-CAM_Tensorflow.js/ESP32-CAM_MultiPersonPose_ControlRelay/ESP32-CAM_MultiPersonPose_ControlRelay02/ESP32-CAM_MultiPersonPose_ControlRelay02.ino
+++ ESP32-CAM_Tensorflow.js/ESP32-CAM_MultiPersonPose_ControlRelay/ESP32-CAM_MultiPersonPose_ControlRelay02/ESP32-CAM_MultiPersonPose_ControlRelay02.ino
@@ -396,6 +396,7 @@ static esp_err_t stream_handler(httpd_req_t *req){
     if(res != ESP_OK){
         return res;
     }
+    httpd_resp_set_hdr(req, "Access-Control-Allow-Origin", "*");
 
     while(true){
         fb = esp_camera_fb_get();
@@ -689,7 +690,7 @@ static const char PROGMEM INDEX_HTML[] = R"rawliteral(<!doctype html>
             <figure>
               <div id="stream-container" class="image-container hidden">
                 <div class="close" id="close-stream">×</div>
-                <img id="stream" src="" style="display:none">
+                <img id="stream" src="" style="display:none" crossorigin="anonymous">
               </div>
             </figure>         
             <div id="logo">
