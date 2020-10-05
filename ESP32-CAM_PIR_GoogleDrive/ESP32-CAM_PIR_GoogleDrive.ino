73d51d6cfca957a4e8858721e5e9b190ac35260b
fustyles <34397289+fustyles@users.noreply.github.com>
2020-10-05 12:29:02 +0800

    Update ESP32-CAM_PIR_GoogleDrive.ino

--- ESP32-CAM_PIR_GoogleDrive/ESP32-CAM_PIR_GoogleDrive.ino
+++ ESP32-CAM_PIR_GoogleDrive/ESP32-CAM_PIR_GoogleDrive.ino
@@ -232,6 +232,7 @@ String SendCapturedImage2GoogleDrive() {
     client_tcp.println("Host: " + String(myDomain));
     client_tcp.println("Content-Length: " + String(Data.length()+imageFile.length()));
     client_tcp.println("Content-Type: application/x-www-form-urlencoded");
+    client_tcp.println("Connection: keep-alive");
     client_tcp.println();
     
     client_tcp.print(Data);
