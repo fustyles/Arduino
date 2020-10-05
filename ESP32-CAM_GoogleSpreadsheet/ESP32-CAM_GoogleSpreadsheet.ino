4927a7abf7ca8a41be0cf0f2d841a2c5ba19c6d5
fustyles <34397289+fustyles@users.noreply.github.com>
2020-10-05 12:27:16 +0800

    Update ESP32-CAM_GoogleSpreadsheet.ino

--- ESP32-CAM_GoogleSpreadsheet/ESP32-CAM_GoogleSpreadsheet.ino
+++ ESP32-CAM_GoogleSpreadsheet/ESP32-CAM_GoogleSpreadsheet.ino
@@ -220,6 +220,7 @@ String SendCapturedImage2Spreadsheet() {
       client_tcp.println("Connection: keep-alive");
       client_tcp.println("Content-Length: " + String(Data.length()+imageFile.length()));
       client_tcp.println("Content-Type: application/x-www-form-urlencoded");
+      client_tcp.println("Connection: keep-alive");
       client_tcp.println();
       
       client_tcp.print(Data);
