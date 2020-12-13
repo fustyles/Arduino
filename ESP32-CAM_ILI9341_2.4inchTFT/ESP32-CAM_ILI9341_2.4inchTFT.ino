eb11eddb618fbee3b9c13e8541cee1bf2fe7579b
fustyles <34397289+fustyles@users.noreply.github.com>
2020-12-13 21:29:52 +0800

    Add files via upload

---
+++ ESP32-CAM_ILI9341_2.4inchTFT/ESP32-CAM_ILI9341_2.4inchTFT.ino
@@ -0,0 +1,185 @@
+/*
+ESP32-CAM 2.4 inch TFT LCD Display Module (ILI9341, SPI, 240x320)
+Author : ChungYi Fu (Kaohsiung, Taiwan)  2020-12-13 21:00
+https://www.facebook.com/francefu
+
+Library：
+https://github.com/Bodmer/TFT_eSPI
+https://github.com/Bodmer/TJpg_Decoder
+
+Tutorial
+https://blog.xuite.net/iamleon/blog/589421027
+
+Datasheet
+http://tjcfile.iicp.net:885/tft/2.8_spi/2.8-spi-9341.rar
+
+Add the settings in the file.  \Documents\Arduino\libraries\TFT_eSPI-master\User_Setup.h
+#define TFT_MOSI 13
+#define TFT_MISO 12
+#define TFT_SCLK 14
+#define TFT_CS   15  // Chip select control pin
+#define TFT_DC   2   // Data Command control pin
+#define TFT_RST  4   // Reset pin (could connect to RST pin)
+*/
+
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
+#include "esp_camera.h"
+#include "SPI.h"
+#include <TJpg_Decoder.h>
+#include <TFT_eSPI.h>              // Hardware-specific library
+TFT_eSPI tft = TFT_eSPI();         // Invoke custom library
+
+//CAMERA_MODEL_AI_THINKER
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
+#define USE_DMA
+
+#ifdef USE_DMA
+  uint16_t  dmaBuffer1[16*16]; // Toggle buffer for 16*16 MCU block, 512bytes
+  uint16_t  dmaBuffer2[16*16]; // Toggle buffer for 16*16 MCU block, 512bytes
+  uint16_t* dmaBufferPtr = dmaBuffer1;
+  bool dmaBufferSel = 0;
+#endif
+
+// This next function will be called during decoding of the jpeg file to render each
+// 16x16 or 8x8 image tile (Minimum Coding Unit) to the TFT.
+bool tft_output(int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t* bitmap)
+{
+   // Stop further decoding as image is running off bottom of screen
+  if ( y >= tft.height() ) return 0;
+
+  // STM32F767 processor takes 43ms just to decode (and not draw) jpeg (-Os compile option)
+  // Total time to decode and also draw to TFT:
+  // SPI 54MHz=71ms, with DMA 50ms, 71-43 = 28ms spent drawing, so DMA is complete before next MCU block is ready
+  // Apparent performance benefit of DMA = 71/50 = 42%, 50 - 43 = 7ms lost elsewhere
+  // SPI 27MHz=95ms, with DMA 52ms. 95-43 = 52ms spent drawing, so DMA is *just* complete before next MCU block is ready!
+  // Apparent performance benefit of DMA = 95/52 = 83%, 52 - 43 = 9ms lost elsewhere
+#ifdef USE_DMA
+  // Double buffering is used, the bitmap is copied to the buffer by pushImageDMA() the
+  // bitmap can then be updated by the jpeg decoder while DMA is in progress
+  if (dmaBufferSel) dmaBufferPtr = dmaBuffer2;
+  else dmaBufferPtr = dmaBuffer1;
+  dmaBufferSel = !dmaBufferSel; // Toggle buffer selection
+  //  pushImageDMA() will clip the image block at screen boundaries before initiating DMA
+  tft.pushImageDMA(x, y, w, h, bitmap, dmaBufferPtr); // Initiate DMA - blocking only if last DMA is not complete
+  // The DMA transfer of image block to the TFT is now in progress...
+#else
+  // Non-DMA blocking alternative
+  tft.pushImage(x, y, w, h, bitmap);  // Blocking, so only returns when image block is drawn
+#endif
+  // Return 1 to decode next block.
+  return 1;
+}
+
+void setup() {
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
+  
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  Serial.println();
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 10000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  //init with high specs to pre-allocate larger buffers
+  if(psramFound()){
+    config.frame_size = FRAMESIZE_QVGA; //FRAMESIZE_QVGA 320x240
+    config.jpeg_quality = 10; //< Quality of JPEG output. 0-63 lower means higher quality 
+    config.fb_count = 2; //Number of frame buffers to be allocated. If more than one, then each frame will be acquired (double speed)
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
+  sensor_t * s = esp_camera_sensor_get();
+  s->set_framesize(s, FRAMESIZE_QVGA);
+
+  // Initialise the TFT
+  tft.begin();
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextColor(TFT_ORANGE, TFT_BLACK);
+  tft.setRotation(3);//1:landscape 3:inv. landscape
+
+#ifdef USE_DMA
+  tft.initDMA(); // To use SPI DMA you must call initDMA() to setup the DMA engine
+#endif
+
+  // The jpeg image can be scaled down by a factor of 1, 2, 4, or 8
+  TJpgDec.setJpgScale(1);
+
+  // The colour byte order can be swapped by the decoder
+  // using TJpgDec.setSwapBytes(true); or by the TFT_eSPI library:
+  tft.setSwapBytes(true);
+
+  // The decoder must be given the exact name of the rendering function above
+  TJpgDec.setCallback(tft_output);   
+}
+
+void loop() {
+  camera_fb_t * fb = NULL;
+  fb = esp_camera_fb_get();
+  if (!fb) {
+    Serial.println("Camera capture failed");
+    return;
+  }
+
+  #ifdef USE_DMA
+  // Must use startWrite first so TFT chip select stays low during DMA and SPI channel settings remain configured
+  tft.startWrite();
+  #endif
+
+  // Draw the image, top left at 0,0 - DMA request is handled in the call-back tft_output() in this sketch
+  //TJpgDec.drawJpg(0, 0, panda, sizeof(panda));
+  TJpgDec.drawJpg(0, 0,  fb->buf, fb->len);
+
+  #ifdef USE_DMA
+  // Must use endWrite to release the TFT chip select and release the SPI channel
+  tft.endWrite();
+  #endif
+  
+  esp_camera_fb_return(fb); 
+}
