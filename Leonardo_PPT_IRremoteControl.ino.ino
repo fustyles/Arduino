c35873b2cead6a5bdb3dc8a8fb70f95be977029f
fustyles <34397289+fustyles@users.noreply.github.com>
2021-01-01 21:40:47 +0800

    Update Leonardo_PPT_IRremoteControl.ino.ino

--- Leonardo_PPT_IRremoteControl.ino.ino
+++ Leonardo_PPT_IRremoteControl.ino.ino
@@ -1,5 +1,5 @@
 /*
-Arduino Leonardo Use IR Remote Control to control PPT
+Arduino Leonardo IR Remote Control for PPT
 Author    ChungYi Fu (Kaohsiung, Taiwan)  2021-01-01 21:00 
 https://www.facebook.com/francefu
 
