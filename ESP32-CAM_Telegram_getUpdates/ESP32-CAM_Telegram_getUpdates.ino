8c4722e10e910091688bcb77de6d44921697e38f
fustyles <34397289+fustyles@users.noreply.github.com>
2021-01-10 18:58:48 +0800

    Update ESP32-CAM_Telegram_getUpdates.ino

--- ESP32-CAM_Telegram_getUpdates/ESP32-CAM_Telegram_getUpdates.ino
+++ ESP32-CAM_Telegram_getUpdates/ESP32-CAM_Telegram_getUpdates.ino
@@ -1,7 +1,7 @@
 /*
 ESP32-CAM Using keyboard in Telegram Bot
 
-Author : ChungYi Fu (Kaohsiung, Taiwan)  2021-1-10 13:20
+Author : ChungYi Fu (Kaohsiung, Taiwan)  2021-1-10 19:00
 https://www.facebook.com/francefu
 
 ArduinoJson Library：
