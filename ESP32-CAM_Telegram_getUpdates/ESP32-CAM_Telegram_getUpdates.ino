79e2c04ce1b372b12ef9816107a2e0812058c923
fustyles <34397289+fustyles@users.noreply.github.com>
2021-01-11 10:54:27 +0800

    Update ESP32-CAM_Telegram_getUpdates.ino

--- ESP32-CAM_Telegram_getUpdates/ESP32-CAM_Telegram_getUpdates.ino
+++ ESP32-CAM_Telegram_getUpdates/ESP32-CAM_Telegram_getUpdates.ino
@@ -19,7 +19,7 @@ String token = "*****:*****";   // Create your bot and get the token -> https://
 String chat_id = "*****";   // Get chat_id -> https://telegram.me/chatid_echo_bot
 
 /*
-If variable "sendHelp" is equal to "true", it will send the message "/help" to Telegram Bot when the board boots. 
+If "sendHelp" variable is equal to "true", it will send the message "/help" to Telegram Bot when the board boots. 
 If you don't want to get the command list when the board restarts every time, you can set the value to "false".
 But you need to input the command "/help" to get the keyboards when the board boots.
 */
