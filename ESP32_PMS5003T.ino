cf04d959f13e19b62843230e298f3609dbb95f6d
fustyles <34397289+fustyles@users.noreply.github.com>
2021-01-15 23:01:40 +0800

    Update ESP32_PMS5003T.ino

--- ESP32_PMS5003T.ino
+++ ESP32_PMS5003T.ino
@@ -607,7 +607,7 @@ String LineNotify(String token, String request, byte wait)
     return "Connection failed";  
 }
 
-void flashWrite(char data[len], int i) {      // i = 0 to 63
+void flashWrite(char data[len], int i) {      // len = 64 -> i = 0 to 63
   uint32_t flashAddress = addressStart + i*len;
   char buff_write[len];
   strcpy(buff_write, data);
@@ -617,7 +617,7 @@ void flashWrite(char data[len], int i) {      // i = 0 to 63
     Serial.printf("address: %p write \"%s\" [error]\n", flashAddress, buff_write);
 }
 
-char* flashRead(int i) {      // i = 0 to 63
+char* flashRead(int i) {      // len = 64 -> i = 0 to 63
   uint32_t flashAddress = addressStart + i*len;
   static char buff_read[len];
   if (ESP.flashRead(flashAddress,(uint32_t*)buff_read, sizeof(buff_read)-1)) {
