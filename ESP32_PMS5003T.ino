f72e575710bec4177a0945fc17bb6645c52cf4f6
fustyles <34397289+fustyles@users.noreply.github.com>
2021-01-19 19:10:50 +0800

    Update ESP32_PMS5003T.ino

--- ESP32_PMS5003T.ino
+++ ESP32_PMS5003T.ino
@@ -337,9 +337,9 @@ void loop() {
          
     lcd.clear();
     lcd.setCursor(0,0);
-    lcd.print("PM2.5=" + myPm25 + "  " + myTemp + " *C");
+    lcd.print("PM2.5=" + myPm25 + " " + myTemp + " *C");
     lcd.setCursor(0,1);
-    lcd.print("PM10 =" + myPm100 + "  " + myHumid + " %RH");
+    lcd.print("PM10 =" + myPm100 + " " + myHumid + " %RH");
 
     //Thingspeak
     String domain="api.thingspeak.com";
