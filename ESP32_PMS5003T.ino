7b464362864b9032f3fe876566654cb27b8bb099
fustyles <34397289+fustyles@users.noreply.github.com>
2021-01-20 08:24:59 +0800

    Add files via upload

--- ESP32_PMS5003T.ino
+++ ESP32_PMS5003T.ino
@@ -1,6 +1,6 @@
 /*
-NODEMCU ESP32 PMS5003T
-Author : ChungYi Fu (Kaohsiung, Taiwan)  2021-1-19 18:30
+ESP32 PMS5003T
+Author : ChungYi Fu (Kaohsiung, Taiwan)  2021-1-19 19:00
 https://www.facebook.com/francefu
 
 Set WIFI ssid and password
@@ -23,7 +23,7 @@ Erase SPI Flash
 http://192.168.4.1?eraseflash
 http://STAIP?eraseflash
 refer to
-https://ruten-proteus.blogspot.com/2016/12/ESP8266ArduinoQA-02.html
+https://ruten-proteus.blogspot.com/2016/12/ESp8266ArduinoQA-02.html
 http://wyj-learning.blogspot.com/2018/03/nodemcu-flash.html
   
 ESP32 LCD Library
@@ -39,8 +39,8 @@ PMS5003T
 3V3, GND, RX:P8 (gpio16), TX:P9 (gpio17)
 
 Command Format :  
-http://APIP/?cmd=P1;P2;P3;P4;P5;P6;P7;P8;P9
-http://STAIP/?cmd=P1;P2;P3;P4;P5;P6;P7;P8;P9
+http://APIP/?cmd=p1;p2;p3;p4;p5;p6;p7;p8;p9
+http://STAIP/?cmd=p1;p2;p3;p4;p5;p6;p7;p8;p9
 
 Default APIP： 192.168.4.1
 
@@ -64,7 +64,7 @@ http://192.168.4.1/?linenotify=token;request
 const char* ssid     = "";  //WIFI ssid
 const char* password = "";  //WIFI pwd
 
-const char* apssid = "ESP32_PMS5003T";
+const char* apssid = "WEBBIT_PMS5003T";
 const char* appassword = "12345678";
 
 String thingspeak_api_key = "";   //ThingSpeak
@@ -99,13 +99,13 @@ int delaycount = 0;
 
 WiFiServer server(80);
 
-String Feedback="", Command="",cmd="",P1="",P2="",P3="",P4="",P5="",P6="",P7="",P8="",P9="";
+String Feedback="", Command="",cmd="",p1="",p2="",p3="",p4="",p5="",p6="",p7="",p8="",p9="";
 byte ReceiveState=0,cmdState=1,strState=1,questionstate=0,equalstate=0,semicolonstate=0;
 
 void ExecuteCommand() {
   Serial.println("");
   //Serial.println("Command: "+Command);
-  Serial.println("cmd= "+cmd+" ,P1= "+P1+" ,P2= "+P2+" ,P3= "+P3+" ,P4= "+P4+" ,P5= "+P5+" ,P6= "+P6+" ,P7= "+P7+" ,P8= "+P8+" ,P9= "+P9);
+  Serial.println("cmd= "+cmd+" ,p1= "+p1+" ,p2= "+p2+" ,p3= "+p3+" ,p4= "+p4+" ,p5= "+p5+" ,p6= "+p6+" ,p7= "+p7+" ,p8= "+p8+" ,p9= "+p9);
   Serial.println("");
 
   if (cmd=="admin") { 
@@ -119,19 +119,19 @@ void ExecuteCommand() {
   }     
   else if (cmd=="thingspeakapikey") {
     char buff_key[len]; 
-    strcpy(buff_key, P1.c_str());
+    strcpy(buff_key, p1.c_str());
     flashWrite(buff_key, 2); 
 
-    thingspeak_api_key = P1;
-    Feedback="Set ThingSpeak API_KEY = "+P1+" OK";
+    thingspeak_api_key = p1;
+    Feedback="Set ThingSpeak API_KEY = "+p1+" OK";
   }  
   else if (cmd=="linetoken") {
     char buff_token[len]; 
-    strcpy(buff_token, P1.c_str());
+    strcpy(buff_token, p1.c_str());
     flashWrite(buff_token, 3); 
         
-    line_token = P1;
-    Feedback="Set Line Notify Token = "+P1+" OK";    
+    line_token = p1;
+    Feedback="Set Line Notify Token = "+p1+" OK";    
   } 
   else if (cmd=="get") {
     Feedback = "PM2.5:    "+String(pmat25)+" ug/m3<br>PM100:    "+String(pmat100)+" ug/m3<br>Temperature:    "+String(Temp)+" °C<br>Humidity:    "+String(Humid)+" %RH";
@@ -142,14 +142,14 @@ void ExecuteCommand() {
   }   
   else if (cmd=="resetwifi") {
     char buff_ssid[len], buff_password[len]; 
-    strcpy(buff_ssid, P1.c_str());
-    strcpy(buff_password, P2.c_str());
+    strcpy(buff_ssid, p1.c_str());
+    strcpy(buff_password, p2.c_str());
     flashWrite(buff_ssid, 0);
     flashWrite(buff_password, 1);   
 
-    WiFi.begin(P1.c_str(), P2.c_str());
+    WiFi.begin(p1.c_str(), p2.c_str());
     Serial.print("Connecting to ");
-    Serial.println(P1);
+    Serial.println(p1);
     long int StartTime=millis();
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
@@ -176,42 +176,42 @@ void ExecuteCommand() {
     ESP.restart();
   } 
   else if (cmd=="inputpullup") {
-    pinMode(P1.toInt(), INPUT_PULLUP);
+    pinMode(p1.toInt(), INPUT_PULLUP);
   }  
   else if (cmd=="pinmode") {
-    if (P2.toInt()==1)
-      pinMode(P1.toInt(), OUTPUT);
+    if (p2.toInt()==1)
+      pinMode(p1.toInt(), OUTPUT);
     else
-      pinMode(P1.toInt(), INPUT);
+      pinMode(p1.toInt(), INPUT);
   }        
   else if (cmd=="digitalwrite") {
-    ledcDetachPin(P1.toInt());
-    pinMode(P1.toInt(), OUTPUT);
-    digitalWrite(P1.toInt(), P2.toInt());
+    ledcDetachPin(p1.toInt());
+    pinMode(p1.toInt(), OUTPUT);
+    digitalWrite(p1.toInt(), p2.toInt());
   }   
   else if (cmd=="digitalread") {
-    Feedback=String(digitalRead(P1.toInt()));
+    Feedback=String(digitalRead(p1.toInt()));
   }
   else if (cmd=="analogwrite") {
-    ledcAttachPin(P1.toInt(), 1);
+    ledcAttachPin(p1.toInt(), 1);
     ledcSetup(1, 5000, 8);
-    ledcWrite(1,P2.toInt());
+    ledcWrite(1,p2.toInt());
   }       
   else if (cmd=="analogread") {
-    Feedback=String(analogRead(P1.toInt()));
+    Feedback=String(analogRead(p1.toInt()));
   }
   else if (cmd=="touchread") {
-    Feedback=String(touchRead(P1.toInt()));
+    Feedback=String(touchRead(p1.toInt()));
   }   
   else if (cmd=="thingspeakupdate") {
     String domain="api.thingspeak.com";
-    String request = "/update?api_key=" + P1;
-    request += "&field1="+P2+"&field2="+P3+"&field3="+P4+"&field4="+P5+"&field5="+P6+"&field6="+P7+"&field7="+P8+"&field8="+P9;
+    String request = "/update?api_key=" + p1;
+    request += "&field1="+p2+"&field2="+p3+"&field3="+p4+"&field4="+p5+"&field5="+p6+"&field6="+p7+"&field7="+p8+"&field8="+p9;
     Feedback=tcp_https(domain,request,443,0);
   }    
   else if (cmd=="linenotify") {    //message=xxx&stickerPackageId=xxx&stickerId=xxx
-    String token = P1;
-    String request = P2;
+    String token = p1;
+    String request = p2;
     Feedback=LineNotify(token,request,1);
   } 
   else {
@@ -416,7 +416,7 @@ void retrievepm25() {
 }
 
 void getRequest() {
-  Command="";cmd="";P1="";P2="";P3="";P4="";P5="";P6="";P7="";P8="";P9="";
+  Command="";cmd="";p1="";p2="";p3="";p4="";p5="";p6="";p7="";p8="";p9="";
   ReceiveState=0,cmdState=1,strState=1,questionstate=0,equalstate=0,semicolonstate=0;
   
   WiFiClient client = server.available();
@@ -487,15 +487,15 @@ void getCommand(char c) {
     if (c==';') strState++;
   
     if ((cmdState==1)&&((c!='?')||(questionstate==1))) cmd=cmd+String(c);
-    if ((cmdState==0)&&(strState==1)&&((c!='=')||(equalstate==1))) P1=P1+String(c);
-    if ((cmdState==0)&&(strState==2)&&(c!=';')) P2=P2+String(c);
-    if ((cmdState==0)&&(strState==3)&&(c!=';')) P3=P3+String(c);
-    if ((cmdState==0)&&(strState==4)&&(c!=';')) P4=P4+String(c);
-    if ((cmdState==0)&&(strState==5)&&(c!=';')) P5=P5+String(c);
-    if ((cmdState==0)&&(strState==6)&&(c!=';')) P6=P6+String(c);
-    if ((cmdState==0)&&(strState==7)&&(c!=';')) P7=P7+String(c);
-    if ((cmdState==0)&&(strState==8)&&(c!=';')) P8=P8+String(c);
-    if ((cmdState==0)&&(strState>=9)&&((c!=';')||(semicolonstate==1))) P9=P9+String(c);
+    if ((cmdState==0)&&(strState==1)&&((c!='=')||(equalstate==1))) p1=p1+String(c);
+    if ((cmdState==0)&&(strState==2)&&(c!=';')) p2=p2+String(c);
+    if ((cmdState==0)&&(strState==3)&&(c!=';')) p3=p3+String(c);
+    if ((cmdState==0)&&(strState==4)&&(c!=';')) p4=p4+String(c);
+    if ((cmdState==0)&&(strState==5)&&(c!=';')) p5=p5+String(c);
+    if ((cmdState==0)&&(strState==6)&&(c!=';')) p6=p6+String(c);
+    if ((cmdState==0)&&(strState==7)&&(c!=';')) p7=p7+String(c);
+    if ((cmdState==0)&&(strState==8)&&(c!=';')) p8=p8+String(c);
+    if ((cmdState==0)&&(strState>=9)&&((c!=';')||(semicolonstate==1))) p9=p9+String(c);
     
     if (c=='?') questionstate=1;
     if (c=='=') equalstate=1;
@@ -564,7 +564,7 @@ String LineNotify(String token, String request, byte wait) {
     client_tcp.println("POST /api/notify HTTP/1.1");
     client_tcp.println("Connection: close"); 
     client_tcp.println("Host: notify-api.line.me");
-    client_tcp.println("User-Agent: ESP8266/1.0");
+    client_tcp.println("User-Agent: ESp8266/1.0");
     client_tcp.println("Authorization: Bearer " + token);
     client_tcp.println("Content-Type: application/x-www-form-urlencoded");
     client_tcp.println("Content-Length: " + String(request.length()));
