8bad8575262ba0cefcf36062f65eee8f541de149
fustyles <34397289+fustyles@users.noreply.github.com>
2021-01-25 18:17:20 +0800

    Update ESP32-CAM_PIR_Telegram.ino

--- ESP32-CAM_PIR_Telegram/ESP32-CAM_PIR_Telegram.ino
+++ ESP32-CAM_PIR_Telegram/ESP32-CAM_PIR_Telegram.ino
@@ -219,6 +219,7 @@ String sendCapturedImage2Telegram(String token, String chat_id) {
       while (client_tcp.available()) 
       {
           char c = client_tcp.read();
+          if (state==true) getBody += String(c);        
           if (c == '\n') 
           {
             if (getAll.length()==0) state=true; 
@@ -226,7 +227,6 @@ String sendCapturedImage2Telegram(String token, String chat_id) {
           } 
           else if (c != '\r')
             getAll += String(c);
-          if (state==true) getBody += String(c);
           startTime = millis();
        }
        if (getBody.length()>0) break;
@@ -270,6 +270,7 @@ String sendMessage2Telegram(String token, String chat_id, String text) {
       while (client_tcp.available()) 
       {
           char c = client_tcp.read();
+          if (state==true) getBody += String(c);        
           if (c == '\n') 
           {
             if (getAll.length()==0) state=true; 
@@ -277,7 +278,6 @@ String sendMessage2Telegram(String token, String chat_id, String text) {
           } 
           else if (c != '\r')
             getAll += String(c);
-          if (state==true) getBody += String(c);
           startTime = millis();
        }
        if (getBody.length()>0) break;
