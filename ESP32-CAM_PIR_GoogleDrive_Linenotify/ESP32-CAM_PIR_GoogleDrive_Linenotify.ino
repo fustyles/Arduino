cbc7577e8d6e6bc7c9081544a4137056adbb19e3
fustyles <34397289+fustyles@users.noreply.github.com>
2021-01-25 18:18:29 +0800

    Update ESP32-CAM_PIR_GoogleDrive_Linenotify.ino

--- ESP32-CAM_PIR_GoogleDrive_Linenotify/ESP32-CAM_PIR_GoogleDrive_Linenotify.ino
+++ ESP32-CAM_PIR_GoogleDrive_Linenotify/ESP32-CAM_PIR_GoogleDrive_Linenotify.ino
@@ -221,6 +221,7 @@ String SendCapturedImage() {
       while (client_tcp.available()) 
       {
           char c = client_tcp.read();
+          if (state==true) getBody += String(c);        
           if (c == '\n') 
           {
             if (getAll.length()==0) state=true; 
@@ -228,7 +229,6 @@ String SendCapturedImage() {
           } 
           else if (c != '\r')
             getAll += String(c);
-          if (state==true) getBody += String(c);
           startTime = millis();
        }
        if (getBody.length()>0) break;
