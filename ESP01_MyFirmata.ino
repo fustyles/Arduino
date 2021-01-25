480f46c1ac7fd56e394905ff118f57b9b89a6401
fustyles <34397289+fustyles@users.noreply.github.com>
2021-01-26 00:48:34 +0800

    Update ESP01_MyFirmata.ino

--- ESP01_MyFirmata.ino
+++ ESP01_MyFirmata.ino
@@ -470,6 +470,7 @@ String tcp(String domain,String request,int port,byte wait)
         while (client_tcp.available()) 
         {
             char c = client_tcp.read();
+            if (state==true) Feedback += String(c);          
             if (c == '\n') 
             {
               if (getResponse.length()==0) state=true; 
@@ -477,7 +478,6 @@ String tcp(String domain,String request,int port,byte wait)
             } 
             else if (c != '\r')
               getResponse += String(c);
-            if (state==true) Feedback += String(c);
             if (wait==1)
               startTime = millis();
          }
