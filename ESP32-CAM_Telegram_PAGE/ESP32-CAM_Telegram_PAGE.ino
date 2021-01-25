1b5239f62d80ced2e3bb30a8f4264216347f4afa
fustyles <34397289+fustyles@users.noreply.github.com>
2021-01-26 00:31:37 +0800

    Update ESP32-CAM_Telegram_PAGE.ino

--- ESP32-CAM_Telegram_PAGE/ESP32-CAM_Telegram_PAGE.ino
+++ ESP32-CAM_Telegram_PAGE/ESP32-CAM_Telegram_PAGE.ino
@@ -521,6 +521,7 @@ String sendCapturedImage2Telegram(String token, String chat_id) {
       while (client_tcp.available()) 
       {
           char c = client_tcp.read();
+          if (state==true) getBody += String(c);       
           if (c == '\n') 
           {
             if (getAll.length()==0) state=true; 
@@ -528,7 +529,6 @@ String sendCapturedImage2Telegram(String token, String chat_id) {
           } 
           else if (c != '\r')
             getAll += String(c);
-          if (state==true) getBody += String(c);
           startTime = millis();
        }
        if (getBody.length()>0) break;
