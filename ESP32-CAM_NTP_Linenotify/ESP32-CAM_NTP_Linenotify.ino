0de22417bb4be79ed39690f761ee8a59721a0c13
fustyles <34397289+fustyles@users.noreply.github.com>
2021-04-19 23:49:58 +0800

    Add files via upload

---
+++ ESP32-CAM_NTP_Linenotify/ESP32-CAM_NTP_Linenotify.ino
@@ -0,0 +1,423 @@
+/*
+ESP32-CAM NTP　(Send a captured photo to Line Notify)
+Author : ChungYi Fu (Kaohsiung, Taiwan)  2021-4-19 23:30
+https://www.facebook.com/francefu
+
+You could only send up to 50 images to Line Notify in one hour.
+The maximum size of post-upload image is XGA(1024*768).
+*/
+
+// Enter your WiFi ssid and password
+const char* ssid     = "";   //your network SSID
+const char* password = "";   //your network password
+String myLineNotifyToken = "";    //Line Notify Token
+
+const char* ntpServer = "pool.ntp.org";
+const long  gmtOffset_sec = 25200;
+const int   daylightOffset_sec = 3600;
+
+struct tm timeinfo;
+String currentTime = "";
+
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
+#include "time.h"
+#include "img_converters.h"
+
+#include "fb_gfx.h"
+#include "fd_forward.h"
+
+typedef struct {
+        size_t size; //number of values used for filtering
+        size_t index; //current value index
+        size_t count; //value count
+        int sum;
+        int * values; //array to be filled with values
+} ra_filter_t;
+
+#define PART_BOUNDARY "123456789000000000000987654321"
+static const char* _STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
+static const char* _STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
+static const char* _STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
+
+static ra_filter_t ra_filter;
+
+static mtmn_config_t mtmn_config = {0};
+
+static ra_filter_t * ra_filter_init(ra_filter_t * filter, size_t sample_size){
+    memset(filter, 0, sizeof(ra_filter_t));
+
+    filter->values = (int *)malloc(sample_size * sizeof(int));
+    if(!filter->values){
+        return NULL;
+    }
+    memset(filter->values, 0, sample_size * sizeof(int));
+
+    filter->size = sample_size;
+    return filter;
+}
+
+static int ra_filter_run(ra_filter_t * filter, int value){
+    if(!filter->values){
+        return value;
+    }
+    filter->sum -= filter->values[filter->index];
+    filter->values[filter->index] = value;
+    filter->sum += filter->values[filter->index];
+    filter->index++;
+    filter->index = filter->index % filter->size;
+    if (filter->count < filter->size) {
+        filter->count++;
+    }
+    return filter->sum / filter->count;
+}
+
+// WARNING!!! Make sure that you have either selected ESP32 Wrover Module,
+//            or another board which has PSRAM enabled
+
+//CAMERA_MODEL_AI_THINKER
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
+static void rgb_print(dl_matrix3du_t *image_matrix, uint32_t color, const char * str){
+    fb_data_t fb;
+    fb.width = image_matrix->w;
+    fb.height = image_matrix->h;
+    fb.data = image_matrix->item;
+    fb.bytes_per_pixel = 3;
+    fb.format = FB_BGR888;
+    //fb_gfx_print(&fb, (fb.width - (strlen(str) * 14)) / 2, 10, color, str);
+    fb_gfx_print(&fb, 10, fb.height - 40, color, str);
+}
+
+static int rgb_printf(dl_matrix3du_t *image_matrix, uint32_t color, const char *format, ...){
+    char loc_buf[64];
+    char * temp = loc_buf;
+    int len;
+    va_list arg;
+    va_list copy;
+    va_start(arg, format);
+    va_copy(copy, arg);
+    len = vsnprintf(loc_buf, sizeof(loc_buf), format, arg);
+    va_end(copy);
+    if(len >= sizeof(loc_buf)){
+        temp = (char*)malloc(len+1);
+        if(temp == NULL) {
+            return 0;
+        }
+    }
+    vsnprintf(temp, len+1, format, arg);
+    va_end(arg);
+    Serial.println(temp);
+    rgb_print(image_matrix, color, temp);
+    if(len > 64){
+        free(temp);
+    }
+    return len;
+}
+
+void printLocalTime()
+{
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time");
+    return;
+  }
+  //Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+  /*
+    Member  Type  Meaning Range
+    tm_sec  int seconds after the minute  0-61*
+    tm_min  int minutes after the hour  0-59
+    tm_hour int hours since midnight  0-23
+    tm_mday int day of the month  1-31
+    tm_mon  int months since January  0-11
+    tm_year int years since 1900  
+    tm_wday int days since Sunday 0-6
+    tm_yday int days since January 1  0-365
+    tm_isdst  int Daylight Saving Time flag 
+  */
+  currentTime = String(timeinfo.tm_year+1900)+"/"+ String(timeinfo.tm_mon+1)+"/"+ String(timeinfo.tm_mday) + " "+ String(timeinfo.tm_hour)+":"+ String(timeinfo.tm_min)+":"+ String(timeinfo.tm_sec);
+}
+
+void setup()
+{
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
+  
+  Serial.begin(115200);
+  delay(10);
+  
+  WiFi.mode(WIFI_STA);
+
+  Serial.println("");
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);  
+  
+  long int StartTime=millis();
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    delay(500);
+    if ((StartTime+10000) < millis()) break;
+  } 
+
+  Serial.println("");
+  Serial.println("STAIP address: ");
+  Serial.println(WiFi.localIP());
+    
+  Serial.println("");
+
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Reset");
+    
+    ledcAttachPin(4, 3);
+    ledcSetup(3, 5000, 8);
+    ledcWrite(3,10);
+    delay(200);
+    ledcWrite(3,0);
+    delay(200);    
+    ledcDetachPin(3);
+        
+    delay(1000);
+    ESP.restart();
+  }
+  else {
+    ledcAttachPin(4, 3);
+    ledcSetup(3, 5000, 8);
+    for (int i=0;i<5;i++) {
+      ledcWrite(3,10);
+      delay(200);
+      ledcWrite(3,0);
+      delay(200);    
+    }
+    ledcDetachPin(3);      
+  }
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  //init with high specs to pre-allocate larger buffers
+  if(psramFound()){
+    config.frame_size = FRAMESIZE_UXGA;
+    config.jpeg_quality = 10;  //0-63 lower number means higher quality
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;  //0-63 lower number means higher quality
+    config.fb_count = 1;
+  }
+  
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    delay(1000);
+    ESP.restart();
+  }
+
+  //drop down frame size for higher initial frame rate
+  sensor_t * s = esp_camera_sensor_get();
+  s->set_framesize(s, FRAMESIZE_CIF);  // UXGA|SXGA|XGA|SVGA|VGA|CIF|QVGA|HQVGA|QQVGA
+  
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+}
+
+void loop()
+{
+  printLocalTime();
+  Serial.println(currentTime);
+  Serial.println(sendCapturedImage2LineNotify());
+  
+  delay(72000);  //You could only send up to 50 images to Line Notify in one hour.
+}
+
+String sendCapturedImage2LineNotify() {
+  dl_matrix3du_t *image_matrix = NULL; 
+  size_t _jpg_buf_len = 0;
+  uint8_t * _jpg_buf = NULL;
+  camera_fb_t * fb = NULL;
+  fb = esp_camera_fb_get();  
+  if (!fb) {
+      Serial.println("Camera capture failed");
+      return "error";
+  } else {
+      image_matrix = dl_matrix3du_alloc(1, fb->width, fb->height, 3);  //分配內部記憶體
+
+      fmt2rgb888(fb->buf, fb->len, fb->format, image_matrix->item);  //影像格式轉換RGB格式
+      face_detect(image_matrix, &mtmn_config);
+          
+      //在畫面上顯示時間
+      const char *str = currentTime.c_str();
+      rgb_print(image_matrix, 0x000000FF, str);
+      
+      fmt2jpg(image_matrix->item, fb->width*fb->height*3, fb->width, fb->height, PIXFORMAT_RGB888, 90, &_jpg_buf, &_jpg_buf_len);
+      dl_matrix3du_free(image_matrix);
+  }
+  
+  WiFiClientSecure client_tcp;
+
+  Serial.println("Connect to notify-api.line.me");
+  
+  if (client_tcp.connect("notify-api.line.me", 443)) {
+    Serial.println("Connection successful");
+    
+    String message = "ESP32-CAM";
+    String head = "--Taiwan\r\nContent-Disposition: form-data; name=\"message\"; \r\n\r\n" + message + "\r\n--Taiwan\r\nContent-Disposition: form-data; name=\"imageFile\"; filename=\"esp32-cam.jpg\"\r\nContent-Type: image/jpeg\r\n\r\n";
+    String tail = "\r\n--Taiwan--\r\n";
+
+    uint16_t imageLen = _jpg_buf_len;
+    uint16_t extraLen = head.length() + tail.length();
+    uint16_t totalLen = imageLen + extraLen;
+  
+    client_tcp.println("POST /api/notify HTTP/1.1");
+    client_tcp.println("Connection: close"); 
+    client_tcp.println("Host: notify-api.line.me");
+    client_tcp.println("Authorization: Bearer " + myLineNotifyToken);
+    client_tcp.println("Content-Length: " + String(totalLen));
+    client_tcp.println("Content-Type: multipart/form-data; boundary=Taiwan");
+    client_tcp.println();
+    client_tcp.print(head);
+    
+    uint8_t *fbBuf = _jpg_buf;
+    size_t fbLen = _jpg_buf_len;
+    for (size_t n=0;n<fbLen;n=n+1024) {
+      if (n+1024<fbLen) {
+        client_tcp.write(fbBuf, 1024);
+        fbBuf += 1024;
+      }
+      else if (fbLen%1024>0) {
+        size_t remainder = fbLen%1024;
+        client_tcp.write(fbBuf, remainder);
+      }
+    }  
+    
+    client_tcp.print(tail);
+    esp_camera_fb_return(fb);
+    free(_jpg_buf);
+    _jpg_buf = NULL;
+            
+    String getResponse="",Feedback="";
+    int waitTime = 10000;   // timeout 10 seconds
+    long startTime = millis();
+    boolean state = false;
+    
+    while ((startTime + waitTime) > millis()) {
+      Serial.print(".");
+      delay(100);      
+      while (client_tcp.available())  {
+          char c = client_tcp.read();
+          if (state==true) Feedback += String(c);        
+          if (c == '\n') {
+            if (getResponse.length()==0) state=true; 
+            getResponse = "";
+          } 
+          else if (c != '\r')
+            getResponse += String(c);
+          startTime = millis();
+       }
+       if (Feedback.length()>0) break;
+    }
+    Serial.println();
+    client_tcp.stop();
+    return Feedback;
+  }
+  else {
+    return "Connected to notify-api.line.me failed.";
+  }
+}
+
+String sendRequest2LineNotify(String request) {
+  request.replace("%","%25");
+  request.replace(" ","%20");
+  //request.replace("&","%20");
+  request.replace("#","%20");
+  //request.replace("\'","%27");
+  request.replace("\"","%22");
+  request.replace("\n","%0D%0A");
+  request.replace("%3Cbr%3E","%0D%0A");
+  request.replace("%3Cbr/%3E","%0D%0A");
+  request.replace("%3Cbr%20/%3E","%0D%0A");
+  request.replace("%3CBR%3E","%0D%0A");
+  request.replace("%3CBR/%3E","%0D%0A");
+  request.replace("%3CBR%20/%3E","%0D%0A"); 
+  request.replace("%20stickerPackageId","&stickerPackageId");
+  request.replace("%20stickerId","&stickerId");    
+
+  WiFiClientSecure client_tcp;
+  Serial.println("Connect to notify-api.line.me");  
+  
+  if (client_tcp.connect("notify-api.line.me", 443)) {
+    Serial.println("Connection successful");
+        
+    Serial.println(request);    
+    client_tcp.println("POST /api/notify HTTP/1.1");
+    client_tcp.println("Connection: close"); 
+    client_tcp.println("Host: notify-api.line.me");
+    client_tcp.println("User-Agent: ESP8266/1.0");
+    client_tcp.println("Authorization: Bearer " + myLineNotifyToken);
+    client_tcp.println("Content-Type: application/x-www-form-urlencoded");
+    client_tcp.println("Content-Length: " + String(request.length()));
+    client_tcp.println();
+    client_tcp.println(request);
+    client_tcp.println();
+    
+    String getResponse="",Feedback="";
+    boolean state = false;
+    int waitTime = 3000;   // timeout 3 seconds
+    long startTime = millis();
+    while ((startTime + waitTime) > millis()) {
+      Serial.print(".");
+      delay(100);      
+      while (client_tcp.available()) {
+          char c = client_tcp.read();
+          if (state==true) Feedback += String(c);        
+          if (c == '\n') {
+            if (getResponse.length()==0) state=true; 
+            getResponse = "";
+          } 
+          else if (c != '\r')
+            getResponse += String(c);
+          startTime = millis();
+       }
+       if (getResponse.length()>0) break;
+    }
+    Serial.println();
+    client_tcp.stop();
+    return Feedback;
+  }
+  else
+    return "Connection failed";  
+}
