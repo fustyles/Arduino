5af03fdf1fe7664f95a94c7bc8505f228615122f
fustyles <34397289+fustyles@users.noreply.github.com>
2021-04-24 22:59:16 +0800

    Update ESP32-CAM_SD_GoogleDrive_Linenotify.ino

--- ESP32-CAM_SD_GoogleDrive_Linenotify/ESP32-CAM_SD_GoogleDrive_Linenotify.ino
+++ ESP32-CAM_SD_GoogleDrive_Linenotify/ESP32-CAM_SD_GoogleDrive_Linenotify.ino
@@ -219,7 +219,7 @@ String SendCapturedImage() {
   
   Serial.println("Connect to " + String(myDomain));
   WiFiClientSecure client_tcp;
-  
+  //client_tcp.setInsecure();   //version 1.0.6
   if (client_tcp.connect(myDomain, 443)) {
     Serial.println("Connection successful");
     
