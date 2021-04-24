c0abde561f582ad3cfbefcabab708a6127fa548e
fustyles <34397289+fustyles@users.noreply.github.com>
2021-04-24 23:07:12 +0800

    Update ESP32-CAM_Telegram_PAGE.ino

--- ESP32-CAM_Telegram_PAGE/ESP32-CAM_Telegram_PAGE.ino
+++ ESP32-CAM_Telegram_PAGE/ESP32-CAM_Telegram_PAGE.ino
@@ -475,7 +475,7 @@ String sendCapturedImage2Telegram(String token, String chat_id) {
   
   Serial.println("Connect to " + String(myDomain));
   WiFiClientSecure client_tcp;
-  
+  //client_tcp.setInsecure();   //version 1.0.6
   if (client_tcp.connect(myDomain, 443)) {
     Serial.println("Connection successful");
     
