293998ba70ea2b07228c4f66e816c0e7d3efd1b2
fustyles <34397289+fustyles@users.noreply.github.com>
2021-04-24 23:23:32 +0800

    Update ESP32_PMS5003T.ino

--- ESP32_PMS5003T.ino
+++ ESP32_PMS5003T.ino
@@ -509,7 +509,7 @@ void getCommand(char c) {
 
 String tcp_https(String domain,String request,int port,byte wait) {
     WiFiClientSecure client_tcp;
-
+    //client_tcp.setInsecure();   //version 1.0.6
     if (client_tcp.connect(domain.c_str(), port)) {
       Serial.println("GET " + request);
       client_tcp.println("GET " + request + " HTTP/1.1");
@@ -562,7 +562,7 @@ String LineNotify(String token, String request, byte wait) {
   request.replace("%20stickerId","&stickerId");    
 
   WiFiClientSecure client_tcp;
-  
+  //client_tcp.setInsecure();   //version 1.0.6
   if (client_tcp.connect("notify-api.line.me", 443)) {
     Serial.println(request);    
     client_tcp.println("POST /api/notify HTTP/1.1");
