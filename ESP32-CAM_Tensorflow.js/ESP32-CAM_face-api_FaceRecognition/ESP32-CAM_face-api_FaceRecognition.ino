1af7abd41427c910ad8b0637561e97063d06d87c
fustyles <34397289+fustyles@users.noreply.github.com>
2021-04-24 23:16:06 +0800

    Update ESP32-CAM_face-api_FaceRecognition.ino

--- ESP32-CAM_Tensorflow.js/ESP32-CAM_face-api_FaceRecognition/ESP32-CAM_face-api_FaceRecognition.ino
+++ ESP32-CAM_Tensorflow.js/ESP32-CAM_face-api_FaceRecognition/ESP32-CAM_face-api_FaceRecognition.ino
@@ -710,7 +710,7 @@ String sendCapturedImage2Telegram(String token, String chat_id) {
   
   Serial.println("Connect to " + String(myDomain));
   WiFiClientSecure client_tcp;
-  
+  //client_tcp.setInsecure();   //version 1.0.6
   if (client_tcp.connect(myDomain, 443)) {
     Serial.println("Connection successful");
     
@@ -786,7 +786,7 @@ String sendMessage2Telegram(String token, String chat_id, String text) {
   
   Serial.println("Connect to " + String(myDomain));
   WiFiClientSecure client_tcp;
-  
+  //client_tcp.setInsecure();   //version 1.0.6
   if (client_tcp.connect(myDomain, 443)) {
     Serial.println("Connection successful");
 
