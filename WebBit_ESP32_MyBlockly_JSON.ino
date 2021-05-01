318faba18010cf45c42553712ed5aa1e636d6a44
fustyles <34397289+fustyles@users.noreply.github.com>
2021-05-01 22:53:39 +0800

    Update WebBit_ESP32_MyBlockly_JSON.ino

--- WebBit_ESP32_MyBlockly_JSON.ino
+++ WebBit_ESP32_MyBlockly_JSON.ino
@@ -690,7 +690,8 @@ String tcp_http(String domain,String request,int port,byte wait)
 String tcp_https(String domain,String request,int port,byte wait)
 {
     WiFiClientSecure client_tcp;
-    //client_tcp.setInsecure();   //version 1.0.6
+    client_tcp.setInsecure();   //run version 1.0.5 or above
+    
     if (client_tcp.connect(domain.c_str(), port)) 
     {
       Serial.println("GET " + request);
@@ -748,7 +749,8 @@ String LineNotify(String token, String request, byte wait)
   request.replace("%20stickerId","&stickerId");    
   
   WiFiClientSecure client_tcp;
-  //client_tcp.setInsecure();   //version 1.0.6
+  client_tcp.setInsecure();   //run version 1.0.5 or above
+    
   if (client_tcp.connect("notify-api.line.me", 443)) 
   {
     client_tcp.println("POST /api/notify HTTP/1.1");
