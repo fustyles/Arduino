fdc8363b7f8fb4745fb53d410776b68fc092d621
fustyles <34397289+fustyles@users.noreply.github.com>
2021-05-01 22:38:27 +0800

    Update ESP32-CAM_PIR_GoogleDrive.ino

--- ESP32-CAM_PIR_GoogleDrive/ESP32-CAM_PIR_GoogleDrive.ino
+++ ESP32-CAM_PIR_GoogleDrive/ESP32-CAM_PIR_GoogleDrive.ino
@@ -215,7 +215,8 @@ String SendCapturedImage2GoogleDrive() {
   
   Serial.println("Connect to " + String(myDomain));
   WiFiClientSecure client_tcp;
-  //client_tcp.setInsecure();   //version 1.0.6
+  client_tcp.setInsecure();   //run version 1.0.5 or above
+  
   if (client_tcp.connect(myDomain, 443)) {
     Serial.println("Connection successful");
     
