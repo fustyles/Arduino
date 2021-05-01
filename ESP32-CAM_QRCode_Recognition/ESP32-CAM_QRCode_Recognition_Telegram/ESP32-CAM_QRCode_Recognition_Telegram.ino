9cb24626eea90003ea9b7bceda02a307e8633629
fustyles <34397289+fustyles@users.noreply.github.com>
2021-05-01 22:31:03 +0800

    Update ESP32-CAM_QRCode_Recognition_Telegram.ino

--- ESP32-CAM_QRCode_Recognition/ESP32-CAM_QRCode_Recognition_Telegram/ESP32-CAM_QRCode_Recognition_Telegram.ino
+++ ESP32-CAM_QRCode_Recognition/ESP32-CAM_QRCode_Recognition_Telegram/ESP32-CAM_QRCode_Recognition_Telegram.ino
@@ -158,7 +158,8 @@ String sendMessage2Telegram(String token, String chat_id, String text) {
   
   Serial.println("Connect to " + String(myDomain));
   WiFiClientSecure client_tcp;
-  //client_tcp.setInsecure();   //version 1.0.6
+  client_tcp.setInsecure();   //run version 1.0.5 or above
+  
   if (client_tcp.connect(myDomain, 443)) {
     Serial.println("Connection successful");
 
