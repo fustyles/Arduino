55d8a3ed8af70ecbb92f228adcb4d3707db1eec5
fustyles <34397289+fustyles@users.noreply.github.com>
2021-05-01 22:39:04 +0800

    Update ESP32-CAM_PIR_GoogleSpreadsheet.ino

--- ESP32-CAM_PIR_GoogleSpreadsheet/ESP32-CAM_PIR_GoogleSpreadsheet.ino
+++ ESP32-CAM_PIR_GoogleSpreadsheet/ESP32-CAM_PIR_GoogleSpreadsheet.ino
@@ -218,7 +218,8 @@ String SendCapturedImage2Spreadsheet() {
         
   Serial.println("Connect to " + String(myDomain));
   WiFiClientSecure client_tcp;
-  //client_tcp.setInsecure();   //version 1.0.6
+  client_tcp.setInsecure();   //run version 1.0.5 or above
+  
   if (client_tcp.connect(myDomain, 443)) {
     Serial.println("Connection successful");
         
