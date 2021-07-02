7f8d1faa915e105b6831641157d040eee2e35038
fustyles <34397289+fustyles@users.noreply.github.com>
2021-07-02 20:50:40 +0800

    Update ESP32-CAM_SD_Manager_PAGE.ino

--- ESP32-CAM_SD_Manager_PAGE/ESP32-CAM_SD_Manager_PAGE.ino
+++ ESP32-CAM_SD_Manager_PAGE/ESP32-CAM_SD_Manager_PAGE.ino
@@ -29,7 +29,7 @@ http://192.168.xxx.xxx?deleteimage=/filename     //刪除SD卡影像
 
 http://192.168.xxx.xxx?flash=value              //閃光燈 value= 0~255
 http://192.168.xxx.xxx?servo=pin;value          //伺服馬達 value= 0~180
-http://192.168.xxx.xxxl?relay=pin;value         //繼電器 value = 0, 1
+http://192.168.xxx.xxx?relay=pin;value          //繼電器 value = 0, 1
 http://192.168.xxx.xxx?framesize=value          //解析度 value = 10->UXGA(1600x1200), 9->SXGA(1280x1024), 8->XGA(1024x768) ,7->SVGA(800x600), 6->VGA(640x480), 5 selected=selected->CIF(400x296), 4->QVGA(320x240), 3->HQVGA(240x176), 0->QQVGA(160x120), 11->QXGA(2048x1564 for OV3660)
 http://192.168.xxx.xxx?quality&val=value        //畫質 value = 10 ~ 63
 http://192.168.xxx.xxx?brightness=value         //亮度 value = -2 ~ 2
