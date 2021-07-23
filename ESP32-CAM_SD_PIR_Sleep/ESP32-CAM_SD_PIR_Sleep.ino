6345f03c18530c372886ea3e6716df7520d5cd40
fustyles <34397289+fustyles@users.noreply.github.com>
2021-07-23 16:32:43 +0800

    Add files via upload

---
+++ ESP32-CAM_SD_PIR_Sleep/ESP32-CAM_SD_PIR_Sleep.ino
@@ -0,0 +1,225 @@
+/*
+ESP32-CAM PIR人體移動感測器觸發影像存檔於SD卡，並進入睡眠模式高電位觸發
+Author : ChungYi Fu (Kaohsiung, Taiwan)  2021-7-23 00:00
+https://www.facebook.com/francefu
+
+PIR人體移動感測器 -> GND, IO13, 3.3V
+*/
+ 
+#include "esp_camera.h"
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
+#include "driver/rtc_io.h"
+#include "FS.h"
+#include "SD_MMC.h"
+
+#include <Preferences.h>
+Preferences preferences;
+
+//Arduino IDE開發版選擇 ESP32 Wrover Module
+
+//ESP32-CAM 安信可模組腳位設定
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+  
+void setup() {
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);  //關閉電源不穩就重開機的設定
+    
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);  //開啟診斷輸出
+  Serial.println();
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  
+  //
+  // WARNING!!! PSRAM IC required for UXGA resolution and high JPEG quality
+  //            Ensure ESP32 Wrover Module or other board with PSRAM is selected
+  //            Partial images will be transmitted if image exceeds buffer size
+  //   
+  // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
+  //                      for larger pre-allocated frame buffer.
+  if(psramFound()){  //是否有PSRAM(Psuedo SRAM)記憶體IC
+    config.frame_size = FRAMESIZE_UXGA;
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+
+  //視訊初始化
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    ESP.restart();
+  }
+
+  //可自訂視訊框架預設大小(解析度大小)
+  sensor_t * s = esp_camera_sensor_get();
+  // initial sensors are flipped vertically and colors are a bit saturated
+  if (s->id.PID == OV3660_PID) {
+    s->set_vflip(s, 1); // flip it back
+    s->set_brightness(s, 1); // up the brightness just a bit
+    s->set_saturation(s, -2); // lower the saturation
+  }
+  // drop down frame size for higher initial frame rate
+  s->set_framesize(s, FRAMESIZE_UXGA);  // UXGA|SXGA|XGA|SVGA|VGA|CIF|QVGA|HQVGA|QQVGA
+
+  //s->set_vflip(s, 1);  //垂直翻轉
+  //s->set_hmirror(s, 1);  //水平鏡像
+  
+  //閃光燈(GPIO4)
+  ledcAttachPin(4, 4);  
+  ledcSetup(4, 5000, 8);
+
+  //SD Card
+  if(!SD_MMC.begin()){
+    Serial.println("Card Mount Failed");
+    ESP.restart();
+  }
+
+  uint8_t cardType = SD_MMC.cardType();
+
+  if(cardType == CARD_NONE){
+    Serial.println("No SD_MMC card attached");
+    SD_MMC.end();
+    ESP.restart();
+  }  
+
+  Serial.printf("SD_MMC Card Size: %lluMB\n", SD_MMC.cardSize() / (1024 * 1024));
+  Serial.printf("Total space: %lluMB\n", SD_MMC.totalBytes() / (1024 * 1024));
+  Serial.printf("Used space: %lluMB\n", SD_MMC.usedBytes() / (1024 * 1024));
+  Serial.println();
+  
+  SD_MMC.end();
+   
+  /*
+  //檔案流水號重設
+  preferences.begin("SD", false);
+  preferences.putUInt("number", 0);
+  preferences.end(); 
+  */
+
+  preferences.begin("SD", false);
+  int n = preferences.getUInt("number", 0);
+  
+  saveCapturedImage2SD(String(n+1));
+  
+  preferences.putUInt("number", (n+1));
+  preferences.end();   
+  
+  Serial.println("Going to sleep now");
+  rtc_gpio_pullup_en(GPIO_NUM_13);
+  esp_sleep_enable_ext0_wakeup(GPIO_NUM_13, 1);
+  esp_deep_sleep_start();
+  Serial.println("This will never be printed");
+} 
+ 
+void loop() {
+ 
+}
+
+void saveCapturedImage2SD(String filename) {
+  camera_fb_t * fb = NULL;
+  fb = esp_camera_fb_get();
+  if (!fb) {
+    Serial.println("Camera capture failed");
+    return;
+  }
+
+  //SD Card
+  if(!SD_MMC.begin()){
+    Serial.println("Card Mount Failed");
+    return;
+  }  
+  
+  String path = "/"+filename+".jpg";
+
+  fs::FS &fs = SD_MMC; 
+  Serial.printf("Picture file name: %s\n", path.c_str());
+
+  File file = fs.open(path.c_str(), FILE_WRITE);
+  if(!file){
+    Serial.println("Failed to open file in writing mode");
+  } 
+  else {
+    file.write(fb->buf, fb->len);
+    Serial.printf("Saved file to path: %s\n", path.c_str());
+  }
+  file.close();
+  
+  file = fs.open(path.c_str());
+  Serial.println("File Size = " + String(file.size()));
+  
+  if (file.size()==0) {
+    Serial.println("Failed");
+    file.close();
+    
+    Serial.println("Try again...");
+
+    file = fs.open(path.c_str(), FILE_WRITE);
+    if (file) {
+      file.write(fb->buf, fb->len);
+      file.close();
+    
+      file = fs.open(path.c_str());
+      Serial.println("File Size = " + String(file.size()));
+      if (file.size()==0) {
+        Serial.println("Failed");
+      }
+      else {
+        Serial.println("Success");
+      }
+    }
+    else {
+      Serial.println("Failed");
+    }
+  }
+  else {
+    Serial.println("Success");
+  }
+  file.close();
+  SD_MMC.end();
+
+  Serial.println("");  
+  esp_camera_fb_return(fb);
+
+  pinMode(4, OUTPUT);
+  digitalWrite(4, LOW);  
+}
