3034e11ff7bb4823e79467839a21b96eb9a8ba23
fustyles <34397289+fustyles@users.noreply.github.com>
2021-07-29 00:45:03 +0800

    Update ESP32-CAM_OTTO_20161204.ino

--- ESP32-CAM_OTTO/OttoRobot_20161204/ESP32-CAM_OTTO_20161204/ESP32-CAM_OTTO_20161204.ino
+++ ESP32-CAM_OTTO/OttoRobot_20161204/ESP32-CAM_OTTO_20161204/ESP32-CAM_OTTO_20161204.ino
@@ -43,8 +43,8 @@ http://192.168.xxx.xxx/control?var=flash&val=value        // value = 0 ~ 255
 */
 
 //輸入WIFI連線帳號密碼
-const char* ssid     = "3COM";   //your network SSID
-const char* password = "godblessyou";   //your network password
+const char* ssid     = "*****";   //your network SSID
+const char* password = "*****";   //your network password
 
 //輸入AP端連線帳號密碼
 const char* apssid = "ESP32-CAM";
