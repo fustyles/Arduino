230c7363420394a359a7119bd0cd67d9fd01e357
fustyles <34397289+fustyles@users.noreply.github.com>
2022-01-15 15:23:53 +0800

    Update ESP32_WebBluetooth.ino

--- ESP32_WebBluetooth.ino
+++ ESP32_WebBluetooth.ino
@@ -20,7 +20,7 @@ bool deviceConnected = false;
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
 
-#define SERVICE_UUID           "4fafc201-1fb5-459e-8fcc-c5c9c331914b" // UART service UUID
+#define SERVICE_UUID           "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID_RX "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 #define CHARACTERISTIC_UUID_TX "498c599b-2601-4600-bb7e-3aa295a92842"
 
