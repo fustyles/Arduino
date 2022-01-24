7403da25209f047f4934ff44f841b62d91f02a83
fustyles <34397289+fustyles@users.noreply.github.com>
2022-01-24 08:36:34 +0800

    Update ESP32_Keyboard_PPT_RC.ino

--- ESP32_Keyboard_PPT_RC.ino
+++ ESP32_Keyboard_PPT_RC.ino
@@ -216,22 +216,11 @@ void ExecuteCommand() {
       Serial.println("Sending 'Hello world'...");
       bleKeyboard.print("Hello world");
   
-      delay(1000);
   
       Serial.println("Sending Enter key...");
       bleKeyboard.write(KEY_RETURN);
   
-      delay(1000);
   
-      Serial.println("Sending Play/Pause media key...");
-      bleKeyboard.write(KEY_MEDIA_PLAY_PAUSE);
-  
-      delay(1000);
-  
-     //
-     // Below is an example of pressing multiple keyboard modifiers 
-     // which by default is commented out.
-      /*
       Serial.println("Sending Ctrl+Alt+Delete...");
       bleKeyboard.press(KEY_LEFT_CTRL);
       bleKeyboard.press(KEY_LEFT_ALT);
