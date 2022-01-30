b1bd96607590707e24d3a655a4ed502f878f3746
fustyles <34397289+fustyles@users.noreply.github.com>
2022-01-30 10:47:14 +0800

    Update ESP32_MQTT.ino

--- ESP32_MQTT.ino
+++ ESP32_MQTT.ino
@@ -66,6 +66,7 @@ void executeCommand() {
   //Serial.println("command: "+command);
   //Serial.println("cmd= "+cmd+" ,P1= "+P1+" ,P2= "+P2+" ,P3= "+P3+" ,P4= "+P4+" ,P5= "+P5+" ,P6= "+P6+" ,P7= "+P7+" ,P8= "+P8+" ,P9= "+P9);
   //Serial.println("");
+  feedback="";
   
   if (cmd=="your cmd") {
     // You can do anything
