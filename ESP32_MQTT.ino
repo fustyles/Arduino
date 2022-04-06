f68f2756ed24da542d9f53888ab31f7fc2391cd1
fustyles <34397289+fustyles@users.noreply.github.com>
2022-04-07 01:10:35 +0800

    Update ESP32_MQTT.ino

--- ESP32_MQTT.ino
+++ ESP32_MQTT.ino
@@ -33,8 +33,8 @@ const char* mqtt_server = "broker.emqx.io";
 const unsigned int mqtt_port = 1883;
 #define MQTT_USER               ""
 #define MQTT_PASSWORD           ""
-#define MQTT_PUBLISH_TOPIC    "yourname/send"
-#define MQTT_SUBSCRIBE_TOPIC    "yourname/get"
+#define MQTT_PUBLISH_TOPIC    "yourtopic/send"
+#define MQTT_SUBSCRIBE_TOPIC    "yourtopic/get"
     
 WiFiClient espClient;
 PubSubClient client(espClient);
