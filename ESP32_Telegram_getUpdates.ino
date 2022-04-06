ce758a3f6c05a32287c5f03e520590bf58d32ed8
fustyles <34397289+fustyles@users.noreply.github.com>
2022-04-06 19:22:17 +0800

    Update ESP32_Telegram_getUpdates.ino

--- ESP32_Telegram_getUpdates.ino
+++ ESP32_Telegram_getUpdates.ino
@@ -16,7 +16,7 @@ const char* ssid     = "*****";   //your network SSID
 const char* password = "*****";   //your network password
 
 String token = "*****:*****";   // Create your bot and get the token -> https://telegram.me/fatherbot
-String chat_id = "*****";   // Get chat_id -> https://telegram.me/chatid_echo_bot
+String chat_id = "*****";   // Get chat_id -> https://telegram.me/userinfobot
 
 /*
 If "sendHelp" variable is equal to "true", it will send the command list to Telegram Bot when the board boots. 
