9867710325385db1916565919b19399a5b324c46
fustyles <34397289+fustyles@users.noreply.github.com>
2022-04-17 10:09:02 +0800

    Update ESP32_Telegram_getUpdates_MQTT.ino

--- ESP32_Telegram_getUpdates_MQTT.ino
+++ ESP32_Telegram_getUpdates_MQTT.ino
@@ -194,6 +194,7 @@ void getTelegramMessage() {
         while (client_tcp.available()) 
         {
             char c = client_tcp.read();
+            if (state==true) getBody += String(c);
             if (c == '\n') 
             {
               if (getAll.length()==0) state=true; 
@@ -201,7 +202,6 @@ void getTelegramMessage() {
             } 
             else if (c != '\r')
               getAll += String(c);
-            if (state==true) getBody += String(c);
             startTime = millis();
          }
          if (getBody.length()>0) break;
