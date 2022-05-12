57c8a18f8dfdbd72716c7bca7874ebefcca5e6da
fustyles <34397289+fustyles@users.noreply.github.com>
2022-05-12 10:29:13 +0800

    Update ESP32_TaiwanWeather1.ino

--- ESP32_TaiwanWeather1.ino
+++ ESP32_TaiwanWeather1.ino
@@ -24,7 +24,7 @@ void setup()
 void loop()
 {
   //宜蘭縣,花蓮縣,臺東縣,澎湖縣,金門縣,連江縣,臺北市,新北市,桃園市,臺中市,臺中市,臺南市,高雄市,基隆市,新竹縣,新竹市,苗栗縣,彰化縣,南投縣,雲林縣,嘉義縣,嘉義市,屏東縣
-  retrieveWeather("高雄市","rdec-key-123-45678-011121314");
+  opendataWeather("高雄市","rdec-key-123-45678-011121314");
 
   Serial.println("位置= "+getWeather(0, 0));
   Serial.println("開始時間= "+getWeather(0, 1));
@@ -84,7 +84,7 @@ void initWiFi() {
   }
 }
 
-void retrieveWeather(String location, String Authorization) {
+void opendataWeather(String location, String Authorization) {
   WiFiClientSecure client_tcp;
   String request = "/api/v1/rest/datastore/F-C0032-001?Authorization="+Authorization+"&locationName="+urlencode(location);
   if (client_tcp.connect("opendata.cwb.gov.tw", 443)) {
