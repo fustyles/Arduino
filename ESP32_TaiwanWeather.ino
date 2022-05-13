046e8f5a153ca6fea2c4e0aea29bd3d1c19e214a
fustyles <34397289+fustyles@users.noreply.github.com>
2022-05-13 10:06:53 +0800

    Update ESP32_TaiwanWeather.ino

--- ESP32_TaiwanWeather.ino
+++ ESP32_TaiwanWeather.ino
@@ -180,6 +180,7 @@ String getWeather(int period,int index) {   //period=0,1,2  index=0,1,2,3,4,5,6,
   return "";
 }
 
+//https://github.com/zenmanenergy/ESP8266-Arduino-Examples/
 String urlencode(String str) {
     String encodedString="";
     char c;
