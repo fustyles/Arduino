68a388450b0f6d5cc6f5ab2c53d30241d39ebbfc
fustyles <34397289+fustyles@users.noreply.github.com>
2022-05-15 13:04:25 +0800

    Update ESP32_TaiwanWeather.ino

--- ESP32_TaiwanWeather.ino
+++ ESP32_TaiwanWeather.ino
@@ -88,6 +88,8 @@ void initWiFi() {
 void opendataWeather(String location, String Authorization) {
   location = urlencode(location);
   WiFiClientSecure client_tcp;
+  //client_tcp.setInsecure();  //for 1.0.5 or 1.0.6
+
   String request = "/api/v1/rest/datastore/F-C0032-001?Authorization="+Authorization+"&locationName="+location;
   if (client_tcp.connect("opendata.cwb.gov.tw", 443)) {
     client_tcp.println("GET " + request + " HTTP/1.1");
