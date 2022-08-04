fa4a69ddcaead601b24803ab6cf81f4cf97caa89
fustyles <34397289+fustyles@users.noreply.github.com>
2022-08-04 17:40:14 +0800

    Add files via upload

--- ESP32_Linebot_Spreadsheet.ino
+++ ESP32_Linebot_Spreadsheet.ino
@@ -1,9 +1,9 @@
 /*
 ESP32 LineBot using spreadsheet
-Author : ChungYi Fu (Kaohsiung, Taiwan)   2022/8/4 12:30
+Author : ChungYi Fu (Kaohsiung, Taiwan)   2022/8/4 17:30
 https://www.facebook.com/francefu
 
-Google apps script
+apps Script
 https://github.com/fustyles/webduino/blob/gs/Linebot_Spreadsheet.gs
  */
  
@@ -13,12 +13,36 @@ https://github.com/fustyles/webduino/blob/gs/Linebot_Spreadsheet.gs
 
 char _lwifi_ssid[] = "teacher";
 char _lwifi_pass[] = "87654321";
-
-String spreadsheetID = "1zztiZMyQ7HplFp0cHc0dKpiomJuStz_hFIs";
+String spreadsheetID = "1zztiZMyQ7HplFp0cHc0dKpiomZLDDfu8nJuStz_hFIss";
 String spreadsheetName = "工作表1";
+String appsScriptID = "AKfycbx-9F6o1gl6-404JBXkpiJQ0-wCyn8tFlxPXZOqiX3qeuoxjlMlv9FdhsXkZY4jYHmss";
 
-String appsScriptID = "AKfycbx-9F6o1gl6-404JBXkpiJQ0-wCyn8tFlxPXZOqiX3FdhsXkZY4jYHms";
+String spreadsheetQueryData = "{\"values\":[]}";
 
+void setup()
+{
+  Serial.begin(115200);
+  initWiFi();
+
+}
+
+void loop()
+{
+  spreadsheetQueryData = Spreadsheet_query("select A, B limit 1 offset 0", String(spreadsheetID), String(spreadsheetName));
+  String message = Spreadsheet_getcell_query(0, 0);
+  String replyToken = Spreadsheet_getcell_query(0, 1);
+  if (message != "") {
+    Serial.println(message);
+    Serial.println(replyToken);
+    if (message == "on") {
+      message = "Led on";
+    } else if (message == "off") {
+      message = "Led off";
+    }
+    tcp_https_esp32("POST", "script.google.com", "/macros/s/"+appsScriptID+"/exec?response="+urlencode(message)+"&token="+String(replyToken), 443, 3000);
+  }
+  delay(1000);
+}
 
 void initWiFi() {
 
@@ -47,18 +71,16 @@ void initWiFi() {
   }
 }
 
-String spreadsheetQueryData = "{\"values\":[]}";
-
 String Spreadsheet_query(String sql, String mySpreadsheetid, String mySpreadsheetname) {
   sql = urlencode(sql);
   mySpreadsheetname = urlencode(mySpreadsheetname);
   const char* myDomain = "docs.google.com";
   String getAll="", getBody = "", getData = "";
-  Serial.println("Connect to " + String(myDomain));
+  //Serial.println("Connect to " + String(myDomain));
   WiFiClientSecure client_tcp;
   client_tcp.setInsecure();
   if (client_tcp.connect(myDomain, 443)) {
-    Serial.println("Connection successful");
+    //Serial.println("Connection successful");
     String url = "https://docs.google.com/spreadsheets/d/"+mySpreadsheetid+"/gviz/tq?tqx=out:json&sheet="+mySpreadsheetname+"&tq="+sql;
     client_tcp.println("GET "+url+" HTTP/1.1");
     client_tcp.println("Host: " + String(myDomain));
@@ -69,12 +91,10 @@ String Spreadsheet_query(String sql, String mySpreadsheetid, String mySpreadshee
     boolean state = false;
     boolean start = false;
 
-    while ((startTime + waitTime) > millis())
-    {
-      Serial.print(".");
+    while ((startTime + waitTime) > millis()) {
+      //Serial.print(".");
       delay(100);
-      while (client_tcp.available())
-      {
+      while (client_tcp.available()) {
           char c = client_tcp.read();
           if (getBody.indexOf("\"rows\":[")!=-1) start = true;
           if (getData.indexOf("],")!=-1) start = false;
@@ -103,59 +123,15 @@ String Spreadsheet_query(String sql, String mySpreadsheetid, String mySpreadshee
   }
 }
 
-String urlencode(String str)
-{
-  String encodedString="";
-  char c;
-  char code0;
-  char code1;
-  char code2;
-  for (int i =0; i < str.length(); i++){
-    c=str.charAt(i);
-    if (c == ' '){
-      encodedString+= '+';
-    } else if (isalnum(c)){
-      encodedString+=c;
-    } else{
-      code1=(c & 0xf)+'0';
-      if ((c & 0xf) >9){
-          code1=(c & 0xf) - 10 + 'A';
-      }
-      c=(c>>4)&0xf;
-      code0=c+'0';
-      if (c > 9){
-          code0=c - 10 + 'A';
-      }
-      code2='\0';
-      encodedString+='%';
-      encodedString+=code0;
-      encodedString+=code1;
-      //encodedString+=code2;
-    }
-    yield();
-  }
-  return encodedString;
-}
-
-String colsToList(int cols, int cole)
-{
-  String colList = "";
-  for (int i=cols;i<=cole;i++){
-    colList += String(char(i-1+65))+",";
-  }
-  colList = colList.substring(0, colList.length()-1);
-  return colList;
-}
-
 String Spreadsheet_getcell_query(int row, int col) {
     if (spreadsheetQueryData!="") {
     	JsonObject obj;
     	DynamicJsonDocument doc(1024);
     	deserializeJson(doc, spreadsheetQueryData);
     	obj = doc.as<JsonObject>();
-		if ((obj["values"].size()<row+1)||(obj["values"][0]["c"].size()<col+1))
-			return "";
-    	return obj["values"][row]["c"][col]["v"].as<String>();
+  		if ((obj["values"].size()<row+1)||(obj["values"][0]["c"].size()<col+1))
+  			return "";
+      	return obj["values"][row]["c"][col]["v"].as<String>();
     }
     else
 		return "";
@@ -197,27 +173,36 @@ String tcp_https_esp32(String type,String domain,String request,int port,int wai
   return getBody;
 }
 
-void setup()
+String urlencode(String str)
 {
-  Serial.begin(115200);
-  initWiFi();
-
-}
-
-void loop()
-{
-  spreadsheetQueryData = Spreadsheet_query("select " + colsToList(1, 2) + " limit " + String(1-1+1)+ " offset "+ String(1-1), String(spreadsheetID), String(spreadsheetName));
-  String message = Spreadsheet_getcell_query(0, 0);
-  String replyToken = Spreadsheet_getcell_query(0, 1);
-  if (message != "") {
-    Serial.println(message);
-    Serial.println(replyToken);
-    if (message == "on") {
-      message = "Led on";
-    } else if (message == "off") {
-      message = "Led off";
+  String encodedString="";
+  char c;
+  char code0;
+  char code1;
+  char code2;
+  for (int i =0; i < str.length(); i++) {
+    c=str.charAt(i);
+    if (c == ' ') {
+      encodedString+= '+';
+    } else if (isalnum(c)) {
+      encodedString+=c;
+    } else {
+      code1=(c & 0xf)+'0';
+      if ((c & 0xf) >9) {
+          code1=(c & 0xf) - 10 + 'A';
+      }
+      c=(c>>4)&0xf;
+      code0=c+'0';
+      if (c > 9) {
+          code0=c - 10 + 'A';
+      }
+      code2='\0';
+      encodedString+='%';
+      encodedString+=code0;
+      encodedString+=code1;
+      //encodedString+=code2;
     }
-    String get = (tcp_https_esp32("POST", "script.google.com", (String("/macros/s/"+appsScriptID+"/exec")+String("?response=")+String(urlencode(message))+String("&token=")+String(replyToken)), 443, 3000));
+    yield();
   }
-  delay(1000);
+  return encodedString;
 }
