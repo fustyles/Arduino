3f54e0a12ebb0e8e7f79c26f8e6e703c92a95bad
fustyles <34397289+fustyles@users.noreply.github.com>
2022-08-07 13:36:31 +0800

    Update ESP32-CAM_Linebot_Spreadsheet.ino

--- ESP32-CAM_Linebot_Spreadsheet/ESP32-CAM_Linebot_Spreadsheet.ino
+++ ESP32-CAM_Linebot_Spreadsheet/ESP32-CAM_Linebot_Spreadsheet.ino
@@ -261,40 +261,6 @@ String tcp_https(String type,String domain,String request,int port,int waittime)
   return getBody;
 }
 
-String urlencode(String str)
-{
-  String encodedString="";
-  char c;
-  char code0;
-  char code1;
-  char code2;
-  for (int i =0; i < str.length(); i++) {
-    c=str.charAt(i);
-    if (c == ' ') {
-      encodedString+= '+';
-    } else if (isalnum(c)) {
-      encodedString+=c;
-    } else {
-      code1=(c & 0xf)+'0';
-      if ((c & 0xf) >9) {
-          code1=(c & 0xf) - 10 + 'A';
-      }
-      c=(c>>4)&0xf;
-      code0=c+'0';
-      if (c > 9) {
-          code0=c - 10 + 'A';
-      }
-      code2='\0';
-      encodedString+='%';
-      encodedString+=code0;
-      encodedString+=code1;
-      //encodedString+=code2;
-    }
-    yield();
-  }
-  return encodedString;
-}
-
 String SendCapturedImage(String token) {
   const char* myDomain = "script.google.com";
   String getAll="", getBody = "";
@@ -370,4 +336,37 @@ String SendCapturedImage(String token) {
   }
   
   return getBody;
+}
+
+String urlencode(String str) {
+  String encodedString="";
+  char c;
+  char code0;
+  char code1;
+  char code2;
+  for (int i =0; i < str.length(); i++) {
+    c=str.charAt(i);
+    if (c == ' ') {
+      encodedString+= '+';
+    } else if (isalnum(c)) {
+      encodedString+=c;
+    } else {
+      code1=(c & 0xf)+'0';
+      if ((c & 0xf) >9) {
+          code1=(c & 0xf) - 10 + 'A';
+      }
+      c=(c>>4)&0xf;
+      code0=c+'0';
+      if (c > 9) {
+          code0=c - 10 + 'A';
+      }
+      code2='\0';
+      encodedString+='%';
+      encodedString+=code0;
+      encodedString+=code1;
+      //encodedString+=code2;
+    }
+    yield();
+  }
+  return encodedString;
 }
