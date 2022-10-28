f20f000231d3d2960743f5326d4023067ba1b96e
fustyles <34397289+fustyles@users.noreply.github.com>
2022-10-29 07:50:36 +0800

    Update ESP32_Keyboard_PPT_RC.ino

--- ESP32_Keyboard_PPT_RC.ino
+++ ESP32_Keyboard_PPT_RC.ino
@@ -6,9 +6,6 @@ https://www.facebook.com/francefu
 Library: 
 https://github.com/T-vK/ESP32-BLE-Keyboard
 
-Keyboard Modifiers (keyboardpress)
-https://www.arduino.cc/en/Reference/KeyboardModifiers
-
 Command Format :  
 http://APIP/?cmd=P1;P2;P3;P4;P5;P6;P7;P8;P9
 http://STAIP/?cmd=P1;P2;P3;P4;P5;P6;P7;P8;P9
@@ -30,7 +27,7 @@ http://192.168.xxx.xxx/?keyboardpress=keycode1;keycode2;keycode3;presstime
 http://192.168.xxx.xxx/?keyboardprint=characters
 http://192.168.xxx.xxx/?keyboardwrite=keycode
  
-Remote Control for PPT  (keyboard press = keycode1, keycode2, keycode3, pressTime)
+Remote Control for PPT
 ?keyboardwrite=198              "F5"
 ?keyboardwrite=211              "PAGE UP"
 ?keyboardpress=133;198;;10      "SHIFT+F5"      
@@ -39,7 +36,7 @@ Remote Control for PPT  (keyboard press = keycode1, keycode2, keycode3, pressTim
 ?keyboardwrite=177              "ESC"
 ?keyboardwrite=66               "B"
 
-Remote Control for Game  (keyboard press = keycode1, keycode2, keycode3, pressTime)
+Remote Control for Game
 ?keyboardpress=215;;;100       "KEY_RIGHT_ARROW"
 ?keyboardpress=216;;;100       "KEY_LEFT_ARROW"
 ?keyboardpress=217;;;100       "KEY_DOWN_ARROW"
