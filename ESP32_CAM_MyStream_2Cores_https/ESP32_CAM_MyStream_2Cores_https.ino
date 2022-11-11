2f57e289870e387e2a501f26a71b6b06b4acb363
fustyles <34397289+fustyles@users.noreply.github.com>
2022-11-12 02:29:26 +0800

    Update ESP32_CAM_MyStream_2Cores_https.ino

--- ESP32_CAM_MyStream_2Cores_https/ESP32_CAM_MyStream_2Cores_https.ino
+++ ESP32_CAM_MyStream_2Cores_https/ESP32_CAM_MyStream_2Cores_https.ino
@@ -7,11 +7,14 @@ https://www.facebook.com/francefu
 library:
 https://github.com/fhessel/esp32_https_server
 
-stream
+stream (https)
 https://yourIP
 
-stop strteam
+stop stream (http)
 http://yourIP/?stop
+
+custom command (http)
+http://yourIP/?cmd=p1;p2;p3;p4;p5;p6;p7;p8;p9 
 */
 
 
