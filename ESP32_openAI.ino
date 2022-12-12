75df8c23f6889d55e027df4774f37766a852787f
fustyles <34397289+fustyles@users.noreply.github.com>
2022-12-12 22:01:17 +0800

    Update ESP32_openAI.ino

--- ESP32_openAI.ino
+++ ESP32_openAI.ino
@@ -112,34 +112,3 @@ String openAI(String request) {
   else
     return "Connection failed";  
 }
-
-//https://github.com/zenmanenergy/ESP8266-Arduino-Examples/
-String urlencode(String str) {
-    String encodedString="";
-    char c;
-    char code0;
-    char code1;
-    for (int i =0; i < str.length(); i++) {
-      c=str.charAt(i);
-      if (c == ' '){
-        encodedString+= '+';
-      } else if (isalnum(c)){
-        encodedString+=c;
-      } else{
-        code1=(c & 0xf)+'0';
-        if ((c & 0xf) >9){
-            code1=(c & 0xf) - 10 + 'A';
-        }
-        c=(c>>4)&0xf;
-        code0=c+'0';
-        if (c > 9){
-            code0=c - 10 + 'A';
-        }
-        encodedString+="%";
-        encodedString+=code0;
-        encodedString+=code1;
-      }
-      yield();
-    }
-    return encodedString;
-}
