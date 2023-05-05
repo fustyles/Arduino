12c2b9b53b71a942a3f8fced028de9853fca8a9d
fustyles <34397289+fustyles@users.noreply.github.com>
2023-05-05 19:37:22 +0800

    Update ESP32_openAI.ino

--- ESP32_openAI.ino
+++ ESP32_openAI.ino
@@ -1,6 +1,6 @@
 /* 
 ESP32 Use ChatGPT API (GPT-3.5)
-Author : ChungYi Fu (Kaohsiung, Taiwan)  2023-4-1 23:30
+Author : ChungYi Fu (Kaohsiung, Taiwan)  2023-5-5 19:30
 https://www.facebook.com/francefu
 
 Tutorial
@@ -11,14 +11,18 @@ https://beta.openai.com/docs/api-reference/completions/create
 #include <WiFi.h>             
 #include <WiFiClientSecure.h>
 
-const char* ssid     = "teacher";   // your network SSID
-const char* password = "87654321";   // your network password
+char wifi_ssid[] = "teacher";
+char wifi_pass[] = "87654321";
 String openaiKey = "";  // openAI API Key
-String role = "You are a helpful assistant.";
 
+//ChatGPT
+String role = "You are a helpful assistant.";
 String model = "gpt-3.5-turbo";
 String system_content = "{\"role\": \"system\", \"content\":\""+ role +"\"}";
 String historical_messages = system_content;
+
+//Image generator
+String imageSize = "256x256"; // 256x256, 512x512 , 1024x1024
 
 void setup() 
 {
@@ -27,12 +31,9 @@ void setup()
   
   initWiFi();
 
-  Serial.println("\nYou can call me Mike Fu and I live in Taiwan.");
-  Serial.println(openAI_chat("You can call me Mike Fu and I live in Taiwan.")); 
-  Serial.println("\nWhat is my name?"); 
-  Serial.println(openAI_chat("What is my name?")); 
-  Serial.println("\nWhere am I?"); 
-  Serial.println(openAI_chat("Where am I?"));     
+  Serial.println(openAI_chat("I am from Taiwan."));
+  Serial.println(openAI_chat("Where am I?"));
+  Serial.println(openAI_image("Beautiful Taiwan."));   
 }
 
 void loop()
@@ -45,12 +46,12 @@ void initWiFi() {
   //WiFi.config(IPAddress(192, 168, 201, 100), IPAddress(192, 168, 201, 2), IPAddress(255, 255, 255, 0));
 
   for (int i=0;i<2;i++) {
-    WiFi.begin(ssid, password);
+    WiFi.begin(wifi_ssid, wifi_pass);
   
     delay(1000);
     Serial.println("");
     Serial.print("Connecting to ");
-    Serial.println(ssid);
+    Serial.println(wifi_ssid);
     
     long int StartTime=millis();
     while (WiFi.status() != WL_CONNECTED) {
@@ -149,4 +150,57 @@ String openAI_chat(String message) {
 
 void openAI_chat_reset() {
   historical_messages = system_content;
+}
+
+String openAI_image(String message) { 
+  WiFiClientSecure client_tcp;
+  client_tcp.setInsecure();   //run version 1.0.5 or above
+
+  String request = "{\"prompt\":\""+ message+"\", \"size\":\""+imageSize+"\", \"n\":1}";
+  if (client_tcp.connect("api.openai.com", 443)) {
+    client_tcp.println("POST /v1/images/generations HTTP/1.1");
+    client_tcp.println("Connection: close"); 
+    client_tcp.println("Host: api.openai.com");
+    client_tcp.println("Authorization: Bearer " + openaiKey);
+    client_tcp.println("Content-Type: application/json; charset=utf-8");
+    client_tcp.println("Content-Length: " + String(request.length()));
+    client_tcp.println();
+    for (int i = 0; i < request.length(); i += 1024) {
+      client_tcp.print(request.substring(i, i+1024));
+    }
+    
+    String getResponse="",Feedback="";
+    boolean state = false;
+    int waitTime = 20000;   // timeout 20 seconds
+    long startTime = millis();
+    while ((startTime + waitTime) > millis()) {
+      Serial.print(".");
+      delay(100);      
+      while (client_tcp.available()) {
+          char c = client_tcp.read();
+          if (String(c)=="\""&&state==true)
+            break;           
+          if (state==true)
+            getResponse += String(c);
+          if (c == '\n')
+            Feedback = "";
+          else if (c != '\r')
+            Feedback += String(c);
+          if (Feedback.indexOf("\"url\": \"")!=-1)
+            state=true;             
+          startTime = millis();
+       }
+       if (getResponse.length()>0) {
+          client_tcp.stop();
+          Serial.println("");
+          return getResponse;
+       }
+    }
+    
+    client_tcp.stop();
+    Serial.println(Feedback);
+    return "error";
+  }
+  else
+    return "Connection failed";  
 }
