0226bd5f5978587a77417ab5307529225ee91667
fustyles <34397289+fustyles@users.noreply.github.com>
2023-09-27 22:03:20 +0800

    Update ESP32-CAM_GoogleDrive_Linenotify.ino

--- ESP32-CAM_GoogleDrive_Linenotify/ESP32-CAM_GoogleDrive_Linenotify.ino
+++ ESP32-CAM_GoogleDrive_Linenotify/ESP32-CAM_GoogleDrive_Linenotify.ino
@@ -236,37 +236,20 @@ String SendCapturedImage() {
   return getBody;
 }
 
-//https://github.com/zenmanenergy/ESP8266-Arduino-Examples/
-String urlencode(String str)
-{
-    String encodedString="";
-    char c;
-    char code0;
-    char code1;
-    char code2;
-    for (int i =0; i < str.length(); i++){
-      c=str.charAt(i);
-      if (c == ' '){
-        encodedString+= '+';
-      } else if (isalnum(c)){
-        encodedString+=c;
-      } else{
-        code1=(c & 0xf)+'0';
-        if ((c & 0xf) >9){
-            code1=(c & 0xf) - 10 + 'A';
-        }
-        c=(c>>4)&0xf;
-        code0=c+'0';
-        if (c > 9){
-            code0=c - 10 + 'A';
-        }
-        code2='\0';
-        encodedString+='%';
-        encodedString+=code0;
-        encodedString+=code1;
-        //encodedString+=code2;
-      }
-      yield();
+//https://www.arduino.cc/reference/en/libraries/urlencode/
+String urlencode(String str) {
+  const char *msg = str.c_str();
+  const char *hex = "0123456789ABCDEF";
+  String encodedMsg = "";
+  while (*msg != '\0') {
+    if (('a' <= *msg && *msg <= 'z') || ('A' <= *msg && *msg <= 'Z') || ('0' <= *msg && *msg <= '9') || *msg == '-' || *msg == '_' || *msg == '.' || *msg == '~') {
+      encodedMsg += *msg;
+    } else {
+      encodedMsg += '%';
+      encodedMsg += hex[(unsigned char)*msg >> 4];
+      encodedMsg += hex[*msg & 0xf];
     }
-    return encodedString;
+    msg++;
+  }
+  return encodedMsg;
 }
