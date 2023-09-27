fff941ccdb3b5b56f58281c415c73d2a7de74703
fustyles <34397289+fustyles@users.noreply.github.com>
2023-09-27 22:02:50 +0800

    Update ESP32-CAM_Linebot_Spreadsheet.ino

--- ESP32-CAM_Linebot_Spreadsheet/ESP32-CAM_Linebot_Spreadsheet.ino
+++ ESP32-CAM_Linebot_Spreadsheet/ESP32-CAM_Linebot_Spreadsheet.ino
@@ -338,35 +338,20 @@ String SendCapturedImage(String token) {
   return getBody;
 }
 
+//https://www.arduino.cc/reference/en/libraries/urlencode/
 String urlencode(String str) {
-  String encodedString="";
-  char c;
-  char code0;
-  char code1;
-  char code2;
-  for (int i =0; i < str.length(); i++) {
-    c=str.charAt(i);
-    if (c == ' ') {
-      encodedString+= '+';
-    } else if (isalnum(c)) {
-      encodedString+=c;
+  const char *msg = str.c_str();
+  const char *hex = "0123456789ABCDEF";
+  String encodedMsg = "";
+  while (*msg != '\0') {
+    if (('a' <= *msg && *msg <= 'z') || ('A' <= *msg && *msg <= 'Z') || ('0' <= *msg && *msg <= '9') || *msg == '-' || *msg == '_' || *msg == '.' || *msg == '~') {
+      encodedMsg += *msg;
     } else {
-      code1=(c & 0xf)+'0';
-      if ((c & 0xf) >9) {
-          code1=(c & 0xf) - 10 + 'A';
-      }
-      c=(c>>4)&0xf;
-      code0=c+'0';
-      if (c > 9) {
-          code0=c - 10 + 'A';
-      }
-      code2='\0';
-      encodedString+='%';
-      encodedString+=code0;
-      encodedString+=code1;
-      //encodedString+=code2;
+      encodedMsg += '%';
+      encodedMsg += hex[(unsigned char)*msg >> 4];
+      encodedMsg += hex[*msg & 0xf];
     }
-    yield();
+    msg++;
   }
-  return encodedString;
+  return encodedMsg;
 }
