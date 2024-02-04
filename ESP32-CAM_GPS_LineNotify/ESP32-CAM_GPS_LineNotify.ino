606c0f000fed3c7554b823d962d1d863f3a4e615
fustyles <34397289+fustyles@users.noreply.github.com>
2024-02-04 12:13:44 +0800

    Update ESP32-CAM_GPS_LineNotify.ino

--- ESP32-CAM_GPS_LineNotify/ESP32-CAM_GPS_LineNotify.ino
+++ ESP32-CAM_GPS_LineNotify/ESP32-CAM_GPS_LineNotify.ino
@@ -1,6 +1,6 @@
 /*
 ESP32-CAM (with flash) Send GPS coordinate and still to Line notify
-Author : ChungYi Fu (Kaohsiung, Taiwan)  2024-2-4 11:00
+Author : ChungYi Fu (Kaohsiung, Taiwan)  2024-2-4 12:00
 https://www.facebook.com/francefu
 
 GPS Module TX -> ESP32-CAM RX (IO3)
