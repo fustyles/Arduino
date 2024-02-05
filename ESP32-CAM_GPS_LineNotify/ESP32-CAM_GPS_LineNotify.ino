e8af70fcc8e76754033392309fb1478595941e31
fustyles <34397289+fustyles@users.noreply.github.com>
2024-02-05 09:00:36 +0800

    Update ESP32-CAM_GPS_LineNotify.ino

--- ESP32-CAM_GPS_LineNotify/ESP32-CAM_GPS_LineNotify.ino
+++ ESP32-CAM_GPS_LineNotify/ESP32-CAM_GPS_LineNotify.ino
@@ -227,11 +227,13 @@ void loop()
         gpsNewData = true;
     }
   }
-  unsigned long TinyGPS_chars;
-  unsigned short TinyGPS_sentences, TinyGPS_failed;
+
   if (gpsNewData) {
     float TinyGPS_flat, TinyGPS_flon;
     unsigned long TinyGPS_age;
+    unsigned long TinyGPS_chars;
+    unsigned short TinyGPS_sentences, TinyGPS_failed;
+    
     gps.f_get_position(&TinyGPS_flat, &TinyGPS_flon, &TinyGPS_age);
     gps.stats(&TinyGPS_chars, &TinyGPS_sentences, &TinyGPS_failed);
     String flat = (TinyGPS_flat == TinyGPS::GPS_INVALID_F_ANGLE ? "0" : TinyGPS_coordinateToString(TinyGPS_flat));
