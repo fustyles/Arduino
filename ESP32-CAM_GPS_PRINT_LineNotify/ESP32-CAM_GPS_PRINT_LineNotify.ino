abdef0e460f1c3683cd158589d095ee0065ef22f
fustyles <34397289+fustyles@users.noreply.github.com>
2024-02-07 10:57:09 +0800

    Update ESP32-CAM_GPS_PRINT_LineNotify.ino

--- ESP32-CAM_GPS_PRINT_LineNotify/ESP32-CAM_GPS_PRINT_LineNotify.ino
+++ ESP32-CAM_GPS_PRINT_LineNotify/ESP32-CAM_GPS_PRINT_LineNotify.ino
@@ -278,8 +278,9 @@ void loop()
     
     String flat = (TinyGPS_flat == TinyGPS::GPS_INVALID_F_ANGLE ? "0" : TinyGPS_coordinateToString(TinyGPS_flat));
     String flon = (TinyGPS_flon == TinyGPS::GPS_INVALID_F_ANGLE ? "0" : TinyGPS_coordinateToString(TinyGPS_flon));
-    String mapURL = "https://www.google.com/maps/search/?api=1&map_action=map&zoom=16&query="+flat+","+flon;
-    sendStillPrintGPSToLineNotify(lineToken, mapURL, flat+", "+flon);
+    String condinate = flat+","+flon;
+    String mapURL = "https://www.google.com/maps/search/?api=1&map_action=map&zoom=16&query="+condinate;
+    sendStillPrintGPSToLineNotify(lineToken, mapURL, condinate);
   
     delay(72000);  //Line Notify上限 50張/時，平均間隔72秒
   }
