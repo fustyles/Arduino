7949de8490e525495e8064fdab3e2dc2cf695602
fustyles <34397289+fustyles@users.noreply.github.com>
2024-11-04 20:33:39 +0800

    Update ESP32-CAM_GoogleDrive_Linenotify.ino

--- ESP32-CAM_GoogleDrive_Linenotify/ESP32-CAM_GoogleDrive_Linenotify.ino
+++ ESP32-CAM_GoogleDrive_Linenotify/ESP32-CAM_GoogleDrive_Linenotify.ino
@@ -1,11 +1,11 @@
 /*
-ESP32-CAM (Save a captured photo to Google Drive and LineNotify)
-Author : ChungYi Fu (Kaohsiung, Taiwan)  2019-8-14 21:00
+ESP32-CAM Upload a captured photo to Google Drive and send the uploaded image link to Line Notify or Line Bot.
+Author : ChungYi Fu (Kaohsiung, Taiwan)  2024-11-4 20:00
 https://www.facebook.com/francefu
 
 Google Apps Script
 https://github.com/fustyles/webduino/blob/gs/SendCapturedImageToGoogleDriveAndLinenotify_doPost.gs
-You must allow anyone and anonymous to execute the google script.
+You must allow anyone, including anonymous users, to execute the Google Script.
 
 How to add a new script
 https://www.youtube.com/watch?v=f46VBqWwUuI
@@ -15,34 +15,31 @@ https://script.google.com/home/executions
 https://drive.google.com/drive/my-drive
 */
 
-// Enter your WiFi ssid and password
-const char* ssid     = "*****";   //your network SSID
-const char* password = "*****";   //your network password
+String myAppsScriptID = "";
+String myFoldername = "ESP32-CAM";   // If you want to see the thumbnail of a photo from Google Drive in a conversation message on the Line app on your mobile phone, you need to change the permissions of the Google Drive folder, setting it so that anyone with the link can view it.
+String myFilename = "HOME";
 
-String myScript = "/macros/s/**********/exec";    //Create your Google Apps Script and replace the "myScript" path.
-String myLineNotifyToken = "myToken=**********";    //Line Notify Token. You can set the value of xxxxxxxxxx empty if you don't want to send picture to Linenotify.
-String myFoldername = "&myFoldername=ESP32-CAM";
-String myFilename = "&myFilename=ESP32-CAM.jpg";
-String myImage = "&myFile=";
+// If you want to send a uploaded image link to Line Notify.
+String myLineNotifyToken = "";
+
+// If you want to send a uploaded image link to Line Bot.
+String myLineBotToken = "";    // Channel access token
+String myLineBotUserID = "";   // userId or groupId
+
+char wifi_ssid[] = "teacher";
+char wifi_pass[] = "12345678";
 
 #include <WiFi.h>
 #include <WiFiClientSecure.h>
+WiFiClientSecure client;
+#include "esp_camera.h"
 #include "soc/soc.h"
 #include "soc/rtc_cntl_reg.h"
-#include "Base64.h"
-
-#include "esp_camera.h"
-
-// WARNING!!! Make sure that you have either selected ESP32 Wrover Module,
-//            or another board which has PSRAM enabled
-
-//CAMERA_MODEL_AI_THINKER
 #define PWDN_GPIO_NUM     32
 #define RESET_GPIO_NUM    -1
 #define XCLK_GPIO_NUM      0
 #define SIOD_GPIO_NUM     26
 #define SIOC_GPIO_NUM     27
-
 #define Y9_GPIO_NUM       35
 #define Y8_GPIO_NUM       34
 #define Y7_GPIO_NUM       39
@@ -55,59 +52,150 @@ String myImage = "&myFile=";
 #define HREF_GPIO_NUM     23
 #define PCLK_GPIO_NUM     22
 
-void setup()
-{
-  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
-  
-  Serial.begin(115200);
-  delay(10);
-  
-  WiFi.mode(WIFI_STA);
+#include "Base64.h"
 
-  Serial.println("");
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);  
-  
-  long int StartTime=millis();
-  while (WiFi.status() != WL_CONNECTED) 
-  {
-    delay(500);
-    if ((StartTime+10000) < millis()) break;
-  } 
+void initWiFi() {
+  for (int i=0;i<2;i++) {
+    WiFi.begin(wifi_ssid, wifi_pass);
 
-  Serial.println("");
-  Serial.println("STAIP address: ");
-  Serial.println(WiFi.localIP());
-    
-  Serial.println("");
+    delay(1000);
+    Serial.println("");
+    Serial.print("Connecting to ");
+    Serial.println(wifi_ssid);
 
-  if (WiFi.status() != WL_CONNECTED) {
-    Serial.println("Reset");
-    
-    ledcAttachPin(4, 3);
-    ledcSetup(3, 5000, 8);
-    ledcWrite(3,10);
-    delay(200);
-    ledcWrite(3,0);
-    delay(200);    
-    ledcDetachPin(3);
-        
+    long int StartTime=millis();
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        if ((StartTime+5000) < millis()) break;
+    }
+
+    if (WiFi.status() == WL_CONNECTED) {
+      Serial.println("");
+      Serial.println("STAIP address: ");
+      Serial.println(WiFi.localIP());
+      Serial.println("");
+
+      break;
+    }
+  }
+}
+
+void SendStillToGoogleDrive(String appsScriptID, String foldername, String filename) {
+  String script = "/macros/s/"+appsScriptID+"/exec";
+  String Data = "&myFoldername="+urlencode(foldername)+"&myFilename="+urlencode(filename)+"&myLineType=&myToken=&myUserID=&myFile=";
+  SendStillToGoogleDrive(script, Data);
+}
+
+void SendStillToGoogleDriveLineNotify(String appsScriptID, String foldername, String filename, String token) {
+  String script = "/macros/s/"+appsScriptID+"/exec";
+  String Data = "&myFoldername="+urlencode(foldername)+"&myFilename="+urlencode(filename)+"&myLineType=notify&myToken="+token+"&myUserID=&myFile=";
+  SendStillToGoogleDrive(script, Data);
+}
+
+void SendStillToGoogleDriveLineBot(String appsScriptID, String foldername, String filename, String token, String userID) {
+  String script = "/macros/s/"+appsScriptID+"/exec";
+  String Data = "&myFoldername="+urlencode(foldername)+"&myFilename="+urlencode(filename)+"&myLineType=bot&myToken="+token+"&myUserID="+userID+"&myFile=";
+  SendStillToGoogleDrive(script, Data);
+}
+
+String SendStillToGoogleDrive(String myScript, String myData) {
+  const char* myDomain = "script.google.com";
+  String getAll="", getBody = "";
+
+  camera_fb_t * fb = NULL;
+  fb = esp_camera_fb_get();
+  if(!fb) {
+    Serial.println("Camera capture failed");
     delay(1000);
     ESP.restart();
-  }
-  else {
-    ledcAttachPin(4, 3);
-    ledcSetup(3, 5000, 8);
-    for (int i=0;i<5;i++) {
-      ledcWrite(3,10);
-      delay(200);
-      ledcWrite(3,0);
-      delay(200);    
-    }
-    ledcDetachPin(3);      
+    return "Camera capture failed";
   }
 
+  Serial.println("Connect to " + String(myDomain));
+  client.setInsecure();
+  if (client.connect(myDomain, 443)) {
+    Serial.println("Connection successful");
+
+    char *input = (char *)fb->buf;
+    char output[base64_enc_len(3)];
+    String imageFile = "data:image/jpeg;base64,";
+    for (int i=0;i<fb->len;i++) {
+      base64_encode(output, (input++), 3);
+      if (i%3==0) imageFile += urlencode(String(output));
+    }
+
+    client.println("POST " + myScript + " HTTP/1.1");
+    client.println("Host: " + String(myDomain));
+    client.println("Content-Length: " + String(myData.length()+imageFile.length()));
+    client.println("Content-Type: application/x-www-form-urlencoded");
+    client.println("Connection: close");
+    client.println();
+
+    client.print(myData);
+    int Index;
+    for (Index = 0; Index < imageFile.length(); Index = Index+1024) {
+      client.print(imageFile.substring(Index, Index+1024));
+    }
+    esp_camera_fb_return(fb);
+
+    int waitTime = 10000;
+    long startTime = millis();
+    boolean state = false;
+
+    while ((startTime + waitTime) > millis())
+    {
+      Serial.print(".");
+      delay(100);
+      while (client.available())
+      {
+          char c = client.read();
+          if (state==true) getBody += String(c);
+          if (c == '\n')
+          {
+            if (getAll.length()==0) state=true;
+            getAll = "";
+          }
+          else if (c != '\r')
+            getAll += String(c);
+          startTime = millis();
+       }
+       if (getBody.length()>0) break;
+    }
+    client.stop();
+    Serial.println(getBody);
+  }
+  else {
+    getBody="Connected to " + String(myDomain) + " failed.";
+    Serial.println("Connected to " + String(myDomain) + " failed.");
+  }
+
+  return getBody;
+}
+
+String urlencode(String str) {
+  const char *msg = str.c_str();
+  const char *hex = "0123456789ABCDEF";
+  String encodedMsg = "";
+  while (*msg != '\0') {
+    if (('a' <= *msg && *msg <= 'z') || ('A' <= *msg && *msg <= 'Z') || ('0' <= *msg && *msg <= '9') || *msg == '-' || *msg == '_' || *msg == '.' || *msg == '~') {
+      encodedMsg += *msg;
+    } else {
+      encodedMsg += '%';
+      encodedMsg += hex[(unsigned char)*msg >> 4];
+      encodedMsg += hex[*msg & 0xf];
+    }
+    msg++;
+  }
+  return encodedMsg;
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
+  Serial.setDebugOutput(true);
+  Serial.println();
+  
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
   config.ledc_timer = LEDC_TIMER_0;
@@ -129,127 +217,45 @@ void setup()
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
   config.pixel_format = PIXFORMAT_JPEG;
-  //init with high specs to pre-allocate larger buffers
   if(psramFound()){
     config.frame_size = FRAMESIZE_UXGA;
-    config.jpeg_quality = 10;  //0-63 lower number means higher quality
+    config.jpeg_quality = 10;
     config.fb_count = 2;
   } else {
     config.frame_size = FRAMESIZE_SVGA;
-    config.jpeg_quality = 12;  //0-63 lower number means higher quality
+    config.jpeg_quality = 12;
     config.fb_count = 1;
   }
-  
-  // camera init
   esp_err_t err = esp_camera_init(&config);
   if (err != ESP_OK) {
     Serial.printf("Camera init failed with error 0x%x", err);
     delay(1000);
     ESP.restart();
   }
-
-  //drop down frame size for higher initial frame rate
   sensor_t * s = esp_camera_sensor_get();
-  s->set_framesize(s, FRAMESIZE_VGA);  // UXGA|SXGA|XGA|SVGA|VGA|CIF|QVGA|HQVGA|QQVGA
+  if (s->id.PID == OV3660_PID) {
+    s->set_vflip(s, 1);
+    s->set_brightness(s, 1);
+    s->set_saturation(s, -2);
+  }
+  s->set_framesize(s, FRAMESIZE_QVGA);
+  
+  pinMode(4, OUTPUT);
+  digitalWrite(4, LOW);
+
+  initWiFi();
+
+  // Google drive
+  SendStillToGoogleDrive(myAppsScriptID, myFoldername, myFilename);
+  
+  // Google drive and Line Notify
+  SendStillToGoogleDriveLineNotify(myAppsScriptID, myFoldername, myFilename, myLineNotifyToken);
+
+  // Google drive and Line Bot  
+  SendStillToGoogleDriveLineBot(myAppsScriptID, myFoldername, myFilename, myLineBotToken, myLineBotUserID);
 }
 
 void loop()
 {
-  SendCapturedImage();
-  delay(60000);
-}
 
-String SendCapturedImage() {
-  const char* myDomain = "script.google.com";
-  String getAll="", getBody = "";
-  
-  camera_fb_t * fb = NULL;
-  fb = esp_camera_fb_get();  
-  if(!fb) {
-    Serial.println("Camera capture failed");
-    delay(1000);
-    ESP.restart();
-    return "Camera capture failed";
-  }  
-  
-  Serial.println("Connect to " + String(myDomain));
-  WiFiClientSecure client_tcp;
-  client_tcp.setInsecure();   //run version 1.0.5 or above
-  
-  if (client_tcp.connect(myDomain, 443)) {
-    Serial.println("Connection successful");
-    
-    char *input = (char *)fb->buf;
-    char output[base64_enc_len(3)];
-    String imageFile = "data:image/jpeg;base64,";
-    for (int i=0;i<fb->len;i++) {
-      base64_encode(output, (input++), 3);
-      if (i%3==0) imageFile += urlencode(String(output));
-    }
-    String Data = myLineNotifyToken+myFoldername+myFilename+myImage;
-    
-    client_tcp.println("POST " + myScript + " HTTP/1.1");
-    client_tcp.println("Host: " + String(myDomain));
-    client_tcp.println("Content-Length: " + String(Data.length()+imageFile.length()));
-    client_tcp.println("Content-Type: application/x-www-form-urlencoded");
-    client_tcp.println("Connection: keep-alive");
-    client_tcp.println();
-    
-    client_tcp.print(Data);
-    int Index;
-    for (Index = 0; Index < imageFile.length(); Index = Index+1000) {
-      client_tcp.print(imageFile.substring(Index, Index+1000));
-    }
-    esp_camera_fb_return(fb);
-    
-    int waitTime = 10000;   // timeout 10 seconds
-    long startTime = millis();
-    boolean state = false;
-    
-    while ((startTime + waitTime) > millis())
-    {
-      Serial.print(".");
-      delay(100);      
-      while (client_tcp.available()) 
-      {
-          char c = client_tcp.read();
-          if (state==true) getBody += String(c);        
-          if (c == '\n') 
-          {
-            if (getAll.length()==0) state=true; 
-            getAll = "";
-          } 
-          else if (c != '\r')
-            getAll += String(c);
-          startTime = millis();
-       }
-       if (getBody.length()>0) break;
-    }
-    client_tcp.stop();
-    Serial.println(getBody);
-  }
-  else {
-    getBody="Connected to " + String(myDomain) + " failed.";
-    Serial.println("Connected to " + String(myDomain) + " failed.");
-  }
-  
-  return getBody;
-}
-
-//https://www.arduino.cc/reference/en/libraries/urlencode/
-String urlencode(String str) {
-  const char *msg = str.c_str();
-  const char *hex = "0123456789ABCDEF";
-  String encodedMsg = "";
-  while (*msg != '\0') {
-    if (('a' <= *msg && *msg <= 'z') || ('A' <= *msg && *msg <= 'Z') || ('0' <= *msg && *msg <= '9') || *msg == '-' || *msg == '_' || *msg == '.' || *msg == '~') {
-      encodedMsg += *msg;
-    } else {
-      encodedMsg += '%';
-      encodedMsg += hex[(unsigned char)*msg >> 4];
-      encodedMsg += hex[*msg & 0xf];
-    }
-    msg++;
-  }
-  return encodedMsg;
 }
